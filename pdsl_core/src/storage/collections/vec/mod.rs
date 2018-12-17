83378c393e14ca0ddd762422ba0446449348aa27
Herobird <robbepop@web.de>
2018-12-17 13:44:33 +0100

    [pdsl_core] Update storage::Vec module docs

--- pdsl_core/src/storage/collections/vec/mod.rs
+++ pdsl_core/src/storage/collections/vec/mod.rs
@@ -1,4 +1,7 @@
 //! A contiguous growable array type, written `Vec<T>` but pronounced 'vector'.
+//!
+//! Stores its elements in the contract's storage
+//! and operates directly on it.
 
 #[cfg(all(test, feature = "test-env"))]
 mod tests;
