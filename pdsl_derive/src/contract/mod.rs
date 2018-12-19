91615af21a1c6c4b86a9c5a6762c08a6a7888a41
Herobird <robbepop@web.de>
2018-12-19 17:19:30 +0100

    [pdsl_derive] Clean up legacy and deprecated pdsl_derive functionality
    This basically removes all code from pdsl_derive.
    It will receive a major overhaul once pdsl_core is done.

--- pdsl_derive/src/contract/mod.rs
+++
@@ -1,9 +0,0 @@
-mod data;
-mod code_gen;
-
-pub use self::data::{
-	ContractItem,
-	ContractStruct,
-	ContractEnum,
-	ContractModule,
-};
--- pdsl_derive/src/lib.rs
+++ pdsl_derive/src/lib.rs
@@ -1,35 +1,13 @@
-#![recursion_limit = "256"]
+#![recursion_limit = "128"]
 
 extern crate proc_macro;
-
-use proc_macro2;
-use syn::{
-	self,
-	parse_macro_input
-};
-use quote::quote;
-
-mod error;
-mod utils;
-mod contract;
-
-use crate::contract::ContractModule;
 
 #[proc_macro_attribute]
 pub fn module(
 	_args: proc_macro::TokenStream,
 	input: proc_macro::TokenStream,
 ) -> proc_macro::TokenStream {
-	// println!("pdsl::module - begin");
-	// let _args_toks = parse_macro_input!(args as syn::AttributeArgs);
-	// println!("[substrate_contract] before parsing");
-	let named_mod = parse_macro_input!(input as ContractModule);
-	// println!("[substrate_contract] after parsing");
-	// println!("{:#?}", named_mod);
-	let out = quote!{ #named_mod };
-	// println!("{:#?}", out);
-	// println!("pdsl::module - end");
-	out.into()
+	input
 }
 
 #[proc_macro_attribute]
@@ -37,6 +15,5 @@ pub fn contract(
 	_args: proc_macro::TokenStream,
 	input: proc_macro::TokenStream
 ) -> proc_macro::TokenStream {
-	// println!("pdsl::contract");
 	input
 }
