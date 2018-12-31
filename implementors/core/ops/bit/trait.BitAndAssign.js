365e7fd445d96932b93463f98801e1d897a847bd
Herobird <robbepop@web.de>
2018-12-31 16:31:51 +0100

    Update documentation

---
+++ implementors/core/ops/bit/trait.BitAndAssign.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["pdsl_core"] = [{text:"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.BitAndAssign.html\" title=\"trait core::ops::bit::BitAndAssign\">BitAndAssign</a>&lt;T&gt; for <a class=\"struct\" href=\"pdsl_core/storage/struct.Value.html\" title=\"struct pdsl_core::storage::Value\">Value</a>&lt;T&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.BitAndAssign.html\" title=\"trait core::ops::bit::BitAndAssign\">BitAndAssign</a>&lt;T&gt; + <a class=\"trait\" href=\"parity_codec/codec/trait.Codec.html\" title=\"trait parity_codec::codec::Codec\">Codec</a>,&nbsp;</span>",synthetic:false,types:["pdsl_core::storage::value::Value"]},{text:"impl&lt;T, '_&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.BitAndAssign.html\" title=\"trait core::ops::bit::BitAndAssign\">BitAndAssign</a>&lt;&amp;'_ <a class=\"struct\" href=\"pdsl_core/storage/struct.Value.html\" title=\"struct pdsl_core::storage::Value\">Value</a>&lt;T&gt;&gt; for <a class=\"struct\" href=\"pdsl_core/storage/struct.Value.html\" title=\"struct pdsl_core::storage::Value\">Value</a>&lt;T&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.BitAndAssign.html\" title=\"trait core::ops::bit::BitAndAssign\">BitAndAssign</a>&lt;T&gt; + <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> + <a class=\"trait\" href=\"parity_codec/codec/trait.Codec.html\" title=\"trait parity_codec::codec::Codec\">Codec</a>,&nbsp;</span>",synthetic:false,types:["pdsl_core::storage::value::Value"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
