9ce455116b27015f0d8edb826327f2bf7e3e1394
Benjamin Kampmann <ben@gnunicorn.org>
2019-01-08 11:44:13 +0100

    Rename Tweet to 'Peep'

--- examples/subpeep/src/tests.rs
+++ examples/subpeep/src/tests.rs
@@ -4,30 +4,30 @@ use super::*;
 fn deploy() {
 	let subpeep = Subpeep::default();
 	assert_eq!(
-		subpeep.recent_tweets(10),
+		subpeep.recent_peeps(10),
 		vec![]
 	);
 	assert_eq!(
-		subpeep.recent_user_tweets(10, "alice"),
+		subpeep.recent_user_peeps(10, "alice"),
 		None
 	);
 }
 
 #[test]
-fn tweet_message() {
+fn peep_message() {
 	let mut subpeep = Subpeep::default();
 	let test_user = "Alice";
 	let test_message = "Hello, World!";
 	subpeep.register(test_user.into());
-	subpeep.tweet_message(test_user.into(), test_message.into());
+	subpeep.peep_message(test_user.into(), test_message.into());
 	assert_eq!(
-		subpeep.recent_tweets(10),
-		vec![Tweet::new(test_user.into(), test_message.into())],
+		subpeep.recent_peeps(10),
+		vec![Peep::new(test_user.into(), test_message.into())],
 	);
 	assert_eq!(
-		subpeep.recent_user_tweets(10, test_user.into()),
+		subpeep.recent_user_peeps(10, test_user.into()),
 		Some(vec![
-			Tweet::new(test_user.into(), test_message.into())
+			Peep::new(test_user.into(), test_message.into())
 		])
 	);
 }
