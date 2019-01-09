34c9c535ef98b07c54ecde6f7ed4eaf945eb6a2a
Herobird <robbepop@web.de>
2019-01-09 12:43:36 +0100

    [pdsl_core] Add docs to eh_personality lang item

--- pdsl_core/src/panic_handler.rs
+++ pdsl_core/src/panic_handler.rs
@@ -12,5 +12,9 @@ pub extern fn oom(_: Layout) -> ! {
 	unsafe { core::intrinsics::abort(); }
 }
 
+/// This is only required in non wasm32-unknown-unknown targets.
+///
+/// Since pdsl_core is targeted for wasm32-unknown-unknown we should
+/// maybe remove this.
 #[lang = "eh_personality"]
 extern fn eh_personality() {}
