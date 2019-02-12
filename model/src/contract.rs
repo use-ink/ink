db7e2e9f4893c9abeb5d44171a72bb512f54ddea
Herobird <robbepop@web.de>
2019-02-12 19:22:59 +0100

    [pdsl_model] Add API to register a deployment handler
    This should further simplify declaration of a contract.

--- model/src/contract.rs
+++ model/src/contract.rs
@@ -3,6 +3,9 @@ use crate::{
 		ContractState,
 		EmptyContractState,
 	},
+	exec_env::{
+		ExecutionEnv,
+	},
 	msg::{
 		Message,
 	},
@@ -16,6 +19,58 @@ use crate::{
 };
 use core::marker::PhantomData;
 
+/// A marker struct to tell that the deploy handler requires no arguments.
+#[derive(Copy, Clone)]
+pub struct NoDeployArgs;
+
+/// A handler specific to deploying a smart contract.
+///
+/// # Note
+///
+/// This is normally mainly used to correctly initialize
+/// a smart contracts state.
+pub struct DeployHandler<State, Args> {
+	/// The arguments that deploy expects.
+	///
+	/// This tricks Rust into thinking that this owns the state type.
+	/// However, it is just a marker which allows the contract declaration
+	/// to be a zero-sized-type (ZST).
+	args: PhantomData<Args>,
+	/// The actual deployment function.
+	deploy_fn: fn(&mut ExecutionEnv<State>, Args),
+}
+
+impl<State> DeployHandler<State, NoDeployArgs> {
+	/// Returns a deploy handler that does nothing.
+	const fn init() -> Self {
+		Self {
+			args: PhantomData,
+			deploy_fn: move |env, _| {},
+		}
+	}
+}
+
+impl<State, Args> DeployHandler<State, Args> {
+	/// Returns a new deploy handler for the given closure.
+	const fn new(raw_handler: fn(&mut ExecutionEnv<State>, Args)) -> Self {
+		Self {
+			args: PhantomData,
+			deploy_fn: raw_handler,
+		}
+	}
+}
+
+impl<State, Args> Copy for DeployHandler<State, Args> {}
+
+impl<State, Args> Clone for DeployHandler<State, Args> {
+	fn clone(&self) -> Self {
+		Self {
+			args: self.args,
+			deploy_fn: self.deploy_fn,
+		}
+	}
+}
+
 /// A contract declaration.
 ///
 /// Uses the builder pattern in order to represent a contract
@@ -25,16 +80,17 @@ use core::marker::PhantomData;
 /// in order to dispatch a contract call or deploy state.
 pub struct ContractDecl<
 	State,
+	DeployArgs,
 	HandlerChain,
 > {
-	/// The name of the contract.
-	name: &'static str,
 	/// The type of the contract state.
 	///
 	/// This tricks Rust into thinking that this owns the state type.
 	/// However, it is just a marker which allows the contract declaration
 	/// to be a zero-sized-type (ZST).
 	state: PhantomData<State>,
+
+	deployer: DeployHandler<State, DeployArgs>,
 	/// The compile-time chain of message handlers.
 	///
 	/// # Note
@@ -55,54 +111,68 @@ pub struct ContractDecl<
 	handlers: HandlerChain,
 }
 
-impl<State, HandlerChain> Clone for ContractDecl<State, HandlerChain>
+impl<State, DeployArgs, HandlerChain> Clone for ContractDecl<State, DeployArgs, HandlerChain>
 where
 	HandlerChain: Clone,
 {
 	fn clone(&self) -> Self {
 		Self {
-			name: self.name,
 			state: self.state,
+			deployer: self.deployer,
 			handlers: self.handlers.clone(),
 		}
 	}
 }
 
-impl<State, HandlerChain> Copy for ContractDecl<State, HandlerChain>
+impl<State, DeployArgs, HandlerChain> Copy for ContractDecl<State, DeployArgs, HandlerChain>
 where
 	HandlerChain: Copy,
 {}
 
-impl ContractDecl<EmptyContractState, UnreachableMessageHandler> {
+impl ContractDecl<EmptyContractState, NoDeployArgs, UnreachableMessageHandler> {
 	/// Creates a new contract declaration with the given name.
-	pub fn new(name: &'static str) -> Self {
-		Self {
-			name,
-			state: PhantomData,
-			handlers: UnreachableMessageHandler,
-		}
-	}
-
-	/// Makes the contract declaration use the given state.
-	pub fn using_state<State>(self) -> ContractDecl<State, UnreachableMessageHandler> {
+	pub fn new<State>() -> ContractDecl<State, NoDeployArgs, UnreachableMessageHandler> {
 		ContractDecl {
-			name: self.name,
 			state: PhantomData,
+			deployer: DeployHandler::init(),
 			handlers: UnreachableMessageHandler,
 		}
 	}
 }
 
-impl<State, HandlerChain> ContractDecl<State, HandlerChain> {
-	/// Convenience method to append another message handler.
-	const fn append_msg_handler<MsgHandler>(self, handler: MsgHandler)
-		-> ContractDecl<State, (MsgHandler, HandlerChain)>
+impl<State> ContractDecl<State, NoDeployArgs, UnreachableMessageHandler> {
+	/// Registers the given deployment procedure for the contract.
+	///
+	/// # Note
+	///
+	/// This is used to initialize the contract state upon deployment.
+	pub const fn on_deploy<Args>(
+		self,
+		handler: fn(&mut ExecutionEnv<State>, Args),
+	)
+		-> ContractDecl<State, Args, UnreachableMessageHandler>
 	where
-		Self: Copy,
+		Args: parity_codec::Decode,
 	{
 		ContractDecl {
-			name: self.name,
+			state: self.state,
+			deployer: DeployHandler::new(handler),
+			handlers: self.handlers,
+		}
+	}
+}
+
+impl<State, DeployArgs, HandlerChain> ContractDecl<State, DeployArgs, HandlerChain>
+where
+	Self: Copy, // Required in order to make this compile-time computable.
+{
+	/// Convenience method to append another message handler.
+	const fn append_msg_handler<MsgHandler>(self, handler: MsgHandler)
+		-> ContractDecl<State, DeployArgs, (MsgHandler, HandlerChain)>
+	{
+		ContractDecl {
 			state: PhantomData,
+			deployer: self.deployer,
 			handlers: (handler, self.handlers)
 		}
 	}
@@ -116,9 +186,8 @@ impl<State, HandlerChain> ContractDecl<State, HandlerChain> {
 		self,
 		handler: RawMessageHandler<Msg, State>,
 	)
-		-> ContractDecl<State, (MessageHandler<Msg, State>, HandlerChain)>
+		-> ContractDecl<State, DeployArgs, (MessageHandler<Msg, State>, HandlerChain)>
 	where
-		Self: Copy,
 		Msg: Message,
 		State: ContractState,
 	{
@@ -134,9 +203,8 @@ impl<State, HandlerChain> ContractDecl<State, HandlerChain> {
 		self,
 		handler: RawMessageHandlerMut<Msg, State>,
 	)
-		-> ContractDecl<State, (MessageHandlerMut<Msg, State>, HandlerChain)>
+		-> ContractDecl<State, DeployArgs, (MessageHandlerMut<Msg, State>, HandlerChain)>
 	where
-		Self: Copy,
 		Msg: Message,
 		State: ContractState,
 	{
