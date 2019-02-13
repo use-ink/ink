9ae6547ac8ee8f4da7761237873c60d3ce785f1b
Herobird <robbepop@web.de>
2019-02-13 18:43:52 +0100

    [pdsl_model] Introduce experimental caller API
    This is only implemented since we want to use it in a demo contract.

--- model/src/exec_env.rs
+++ model/src/exec_env.rs
@@ -1,6 +1,21 @@
+use pdsl_core::{
+	env::{
+		Env,
+		ContractEnv,
+		srml::{
+			Address,
+		},
+	},
+};
 
 /// Provides a safe interface to an environment given a contract state.
-pub struct ExecutionEnv<S>{
+pub struct ExecutionEnv<State>{
 	/// The contract state.
-	pub state: S,
+	pub state: State,
+}
+
+impl<State> ExecutionEnv<State> {
+	pub fn caller(&self) -> Address {
+		ContractEnv::caller()
+	}
 }
