61c89eb8559c657ffe0de9af532706fb253249de
Herobird <robbepop@web.de>
2019-02-18 15:12:15 +0100

    [examples/fleet_inc] Add fleet_inc example

---
+++ examples/fleet_inc/src/lib.rs
@@ -0,0 +1,49 @@
+#![no_std]
+#![feature(const_str_as_bytes)]
+
+use pdsl_model::{
+	ContractDecl,
+	Contract,
+	state,
+	messages,
+};
+use pdsl_core::storage;
+
+state! {
+	/// A simple contract having just one value that can be incremented and returned.
+	struct Adder {
+		/// The simple value on the contract storage.
+		val: storage::Value<u32>
+	}
+}
+
+messages! {
+	/// Increases the storage value by the given amount.
+	0 => Inc(by: u32);
+	/// Returns the storage value.
+	1 => Get() -> u32;
+}
+
+fn instantiate() -> impl Contract {
+	ContractDecl::new::<Adder>()
+		.on_deploy(|env, init_val| {
+			env.state.val.set(init_val)
+		})
+		.on_msg_mut::<Inc>(|env, by| {
+			env.state.val += by
+		})
+		.on_msg::<Get>(|env, _| {
+			*env.state.val.get()
+		})
+		.instantiate()
+}
+
+#[no_mangle]
+fn deploy() {
+	instantiate().deploy()
+}
+
+#[no_mangle]
+fn call() {
+	instantiate().dispatch()
+}
