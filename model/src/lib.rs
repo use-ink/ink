b1d798fb9225de789615638c8d42e7fe751ddd72
Herobird <robbepop@web.de>
2019-02-19 11:42:24 +0100

    [pdsl] Add bunch of denied compiler warnings

--- model/src/lib.rs
+++ model/src/lib.rs
@@ -4,7 +4,35 @@
 	const_str_as_bytes,
 	core_intrinsics,
 )]
-#![allow(unused)]
+#![deny(bad_style,
+	const_err,
+	dead_code,
+	improper_ctypes,
+	legacy_directory_ownership,
+	non_shorthand_field_patterns,
+	no_mangle_generic_items,
+	overflowing_literals,
+	path_statements,
+	patterns_in_fns_without_body,
+	plugin_as_library,
+	private_in_public,
+	safe_extern_statics,
+	unconditional_recursion,
+	unions_with_drop_fields,
+	unused,
+	unused_allocation,
+	unused_comparisons,
+	unused_parens,
+	while_true,
+	// missing_docs,
+	trivial_casts,
+	trivial_numeric_casts,
+	unused_extern_crates,
+	// unused_import_braces,
+	unused_qualifications,
+	unused_results,
+	// missing-copy-implementations
+)]
 
 #[macro_use]
 mod state;
