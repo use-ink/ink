e2abf2e77b9066c46787d1905cadb97d63badab2
Herobird <robbepop@web.de>
2019-02-20 16:25:48 +0100

    [examples] Extend build.sh to do basically everything
    This builds an optimized wasm file as outputted by rustc.
    Then transforms it into .wat format.
    Adds an important detail about its memory import statement.
    Transforms it back into .wasm.
    Runs wasm-opt on it to further optimize it for binary size.

--- examples/core/incrementer/build.sh
+++ examples/core/incrementer/build.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+# Deprecated Note:
 # The wasm-build executable that is used to tree-shake the wasm binary
 # resulting from the cargo build in the first step expects to find it
 # under target/release/wasm32-unknown-unknown/ in the cwd.
 
-cargo +nightly build --release --target=wasm32-unknown-unknown --verbose
-wasm-build target incrementer --target-runtime=substrate --final=incrementer --save-raw=./target/incrementer-deployed.wasm --target wasm32-unknown-unknown
+PROJNAME=incrementer
+#cargo clean
+#rm Cargo.lock
+CARGO_INCREMENTAL=0 cargo +nightly build --release --target=wasm32-unknown-unknown --verbose
+wasm2wat -o target/$PROJNAME.wat target/wasm32-unknown-unknown/release/$PROJNAME.wasm
+cat target/$PROJNAME.wat | sed "s/(import \"env\" \"memory\" (memory (;0;) 2))/(import \"env\" \"memory\" (memory (;0;) 2 16))/" > target/$PROJNAME-fixed.wat
+wat2wasm -o target/$PROJNAME.wasm target/$PROJNAME-fixed.wat
+wasm-opt -Oz target/$PROJNAME.wasm -o target/$PROJNAME-opt.wasm
+
+#wasm-build target enyzme --target-runtime=substrate --final=adder --save-raw=./target/enzyme-deployed.wasm --target wasm32-unknown-unknown
