b7e3dbed99e029fd35be1424614ef4f7f8ba0dd0
Robin Freyler <robin.freyler@gmail.com>
2019-03-14 11:50:30 +0100

    [model] Move simple model tests out of src

---
+++ model/tests/noop.rs
@@ -0,0 +1,53 @@
+// Copyright 2018-2019 Parity Technologies (UK) Ltd.
+// This file is part of pDSL.
+//
+// pDSL is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// pDSL is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with pDSL.  If not, see <http://www.gnu.org/licenses/>.
+
+#![feature(const_str_as_bytes)]
+
+use pdsl_model::{
+    messages,
+    state,
+    Contract,
+    ContractDecl,
+    EnvHandler,
+};
+
+state! {
+    /// The simplest contract that can still be deployed and called.
+    struct Noop {}
+}
+
+messages! {
+    0 => DoNothing();
+}
+
+impl Noop {
+    pub fn deploy(&mut self, env: &mut EnvHandler) {}
+    pub fn do_nothing(&self, env: &EnvHandler) {}
+}
+
+#[rustfmt::skip]
+fn instantiate() -> impl Contract {
+	ContractDecl::using::<Noop>()
+		.on_deploy(|env, ()| {
+            let (handler, state) = env.split_mut();
+            state.deploy(handler)
+        })
+        .on_msg::<DoNothing>(|env, ()| {
+            let (handler, state) = env.split();
+            state.do_nothing(handler)
+        })
+		.instantiate()
+}
