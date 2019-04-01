7c649563bab9e24cc3544950b072d250bf0f2a65
Sergey Pepyakin <sergei@parity.io>
2019-04-01 18:56:06 +0200

    [chores] Remove +nightly qualifier bc/ of rust-toolchain

--- examples/core/subpeep/build.sh
+++ examples/core/subpeep/build.sh
@@ -8,7 +8,7 @@
 PROJNAME=subpeep
 #cargo clean
 #rm Cargo.lock
-CARGO_INCREMENTAL=0 cargo +nightly build --release --target=wasm32-unknown-unknown --verbose
+CARGO_INCREMENTAL=0 cargo build --release --target=wasm32-unknown-unknown --verbose
 wasm2wat -o target/$PROJNAME.wat target/wasm32-unknown-unknown/release/$PROJNAME.wasm
 cat target/$PROJNAME.wat | sed "s/(import \"env\" \"memory\" (memory (;0;) 2))/(import \"env\" \"memory\" (memory (;0;) 2 16))/" > target/$PROJNAME-fixed.wat
 wat2wasm -o target/$PROJNAME.wasm target/$PROJNAME-fixed.wat
