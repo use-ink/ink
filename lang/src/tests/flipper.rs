6af4c161fc8a358d83c5f6a6da190bb5eb5134bd
Robin Freyler <robin.freyler@gmail.com>
2019-04-16 14:34:43 +0200

    [lang] Add flipper contract test case

---
+++ lang/src/tests/flipper.rs
@@ -0,0 +1,109 @@
+// Copyright 2018-2019 Parity Technologies (UK) Ltd.
+// This file is part of pDSL.
+//
+// pDSL is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// pDSL is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with pDSL.  If not, see <http://www.gnu.org/licenses/>.
+
+use super::*;
+
+#[test]
+fn flipper_contract() {
+    assert_eq_tokenstreams(
+        quote! {
+            /// A simple contract that has a boolean value that can be flipped and be retured.
+            struct Flipper {
+                /// The internal value.
+                value: storage::Value<bool>,
+            }
+
+            impl Deploy for Flipper {
+                /// The internal boolean is initialized with `true`.
+                fn deploy(&mut self) {
+                    self.value.set(true)
+                }
+            }
+
+            impl Flipper {
+                /// Flips the internal boolean.
+                pub(external) fn flip(&mut self) {
+                    self.value = !(*self.value)
+                }
+
+                /// Returns the internal boolean.
+                pub(external) fn get(&self) -> bool {
+                    *self.value
+                }
+            }
+        },
+        quote! {
+            pdsl_model::state! {
+                /// A simple contract that has a boolean value that can be flipped and be retured.
+                pub struct Flipper {
+                    /// The internal value.
+                    value: storage::Value<bool>,
+                }
+            }
+
+            use pdsl_model::messages;
+
+            pdsl_model::messages! {
+                /// Flips the internal boolean.
+                970692492 => Flip();
+                /// Returns the internal boolean.
+                4266279973 => Get() -> bool;
+            }
+
+            impl Flipper {
+                /// The internal boolean is initialized with `true`.
+                pub fn deploy(&mut self, env: &mut pdsl_model::EnvHandler) {
+                    self.value.set(true)
+                }
+
+                /// Flips the internal boolean.
+                pub fn flip(&mut self, env: &mut pdsl_model::EnvHandler) {
+                    self.value = !(*self.value)
+                }
+
+                /// Returns the internal boolean.
+                pub fn get(&self, env: &pdsl_model::EnvHandler) -> bool {
+                    *self.value
+                }
+            }
+
+            use pdsl_model::Contract as _;
+
+            #[cfg(not(test))]
+            impl Flipper {
+                pub(crate) fn instantiate() -> impl pdsl_model::Contract {
+                    pdsl_model::ContractDecl::using::<Self>()
+                        .on_deploy(|env, ()| {
+                            let (handler, state) = env.split_mut();
+                            state.deploy(handler,)
+                        })
+                        .on_msg_mut::<Flip>(|env, _| {
+                            let (handler, state) = env.split_mut();
+                            state.flip(handler,)
+                        })
+                        .on_msg::<Get>(|env, _| {
+                            let (handler, state) = env.split();
+                            state.get(handler,)
+                        })
+                        .instantiate()
+                }
+            }
+
+            #[cfg(not(test))] #[no_mangle] fn deploy() { Flipper::instantiate().deploy() }
+            #[cfg(not(test))] #[no_mangle] fn call() { Flipper::instantiate().dispatch() }
+        },
+    )
+}
