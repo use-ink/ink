615afbcc54635c2eeda9b56860bffef767dd7f3c
Andrew Jones <ascjones@gmail.com>
2019-04-17 12:06:08 +0100

    WIP: attempt to get tests to compile

--- core/src/env/srml/types.rs
+++ core/src/env/srml/types.rs
@@ -26,20 +26,6 @@ impl EnvTypes for DefaultSrmlTypes {
 }
 
 pub type Address = node_runtime::Address;
-//
-///// The default SRML address type.
-//#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Encode, Decode)]
-//pub struct Address([u8; 32]);
-//
-//impl<'a> From<&'a [u8]> for Address {
-//    fn from(bytes: &'a [u8]) -> Self {
-//        assert_eq!(bytes.len(), 32);
-//        let mut array = [0; 32];
-//        let bytes = &bytes[..array.len()]; // panics if not enough data
-//        array.copy_from_slice(bytes);
-//        Address(array)
-//    }
-//}
-//
+
 /// The default SRML balance type.
 pub type Balance = u64;
