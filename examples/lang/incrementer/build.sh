aac0c63aa641a3909c4050e618f8aaf1ac9f1449
Robin Freyler <robin.freyler@gmail.com>
2019-04-18 15:21:24 +0200

    [examples/lang] Erc20 and Incrementer contracts now generate API description by default

--- examples/lang/incrementer/build.sh
+++ examples/lang/incrementer/build.sh
@@ -3,7 +3,7 @@
 PROJNAME=incrementer
 
 CARGO_INCREMENTAL=0 &&
-cargo +nightly build --release --target=wasm32-unknown-unknown --verbose &&
+cargo +nightly build --release --features generate-api-description --target=wasm32-unknown-unknown --verbose &&
 wasm2wat -o target/$PROJNAME.wat target/wasm32-unknown-unknown/release/$PROJNAME.wasm &&
 cat target/$PROJNAME.wat | sed "s/(import \"env\" \"memory\" (memory (;0;) 2))/(import \"env\" \"memory\" (memory (;0;) 2 16))/" > target/$PROJNAME-fixed.wat &&
 wat2wasm -o target/$PROJNAME.wasm target/$PROJNAME-fixed.wat &&
