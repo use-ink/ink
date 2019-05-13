29edad8972aa6b484aac69a0635a26d799b79849
Andrew Jones <ascjones@gmail.com>
2019-05-13 17:30:52 +0100

    Refactor build.rs

--- cli/build.rs
+++ cli/build.rs
@@ -1,56 +1,50 @@
-extern crate zip;
-extern crate walkdir;
-
 use std::io::prelude::*;
-use std::io::{Write, Seek};
+use std::io::Write;
 use std::iter::Iterator;
+use std::error::Error;
+use std::result::Result;
 use zip::write::FileOptions;
 use zip::result::ZipError;
 
-use walkdir::{WalkDir, DirEntry};
+use walkdir::WalkDir;
 use std::path::Path;
 use std::fs::File;
 
+const DEFAULT_UNIX_PERMISSIONS: u32 = 0o755;
+
 fn main() {
     let src_dir = "./template";
-    let out_dir = std::env::var("OUT_DIR").unwrap();
+    let out_dir = std::env::var("OUT_DIR").expect("OUT_DIR should be set by cargo");
     let dst_file = format!("{}/template.zip", out_dir); // todo: [AJ] proper path concat
 
-    match doit(src_dir, &dst_file, zip::CompressionMethod::Stored) {
+    match zip_dir(src_dir, &dst_file, zip::CompressionMethod::Stored) {
         Ok(_) => println!("done: {} written to {}", src_dir, dst_file),
         Err(e) => eprintln!("Error: {:?}", e),
     };
 }
 
-fn doit(src_dir: &str, dst_file: &str, method: zip::CompressionMethod) -> zip::result::ZipResult<()> {
+fn zip_dir(src_dir: &str, dst_file: &str, method: zip::CompressionMethod) -> Result<(), Box<Error>> {
     if !Path::new(src_dir).is_dir() {
-        return Err(ZipError::FileNotFound);
+        return Err(ZipError::FileNotFound.into());
     }
 
     let path = Path::new(dst_file);
-    let file = File::create(&path).unwrap();
+    let file = File::create(&path)?;
 
     let walkdir = WalkDir::new(src_dir.to_string());
-    let it = walkdir.into_iter();
+    let it = walkdir
+        .into_iter()
+        .filter_map(|e| e.ok());
 
-    zip_dir(&mut it.filter_map(|e| e.ok()), src_dir, file, method)?;
-
-    Ok(())
-}
-
-fn zip_dir<T>(it: &mut Iterator<Item=DirEntry>, prefix: &str, writer: T, method: zip::CompressionMethod)
-              -> zip::result::ZipResult<()>
-    where T: Write+Seek
-{
-    let mut zip = zip::ZipWriter::new(writer);
+    let mut zip = zip::ZipWriter::new(file);
     let options = FileOptions::default()
         .compression_method(method)
-        .unix_permissions(0o755);
+        .unix_permissions(DEFAULT_UNIX_PERMISSIONS);
 
     let mut buffer = Vec::new();
     for entry in it {
         let path = entry.path();
-        let name = path.strip_prefix(Path::new(prefix)).unwrap();
+        let name = path.strip_prefix(Path::new(src_dir))?;
 
         if path.is_file() {
             println!("adding file {:?} as {:?} ...", path, name);
@@ -66,5 +60,6 @@ fn zip_dir<T>(it: &mut Iterator<Item=DirEntry>, prefix: &str, writer: T, method:
         }
     }
     zip.finish()?;
-    Result::Ok(())
+
+    Ok(())
 }
