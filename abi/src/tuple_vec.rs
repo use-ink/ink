066a32b15914aa29c106de72cddf7e0ac0516637
Robin Freyler <robin.freyler@gmail.com>
2019-05-30 10:36:14 +0200

    [abi] Add missing license headers

--- abi/src/tuple_vec.rs
+++ abi/src/tuple_vec.rs
@@ -1,4 +1,18 @@
-
+// Copyright 2018-2019 Parity Technologies (UK) Ltd.
+// This file is part of ink!.
+//
+// ink! is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// ink! is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with ink!.  If not, see <http://www.gnu.org/licenses/>.
 
 /// Type state for an empty type list.
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
