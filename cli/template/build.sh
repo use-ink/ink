2e308e5bce6b50fc3c52ce25fef20ded284ee786
Shawn Tabrizi <shawntabrizi@gmail.com>
2019-05-31 12:17:49 +0200

    [chores] Remove `wasm-opt` from build script

--- cli/template/build.sh
+++ cli/template/build.sh
@@ -11,5 +11,4 @@ cargo build --release --features generate-api-description --target=wasm32-unknow
 wasm2wat -o target/$PROJNAME.wat target/wasm32-unknown-unknown/release/$PROJNAME.wasm
 cat target/$PROJNAME.wat | sed "s/(import \"env\" \"memory\" (memory (;0;) 2))/(import \"env\" \"memory\" (memory (;0;) 2 16))/" > target/$PROJNAME-fixed.wat
 wat2wasm -o target/$PROJNAME.wasm target/$PROJNAME-fixed.wat
-wasm-opt -Oz target/$PROJNAME.wasm -o target/$PROJNAME-opt.wasm
-wasm-prune --exports call,deploy target/$PROJNAME-opt.wasm target/$PROJNAME-pruned.wasm
+wasm-prune --exports call,deploy target/$PROJNAME.wasm target/$PROJNAME-pruned.wasm
