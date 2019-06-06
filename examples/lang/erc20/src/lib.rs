b044e74ad2243beaecb7383ed0e41294199b6f81
Andrew Jones <ascjones@gmail.com>
2019-06-06 18:56:13 +0100

    [lang, examples] Convert erc20 example to use new ink-types-node-runtime

--- examples/lang/erc20/src/lib.rs
+++ examples/lang/erc20/src/lib.rs
@@ -20,7 +20,6 @@ use ink_core::{
     memory::format,
     storage,
 };
-use ink_core::env::ContractEnv;
 use ink_model::EnvHandler;
 use ink_lang::contract;
 use ink_types_node_runtime::NodeRuntimeTypes;
@@ -130,7 +129,7 @@ contract! {
         }
 
         /// Transfers token from a specified AccountId to another AccountId.
-        fn transfer_impl(&mut self, env: &mut EnvHandler<ContractEnv<NodeRuntimeTypes>>, from: AccountId, to: AccountId, value: Balance) -> bool {
+        fn transfer_impl(&mut self, env: &mut EnvHandler<ink_core::env::ContractEnv<NodeRuntimeTypes>>, from: AccountId, to: AccountId, value: Balance) -> bool {
             let balance_from = self.balance_of_or_zero(&from);
             let balance_to = self.balance_of_or_zero(&to);
             if balance_from < value {
