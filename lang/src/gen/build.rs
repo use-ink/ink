53bb02bc364fc5f413fc8f2429b2feeedaad6b08
Andrew Jones <ascjones@gmail.com>
2019-06-07 12:33:11 +0100

    [lang] Add super import to types mod

--- lang/src/gen/build.rs
+++ lang/src/gen/build.rs
@@ -53,6 +53,7 @@ fn codegen_for_contract_env(tokens: &mut TokenStream2, contract: &hir::Contract)
     let env_types = &contract.env_types_type;
     tokens.extend(quote! {
         mod types {
+            use super::*;
             use ink_core::env::{ContractEnv, EnvTypes};
 
             pub type AccountId = <ContractEnv<#env_types> as EnvTypes>::AccountId;
