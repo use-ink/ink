d3835330f0df2101adfebaf207bd12d6d699a7ae
Andrew Jones <ascjones@gmail.com>
2019-07-15 12:34:56 +0100

    [memory] add no_std cf_attr and alloc crate

--- memory/src/lib.rs
+++ memory/src/lib.rs
@@ -18,6 +18,11 @@
 //!
 //! These definitions are useful since we are operating in a `no_std` environment.
 
+#![cfg_attr(not(feature = "std"), no_std)]
+
+#[cfg(not(feature = "std"))]
+extern crate alloc;
+
 #[cfg(not(feature = "std"))]
 mod no_std_defs {
     pub use alloc::{
