6389409c6e9de7c233babb6c1a7b1191cd30bce3
Robin Freyler <robin.freyler@gmail.com>
2019-07-24 15:37:25 +0200

    adjust template and lang example std feature and propagation

--- cli/template/build.sh
+++ cli/template/build.sh
@@ -7,7 +7,7 @@ PROJNAME={{name}}
 # rm Cargo.lock
 
 CARGO_INCREMENTAL=0 &&
-cargo build --release --features generate-api-description --target=wasm32-unknown-unknown --verbose
+cargo build --no-default-features --release --features generate-api-description --target=wasm32-unknown-unknown --verbose
 wasm2wat -o target/$PROJNAME.wat target/wasm32-unknown-unknown/release/$PROJNAME.wasm
 cat target/$PROJNAME.wat | sed "s/(import \"env\" \"memory\" (memory (;0;) 2))/(import \"env\" \"memory\" (memory (;0;) 2 16))/" > target/$PROJNAME-fixed.wat
 wat2wasm -o target/$PROJNAME.wasm target/$PROJNAME-fixed.wat
