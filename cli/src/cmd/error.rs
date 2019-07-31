e03f5eec9746fb071386e54062ff566b6d836bca
Andrew Jones <ascjones@gmail.com>
2019-07-31 17:46:29 +0100

    [cli] reject creating new contract with hyphenated name

--- cli/src/cmd/error.rs
+++ cli/src/cmd/error.rs
@@ -27,6 +27,7 @@ pub enum CommandErrorKind {
     UnimplementedCommand,
     UnimplementedAbstractionLayer,
     ZipError(ZipError),
+    Other(String),
 }
 
 /// An error that can be encountered while executing commands.
@@ -47,6 +48,14 @@ impl From<ZipError> for CommandError {
     fn from(error: ZipError) -> Self {
         Self {
             kind: CommandErrorKind::ZipError(error),
+        }
+    }
+}
+
+impl From<&str> for CommandError {
+    fn from(error: &str) -> Self {
+        Self {
+            kind: CommandErrorKind::Other(error.to_string()),
         }
     }
 }
