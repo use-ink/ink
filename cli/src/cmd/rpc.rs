4ca025224083fe2acd310250040ced6c8f57991d
Andrew Jones <ascjones@gmail.com>
2019-08-02 10:53:55 +0100

    Combine all rpc interfaces

--- cli/src/cmd/rpc.rs
+++ cli/src/cmd/rpc.rs
@@ -127,18 +127,23 @@ pub struct ExtrinsicSuccess {
     pub events: Vec<Event>,
 }
 
-struct Query {
+struct Rpc {
     state: StateClient<Hash>,
     chain: ChainClient<BlockNumber, Hash, (), SignedBlock>,
+    author: AuthorClient<Hash, Hash>,
 }
 
-impl Query {
-    fn connect_ws(url: &Url) -> impl Future<Item = Query, Error = RpcError> {
-        ws_connect(url)
-            .join(ws_connect(url))
-            .map(|(state, chain)| Query { state, chain })
+impl From<RpcChannel> for Rpc {
+    fn from(channel: RpcChannel) -> Rpc {
+        Rpc {
+            state: channel.clone().into(),
+            chain: channel.clone().into(),
+            author: channel.into(),
+        }
     }
+}
 
+impl Rpc {
     fn fetch_nonce(
         &self,
         account: &AccountId,
@@ -160,13 +165,6 @@ impl Query {
         self.chain.block_hash(Some(NumberOrHex::Number(0)))
     }
 
-    fn fetch_block(
-        &self,
-        block_hash: H256,
-    ) -> impl Future<Item = Option<SignedBlock>, Error = RpcError> {
-        self.chain.block(Some(block_hash))
-    }
-
     fn subscribe_events(
         &self,
     ) -> impl Future<Item = TypedSubscriptionStream<StorageChangeSet<H256>>, Error = RpcError>
@@ -178,16 +176,6 @@ impl Query {
         self.state
             .subscribe_storage(Some(vec![StorageKey(storage_key.to_vec())]))
     }
-}
-
-struct Author {
-    cli: AuthorClient<Hash, Hash>,
-}
-
-impl Author {
-    fn connect_ws(url: &Url) -> impl Future<Item = Author, Error = RpcError> {
-        ws_connect(url).map(|cli| Author { cli })
-    }
 
     /// Submit an extrinsic, waiting for it to be finalized.
     /// If successful, returns the block hash.
@@ -195,7 +183,7 @@ impl Author {
         self,
         extrinsic: UncheckedExtrinsic,
     ) -> impl Future<Item = H256, Error = CommandError> {
-        self.cli
+        self.author
             .watch_extrinsic(extrinsic.encode().into())
             .map_err(Into::into)
             .and_then(|stream| {
@@ -219,14 +207,55 @@ impl Author {
                     })
             })
     }
-}
 
-/// Wraps websockets connect, ensuring a valid url
-fn ws_connect<T>(url: &Url) -> impl Future<Item = T, Error = RpcError>
-    where
-        T: From<RpcChannel>,
-{
-    ws::connect(url.as_str()).expect("Url is a valid url; qed")
+    /// Create and submit an extrinsic and return corresponding Event if successful
+    fn create_and_submit_extrinsic(
+        self,
+        signer: Pair,
+        call: Call,
+    ) -> impl Future<Item = ExtrinsicSuccess, Error = CommandError> {
+        let account_nonce = self.fetch_nonce(&signer.public()).map_err(Into::into);
+        let genesis_hash =
+            self
+                .fetch_genesis_hash()
+                .map_err(Into::into)
+                .and_then(|genesis_hash| {
+                    future::result(genesis_hash.ok_or("Genesis hash not found".into()))
+                });
+        let events = self.subscribe_events().map_err(Into::into);
+
+        account_nonce.join3(genesis_hash, events).and_then(
+            move |(index, genesis_hash, events)| {
+                let extrinsic = create_extrinsic(index, call, genesis_hash, &signer);
+                let ext_hash =
+                    H256(extrinsic.using_encoded(|encoded| blake2_256(encoded)));
+                log::info!("Submitting Extrinsic `{:?}`", ext_hash);
+
+                let chain = self.chain.clone();
+                self.submit_and_watch(extrinsic)
+                    .and_then(move |bh| {
+                        log::info!("Fetching block {:?}", bh);
+                        chain
+                            .block(Some(bh))
+                            .map(move |b| (bh, b))
+                            .map_err(Into::into)
+                    })
+                    .and_then(|(h, b)| {
+                        b.ok_or(format!("Failed to find block '{:#x}'", h).into())
+                            .map(|b| (h, b))
+                            .into_future()
+                    })
+                    .and_then(move |(bh, sb)| {
+                        log::info!(
+                            "Found block {:?}, with {} extrinsics",
+                            bh,
+                            sb.block.extrinsics.len()
+                        );
+                        extract_events(ext_hash, &sb, bh, events)
+                    })
+            },
+        )
+    }
 }
 
 fn create_extrinsic(
@@ -330,60 +359,13 @@ fn extract_events(
         })
 }
 
-/// Create and submit an extrinsic and return corresponding Event if successful
-fn create_and_submit_extrinsic(
-    author: Author,
-    query: Query,
-    signer: Pair,
-    call: Call,
-) -> impl Future<Item = ExtrinsicSuccess, Error = CommandError> {
-    let account_nonce = query.fetch_nonce(&signer.public()).map_err(Into::into);
-    let genesis_hash =
-        query
-            .fetch_genesis_hash()
-            .map_err(Into::into)
-            .and_then(|genesis_hash| {
-                future::result(genesis_hash.ok_or("Genesis hash not found".into()))
-            });
-    let events = query.subscribe_events().map_err(Into::into);
-
-    account_nonce.join3(genesis_hash, events).and_then(
-        move |(index, genesis_hash, events)| {
-            let extrinsic = create_extrinsic(index, call, genesis_hash, &signer);
-            let ext_hash =
-                H256(extrinsic.using_encoded(|encoded| blake2_256(encoded)));
-            log::info!("Submitting Extrinsic `{:?}`", ext_hash);
-
-            author.submit_and_watch(extrinsic)
-                .and_then(move |bh| {
-                    log::info!("Fetching block {:?}", bh);
-                    query
-                        .fetch_block(bh)
-                        .map(move |b| (bh, b))
-                        .map_err(Into::into)
-                })
-                .and_then(|(h, b)| {
-                    b.ok_or(format!("Failed to find block '{:#x}'", h).into())
-                        .map(|b| (h, b))
-                        .into_future()
-                })
-                .and_then(move |(bh, sb)| {
-                    log::info!(
-                            "Found block {:?}, with {} extrinsics",
-                            bh,
-                            sb.block.extrinsics.len()
-                        );
-                    extract_events(ext_hash, &sb, bh, events)
-                })
-        },
-    )
-}
-
 pub fn submit(url: &Url, signer: Pair, call: Call) -> Result<ExtrinsicSuccess> {
-    let submit = Query::connect_ws(url)
-        .join(Author::connect_ws(url))
+    let submit = ws::connect(url.as_str())
+        .expect("Url is a valid url; qed")
         .map_err(Into::into)
-        .and_then(|(query, author)| create_and_submit_extrinsic(author, query, signer, call));
+        .and_then(|rpc: Rpc| {
+            rpc.create_and_submit_extrinsic(signer, call)
+        });
 
     let mut rt = tokio::runtime::Runtime::new()?;
     rt.block_on(submit)
