f4c306a11667cfefcfb9b9ef79804e4487434dbd
Robin Freyler <robin.freyler@gmail.com>
2019-08-06 21:36:23 +0200

    [cli] adjust template for ink-abi update

--- cli/template/.tools/abi_gen/main.rs
+++ cli/template/.tools/abi_gen/main.rs
@@ -1,15 +1,8 @@
 
-
-fn main() {
-    let abi = contract::generate_abi();
-    let contents = serde_json::to_string(&abi)
-        .expect("Failed at converting contract ABI to JSON");
-    let mut path_buf = String::from("target/");
-    path_buf.push_str(description.name());
-    path_buf.push_str(".json");
-    std::fs::create_dir("target")
-		.expect("Failed to create target directory");
-    std::fs::write(path_buf, contents)
-        .expect("Failed at writing JSON ABI to file");
+fn main() -> Result<(), std::io::Error> {
+    let abi = contract::ink_generate_abi();
+    let contents = serde_json::to_string_pretty(&abi)?;
+    std::fs::create_dir("target").ok();
+    std::fs::write("target/abi.json", contents)?;
     Ok(())
 }
