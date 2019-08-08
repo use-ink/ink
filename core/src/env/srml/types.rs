5ec8636c926f5b7a080d08533097b6c1abf25bd5
Andrew Jones <ascjones@gmail.com>
2019-08-08 13:37:51 +0100

    [core] fix dummy Call Decode impl

--- core/src/env/srml/types.rs
+++ core/src/env/srml/types.rs
@@ -51,8 +51,8 @@ impl scale::Encode for Call {}
 /// None, but this should never be called.
 #[cfg(feature = "test-env")]
 impl scale::Decode for Call {
-    fn decode<I: scale::Input>(_value: &mut I) -> Option<Self> {
-        None
+    fn decode<I: scale::Input>(_value: &mut I) -> Result<Self, scale::Error> {
+        Err("Call cannot be instantiated".into())
     }
 }
 
