b286b242719bc2aa2972d29d549feb2929faad12
Robin Freyler <robin.freyler@gmail.com>
2019-08-20 15:29:19 +0200

    [examples] add example to call remote smart contracts

---
+++ examples/lang/delegator/src/lib.rs
@@ -0,0 +1,122 @@
+// Copyright 2018-2019 Parity Technologies (UK) Ltd.
+// This file is part of ink!.
+//
+// ink! is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// ink! is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with ink!.  If not, see <http://www.gnu.org/licenses/>.
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use ink_core::{
+    memory::format,
+    storage,
+};
+use ink_lang::contract;
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, scale::Encode, scale::Decode)]
+#[cfg_attr(feature = "ink-generate-abi", derive(Metadata))]
+pub enum Which {
+    Adder,
+    Subber,
+}
+
+impl ink_core::storage::Flush for Which {
+    fn flush(&mut self) {}
+}
+
+fn adder_code_hash() -> Hash {
+    Default::default()
+}
+
+fn subber_code_hash() -> Hash {
+    Default::default()
+}
+
+fn accumulator_code_hash() -> Hash {
+    Default::default()
+}
+
+contract! {
+    #![env = ink_core::env::DefaultSrmlTypes]
+
+    /// Delegates calls to an adder or subber contract to mutate
+    /// a value in an accumulator contract.
+    ///
+    /// In order to deploy the delegator smart contract we first
+    /// have to manually put the code of the accumulator, adder
+    /// and subber smart contracts, receive their code hashes from
+    /// the signalled events and put their code hash into our
+    /// delegator smart contract.
+    struct Delegator {
+        /// Says which of adder or subber is currently in use.
+        which: storage::Value<Which>,
+        /// The accumulator smart contract.
+        accumulator: storage::Value<accumulator::Accumulator>,
+        /// The adder smart contract.
+        adder: storage::Value<adder::Adder>,
+        /// The subber smart contract.
+        subber: storage::Value<subber::Subber>,
+    }
+
+    impl Deploy for Delegator {
+        /// Initializes the value to the initial value.
+        fn deploy(&mut self, init_value: u32) {
+            self.which.set(Which::Adder);
+            self.accumulator.set(accumulator::Accumulator::new(accumulator_code_hash(), 100)
+                .create()
+                .expect("failed at instantiating the accumulator contract"));
+            self.adder.set(adder::Adder::new(adder_code_hash(), (*self.accumulator).clone())
+                .create()
+                .expect("failed at instantiating the adder contract"));
+            self.subber.set(subber::Subber::new(subber_code_hash(), (*self.accumulator).clone())
+                .create()
+                .expect("failed at instantiating the subber contract"));
+        }
+    }
+
+    impl Delegator {
+        /// Delegates the call.
+        pub(external) fn delegate(&mut self, by: i32) {
+            match &*self.which {
+                Which::Adder => self.adder.inc(by),
+                Which::Subber => self.subber.dec(by),
+            }
+        }
+
+        /// Switches the delegator.
+        pub(external) fn switch(&mut self) {
+            match *self.which {
+                Which::Adder => {
+                    *self.which = Which::Subber;
+                }
+                Which::Subber => {
+                    *self.which = Which::Adder;
+                }
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Incrementer;
+
+    #[test]
+    fn it_works() {
+        let mut incrementer = Incrementer::deploy_mock(5);
+        assert_eq!(incrementer.get(), 5);
+        incrementer.inc(42);
+        assert_eq!(incrementer.get(), 47);
+        incrementer.inc(0);
+        assert_eq!(incrementer.get(), 47);
+    }
+}
