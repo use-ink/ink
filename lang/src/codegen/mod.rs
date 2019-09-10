8221b881c0474205c5b89e2102088e1d031ab22c
Robin Freyler <robin.freyler@gmail.com>
2019-09-10 15:47:08 +0200

    [lang] implement initial bits of the new codegen

--- lang/src/codegen/mod.rs
+++ lang/src/codegen/mod.rs
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with ink!.  If not, see <http://www.gnu.org/licenses/>.
 
+mod model;
+
 use crate::ir::Contract;
 use proc_macro2::TokenStream as TokenStream2;
 use quote::quote;
--- lang/src/codegen/model.rs
+++ lang/src/codegen/model.rs
@@ -18,17 +18,81 @@ use crate::{
     ir::Contract,
     codegen::GenerateCode,
 };
-use quote::quote;
+use quote::{
+    quote,
+    quote_spanned,
+};
 use proc_macro2::TokenStream as TokenStream2;
 
 /// Generates code for the `ink_model` parts that dispatch constructors
 /// and messages from the input and also handle the returning of data.
 pub struct Model<'a> {
+    /// The contract to generate code for.
     contract: &'a Contract,
 }
 
 impl GenerateCode for Model<'_> {
     fn generate_code(&self) -> TokenStream2 {
         quote! {}
+    }
+}
+
+/// Generates code for the environmental types used by a contract.
+pub struct MetaTypes<'a> {
+    /// The contract to generate code for.
+    contract: &'a Contract,
+}
+
+impl GenerateCode for MetaTypes<'_> {
+    fn generate_code(&self) -> TokenStream2 {
+        let env_types = &self.contract.meta_info.env_types.ty;
+
+        quote! {
+            mod env_types {
+                use super::*;
+                use ink_core::env::{ContractEnv, EnvTypes};
+
+                pub type AccountId = <#env_types as EnvTypes>::AccountId;
+                pub type Balance = <#env_types as EnvTypes>::Balance;
+                pub type Hash = <#env_types as EnvTypes>::Hash;
+                pub type Moment = <#env_types as EnvTypes>::Moment;
+                pub type BlockNumber = <#env_types as EnvTypes>::BlockNumber;
+            }
+
+            type Env = ink_core::env::ContractEnv<#env_types>;
+            use env_types::{
+                AccountId,
+                Balance,
+                Hash,
+                Moment,
+                BlockNumber,
+            };
+        }
+    }
+}
+
+/// Generates code for the entry points of a contract.
+pub struct EntryPoints<'a> {
+    /// The contract to generate code for.
+    contract: &'a Contract,
+}
+
+impl GenerateCode for EntryPoints<'_> {
+    fn generate_code(&self) -> TokenStream2 {
+        let ident = &self.contract.ident;
+
+        quote_spanned! { ident.span() =>
+            #[cfg(not(test))]
+            #[no_mangle]
+            fn deploy() -> u32 {
+                #ident::instantiate().deploy().to_u32()
+            }
+
+            #[cfg(not(test))]
+            #[no_mangle]
+            fn call() -> u32 {
+                #ident::instantiate().dispatch().to_u32()
+            }
+        }
     }
 }
