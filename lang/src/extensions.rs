3e113fc65fd6d9bf44cdf9d812ed2770852e5e1d
Robin Freyler <robin.freyler@gmail.com>
2019-09-10 15:16:20 +0200

    [lang] add some doc comments

--- lang/src/extensions.rs
+++ lang/src/extensions.rs
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with ink!.  If not, see <http://www.gnu.org/licenses/>.
 
+//! Module to introduce extended functionality to types from other crates.
+
 use proc_macro2::{
     Ident,
     Span,
