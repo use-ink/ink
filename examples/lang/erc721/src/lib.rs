5cf74884e73e3462acb7feaf8409e2368fe7cd6f
Ricardo Rius <ricardo@parity.io>
2019-10-14 10:40:55 +0200

    Change Result type

--- examples/lang/erc721/src/lib.rs
+++ examples/lang/erc721/src/lib.rs
@@ -21,11 +21,11 @@ use ink_core::{
     memory::format,
     storage,
 };
+pub use core::result::Result;
 use ink_lang::contract;
 use ink_model;
 
 pub type EnvHandler = ink_model::EnvHandler<ink_core::env::ContractEnv<DefaultSrmlTypes>>;
-pub use core::result::Result;
 pub type TokenId = u32;
 pub type Counter = u32;
 
