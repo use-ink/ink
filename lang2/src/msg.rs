3b799ae8ea133ad41e9e0040a24a1612fb04ec01
Robin Freyler <robin.freyler@gmail.com>
2019-10-21 16:29:32 +0200

    [lang2] fix unused warning

--- lang2/src/msg.rs
+++ lang2/src/msg.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with ink!.  If not, see <http://www.gnu.org/licenses/>.
 
-use core::marker::PhantomData;
 use ink_core::env2::call::Selector;
 
 /// Dispatchable functions that have inputs.
