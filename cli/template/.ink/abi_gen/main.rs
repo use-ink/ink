e694c9ec8b72f14e1f724239e85330e2c3125dd5
Robin Freyler <robin.freyler@gmail.com>
2019-11-02 09:22:56 +0100

    [cli] adjust CLI to generate ink_lang2 contract upon new command

--- cli/template/.ink/abi_gen/main.rs
+++ cli/template/.ink/abi_gen/main.rs
@@ -1,6 +1,5 @@
-
 fn main() -> Result<(), std::io::Error> {
-    let abi = contract::ink_generate_abi();
+    let abi = <contract::{{camel_name}} as ink_lang2::GenerateAbi>::generate_abi();
     let contents = serde_json::to_string_pretty(&abi)?;
     std::fs::create_dir("target").ok();
     std::fs::write("target/abi.json", contents)?;
