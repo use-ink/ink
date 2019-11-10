e82d9a0db33effc2525d77342f0b404e442f3c1d
Robin Freyler <robin.freyler@gmail.com>
2019-11-10 22:39:03 +0100

    [examples/lang] remove all lang 1.0 examples

--- examples/lang/delegator/build-all.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-pushd accumulator && cargo contract build && popd &&
-pushd adder && cargo contract build && popd &&
-pushd subber && cargo contract build && popd &&
-cargo contract build
