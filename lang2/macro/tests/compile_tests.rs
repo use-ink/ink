273b68c1402fd131bb7dd538b4e6ac47ea701401
Robin Freyler <robin.freyler@gmail.com>
2019-11-11 00:20:34 +0100

    [lang2/macro] add failure UI test for #[ink(event)] provided with another #[ink(storage)]

--- lang2/macro/tests/compile_tests.rs
+++ lang2/macro/tests/compile_tests.rs
@@ -41,4 +41,5 @@ fn compile_tests() {
     t.compile_fail("tests/ui/fail/15-multiple-storage-structs.rs");
     t.compile_fail("tests/ui/fail/16-storage-impl-ident-conflict.rs");
     t.compile_fail("tests/ui/fail/17-conflicting-ink-markers.rs");
+    t.compile_fail("tests/ui/fail/18-conflicting-ink-markers-2.rs");
 }
