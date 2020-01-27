062a4f32dafffd761a7a3c18f80bd6272a9b986d
Robin Freyler <robin.freyler@gmail.com>
2020-01-27 22:13:37 +0100

    [core] env3: add comment describing potential future extensions to SimpleArithmetic trait

--- core/src/env3/types.rs
+++ core/src/env3/types.rs
@@ -72,6 +72,38 @@ pub trait SimpleArithmetic:
     + MulAssign<Self>
     + Div<Self>
     + DivAssign<Self>
+    // Further trait bounds from the original SimpleArithmetic trait
+    // that we could use to extend ink!'s SimpleArithmetic trait.
+    //
+    // From<u8> +
+    // From<u16> +
+    // From<u32> +
+    // TryFrom<u64> +
+    // TryFrom<u128> +
+    // TryFrom<usize> +
+    // TryInto<u8> +
+    // TryInto<u16> +
+    // TryInto<u32> +
+    // TryInto<u64> +
+    // TryInto<u128> +
+    // TryInto<usize> +
+    // UniqueSaturatedInto<u8> +
+    // UniqueSaturatedInto<u16> +
+    // UniqueSaturatedInto<u32> +
+    // UniqueSaturatedInto<u64> +
+    // UniqueSaturatedInto<u128> +
+    // UniqueSaturatedFrom<u64> +
+    // UniqueSaturatedFrom<u128> +
+    // Shl<u32, Output = Self> +
+    // Shr<u32, Output = Self> +
+    // CheckedAdd +
+    // CheckedSub +
+    // CheckedMul +
+    // CheckedDiv +
+    // CheckedShl +
+    // CheckedShr +
+    // IntegerSquareRoot +
+    // Saturating +
 {
 }
 
