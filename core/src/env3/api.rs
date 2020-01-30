13a7d86a5d09202958226159f4ed9c23d7612511
Hero Bird <robin.freyler@gmail.com>
2020-01-30 20:59:34 +0100

    remove "If given too few endowment" from docs
    Co-Authored-By: Andrew Jones <ascjones@gmail.com>

--- core/src/env3/api.rs
+++ core/src/env3/api.rs
@@ -264,7 +264,6 @@ where
 /// - If arguments passed to the called contract message are invalid.
 /// - If the called contract execution has trapped.
 /// - If the called contract ran out of gas upon execution.
-/// - If given too few endowment.
 pub fn invoke_contract<T>(params: &CallParams<T, ()>) -> Result<()>
 where
     T: EnvTypes,
