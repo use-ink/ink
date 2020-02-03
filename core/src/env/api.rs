aef279b253f3c6a7604ea87c49a301dfea3d2145
Hero Bird <robin.freyler@gmail.com>
2020-02-03 13:46:32 +0100

    rename create_contract -> instantiate_contract
    Co-Authored-By: Andrew Jones <ascjones@gmail.com>

--- core/src/env/api.rs
+++ core/src/env/api.rs
@@ -306,7 +306,7 @@ where
 /// - If the instantiation process runs out of gas.
 /// - If given too few endowment.
 /// - If the returned account ID failed to decode properly.
-pub fn create_contract<T, C>(params: &InstantiateParams<T, C>) -> Result<T::AccountId>
+pub fn instantiate_contract<T, C>(params: &InstantiateParams<T, C>) -> Result<T::AccountId>
 where
     T: EnvTypes,
 {
