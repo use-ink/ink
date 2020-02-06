aca7181b05545fd441d750e55bc1a1f2a6a6cc14
devops-parity <devops-team@parity.io>
2020-02-06 13:48:08 +0000

    Update docs to 318

--- examples/delegator/build-all.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-pushd accumulator && cargo contract build && popd &&
-pushd adder && cargo contract build && popd &&
-pushd subber && cargo contract build && popd &&
-cargo contract build
--- examples/erc20/.ink/abi_gen/main.rs
+++
@@ -1,7 +0,0 @@
-fn main() -> Result<(), std::io::Error> {
-    let abi = <contract::Erc20 as ink_lang::GenerateAbi>::generate_abi();
-    let contents = serde_json::to_string_pretty(&abi)?;
-    std::fs::create_dir("target").ok();
-    std::fs::write("target/metadata.json", contents)?;
-    Ok(())
-}
