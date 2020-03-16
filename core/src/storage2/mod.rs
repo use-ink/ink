244933e212675a47009ef8ff704baddb70d194ef
Robin Freyler <robin.freyler@gmail.com>
2020-03-16 15:44:53 +0100

    [core] add brief doc to storage2

--- core/src/storage2/mod.rs
+++ core/src/storage2/mod.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//! Core abstractions for storage manipulation. (revision 2)
+
 pub mod collections;
 pub mod lazy;
 mod traits;
