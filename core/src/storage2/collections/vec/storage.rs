35f1ee9ac71cfabe01651655a1a1a2679dcc0546
Robin Freyler <robin.freyler@gmail.com>
2020-03-27 10:12:55 +0100

    [core] add doc comments to new trait impl files

--- core/src/storage2/collections/vec/storage.rs
+++ core/src/storage2/collections/vec/storage.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//! Implementation of ink! storage traits.
+
 use super::Vec as StorageVec;
 use crate::{
     storage2 as storage,
