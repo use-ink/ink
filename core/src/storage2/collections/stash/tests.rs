a1e0180c5429618f9ed77b850b050280077b94e3
Robin Freyler <robin.freyler@gmail.com>
2020-03-29 11:50:06 +0200

    [core] add initial test for storage2::Stash::{new, default}

---
+++ core/src/storage2/collections/stash/tests.rs
@@ -0,0 +1,33 @@
+// Copyright 2019-2020 Parity Technologies (UK) Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use super::Stash as StorageStash;
+
+#[test]
+fn new_works() {
+    // `StorageVec::new`
+    let stash = <StorageStash<i32>>::new();
+    assert!(stash.is_empty());
+    assert_eq!(stash.len(), 0);
+    assert_eq!(stash.get(0), None);
+    assert!(stash.iter().next().is_none());
+    // `StorageVec::default`
+    let default = <StorageStash<i32> as Default>::default();
+    assert!(default.is_empty());
+    assert_eq!(default.len(), 0);
+    assert_eq!(stash.get(0), None);
+    assert!(default.iter().next().is_none());
+    // `StorageVec::new` and `StorageVec::default` should be equal.
+    assert_eq!(stash, default);
+}
