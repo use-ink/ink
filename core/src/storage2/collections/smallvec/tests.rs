20915ff36d2fc26385edc10e5e08fb4368852136
Robin Freyler <robin.freyler@gmail.com>
2020-03-30 14:18:39 +0200

    [core] add some unit tests to storage2::SmallVec

--- core/src/storage2/collections/smallvec/tests.rs
+++ core/src/storage2/collections/smallvec/tests.rs
@@ -20,9 +20,53 @@ fn new_vec_works() {
     let vec = <SmallVec<i32, U4>>::new();
     assert!(vec.is_empty());
     assert_eq!(vec.len(), 0);
+    assert_eq!(vec.get(0), None);
     assert!(vec.iter().next().is_none());
     let default = <SmallVec<i32, U4> as Default>::default();
     assert!(default.is_empty());
     assert_eq!(default.len(), 0);
+    assert_eq!(vec.get(0), None);
     assert!(default.iter().next().is_none());
+}
+
+#[test]
+fn from_iterator_works() {
+    let some_primes = [b'A', b'B', b'C', b'D'];
+    assert_eq!(some_primes.iter().copied().collect::<SmallVec<_, U4>>(), {
+        let mut vec = SmallVec::new();
+        for prime in &some_primes {
+            vec.push(*prime)
+        }
+        vec
+    });
+}
+
+#[test]
+#[should_panic]
+fn from_iterator_too_many() {
+    let some_primes = [b'A', b'B', b'C', b'D', b'E'];
+    let _ = some_primes.iter().copied().collect::<SmallVec<_, U4>>();
+}
+
+#[test]
+fn from_empty_iterator_works() {
+    assert_eq!(
+        [].iter().copied().collect::<SmallVec<u8, U4>>(),
+        SmallVec::new(),
+    );
+}
+
+#[test]
+fn first_last_of_empty() {
+    let mut vec = <SmallVec<u8, U4>>::new();
+    assert_eq!(vec.first(), None);
+    assert_eq!(vec.first_mut(), None);
+    assert_eq!(vec.last(), None);
+    assert_eq!(vec.last_mut(), None);
+}
+
+#[test]
+fn pop_on_empty_works() {
+    let mut vec = <SmallVec<u8, U4>>::new();
+    assert_eq!(vec.pop(), None);
 }
