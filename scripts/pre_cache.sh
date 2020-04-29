e89053d95f633fbdd36063a8d7e336f9a3a6497b
Denis P <denis.pisarev@parity.io>
2020-04-29 12:57:01 +0200

    test (ci): remove CARGO_HOME caching

--- scripts/pre_cache.sh
+++ scripts/pre_cache.sh
@@ -5,21 +5,6 @@ set -ux
 # if there is no directory for this $CI_COMMIT_REF_NAME/$CI_JOB_NAME
 # create such directory and
 # copy recursively all the files from the newest dir which has $CI_JOB_NAME, if it exists
-if [[ ! -d $CARGO_HOME ]]; then
-  mkdir -p "/ci-cache/$CI_PROJECT_NAME/cargo/$CI_COMMIT_REF_NAME";
-  FRESH_CACHE=$(find "/ci-cache/$CI_PROJECT_NAME/cargo" -mindepth 2 -maxdepth 2 \
-    -type d -name "$CI_JOB_NAME"  -exec stat --printf="%Y\t%n\n" {} \; |sort -n -r |head -1 |cut -f2);
-  if [[ -d $FRESH_CACHE ]]; then
-    echo "____Using" "$FRESH_CACHE" "to prepopulate the cache____";
-    time cp -r "$FRESH_CACHE" "$CARGO_HOME";
-    touch "$CARGO_HOME/config";
-  else
-   echo "_____No such cargo dir, proceeding from scratch_____";
-  fi
-else
-  echo "____No need to prepopulate CARGO_HOME cache____";
-fi
-
 if [[ ! -d $CARGO_TARGET_DIR ]]; then
   mkdir -p "/ci-cache/$CI_PROJECT_NAME/targets/$CI_COMMIT_REF_NAME";
   FRESH_CACHE=$(find "/ci-cache/$CI_PROJECT_NAME/targets" -mindepth 2 -maxdepth 2 \
