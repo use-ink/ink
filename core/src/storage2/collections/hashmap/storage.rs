eeae32df51210e9102aaea0b9ef77dadaa679f11
Robin Freyler <robin.freyler@gmail.com>
2020-05-06 14:10:23 +0200

    [core] add SpreadLayout impl for storage::HashMap

--- core/src/storage2/collections/hashmap/storage.rs
+++ core/src/storage2/collections/hashmap/storage.rs
@@ -23,6 +23,14 @@ use crate::{
     hash::hasher::Hasher,
     storage2::{
         collections::Stash as StorageStash,
+        traits2::{
+            forward_clear_packed,
+            forward_pull_packed,
+            forward_push_packed,
+            KeyPtr as KeyPtr2,
+            PackedLayout,
+            SpreadLayout,
+        },
         ClearAt,
         ClearForward,
         KeyPtr,
@@ -34,6 +42,81 @@ use crate::{
     },
 };
 use ink_primitives::Key;
+
+impl<T> SpreadLayout for ValueEntry<T>
+where
+    T: PackedLayout,
+{
+    const FOOTPRINT: u64 = 1;
+
+    fn pull_spread(ptr: &mut KeyPtr2) -> Self {
+        forward_pull_packed::<Self>(ptr)
+    }
+
+    fn push_spread(&self, ptr: &mut KeyPtr2) {
+        forward_push_packed::<Self>(self, ptr)
+    }
+
+    fn clear_spread(&self, ptr: &mut KeyPtr2) {
+        forward_clear_packed::<Self>(self, ptr)
+    }
+}
+
+impl<T> PackedLayout for ValueEntry<T>
+where
+    T: PackedLayout,
+{
+    fn pull_packed(&mut self, at: &Key) {
+        <T as PackedLayout>::pull_packed(&mut self.value, at)
+    }
+
+    fn push_packed(&self, at: &Key) {
+        <T as PackedLayout>::push_packed(&self.value, at)
+    }
+
+    fn clear_packed(&self, at: &Key) {
+        <T as PackedLayout>::clear_packed(&self.value, at)
+    }
+}
+
+impl<K, V, H, O> SpreadLayout for StorageHashMap<K, V, H>
+where
+    K: Ord + Clone + PackedLayout,
+    K: StorageFootprint + PullForward,
+    V: PackedLayout,
+    H: Hasher<Output = O>,
+    O: Default,
+    Key: From<O>,
+{
+    const FOOTPRINT: u64 = 1 + <StorageStash<K> as SpreadLayout>::FOOTPRINT;
+
+    fn pull_spread(ptr: &mut KeyPtr2) -> Self {
+        Self {
+            keys: SpreadLayout::pull_spread(ptr),
+            values: SpreadLayout::pull_spread(ptr),
+        }
+    }
+
+    fn push_spread(&self, ptr: &mut KeyPtr2) {
+        SpreadLayout::push_spread(&self.keys, ptr);
+        SpreadLayout::push_spread(&self.values, ptr);
+    }
+
+    fn clear_spread(&self, ptr: &mut KeyPtr2) {
+        for key in self.keys() {
+            // It might seem wasteful to clear all entries instead of just
+            // the occupied ones. However this spares us from having one extra
+            // read for every element in the storage stash to filter out vacant
+            // entries. So this is actually a trade-off and at the time of this
+            // implementation it is unclear which path is more efficient.
+            //
+            // The bet is that clearing a storage cell is cheaper than reading one.
+            self.values.clear_packed_at(key);
+        }
+        SpreadLayout::clear_spread(&self.keys, ptr);
+        SpreadLayout::clear_spread(&self.values, ptr);
+    }
+}
 
 impl<K, V, H> StorageFootprint for StorageHashMap<K, V, H>
 where
