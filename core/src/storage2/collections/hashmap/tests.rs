018e5f6ace094cb39b1c2362f3140db4ebc13df0
Robin Freyler <robin.freyler@gmail.com>
2020-05-09 17:34:24 +0200

    [core] add unit test for storage::HashMap::{get, get_mut}

--- core/src/storage2/collections/hashmap/tests.rs
+++ core/src/storage2/collections/hashmap/tests.rs
@@ -73,3 +73,28 @@ fn contains_key_works() {
     assert!(hmap.contains_key(&b'D'));
     assert!(!hmap.contains_key(&b'E'));
 }
+
+#[test]
+fn get_works() {
+    // Empty hash map.
+    let hmap = <StorageHashMap<u8, i32>>::new();
+    assert_eq!(hmap.get(&b'A'), None);
+    assert_eq!(hmap.get(&b'E'), None);
+    // Filled hash map: `get`
+    let hmap = [(b'A', 1), (b'B', 2), (b'C', 3), (b'D', 4)]
+        .iter()
+        .copied()
+        .collect::<StorageHashMap<u8, i32>>();
+    assert_eq!(hmap.get(&b'A'), Some(&1));
+    assert_eq!(hmap.get(&b'B'), Some(&2));
+    assert_eq!(hmap.get(&b'C'), Some(&3));
+    assert_eq!(hmap.get(&b'D'), Some(&4));
+    assert_eq!(hmap.get(&b'E'), None);
+    // Filled hash map: `get_mut`
+    let mut hmap = hmap;
+    assert_eq!(hmap.get_mut(&b'A'), Some(&mut 1));
+    assert_eq!(hmap.get_mut(&b'B'), Some(&mut 2));
+    assert_eq!(hmap.get_mut(&b'C'), Some(&mut 3));
+    assert_eq!(hmap.get_mut(&b'D'), Some(&mut 4));
+    assert_eq!(hmap.get_mut(&b'E'), None);
+}
