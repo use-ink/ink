350ad01d2abe9817ce1bbb4d74fdfce0689a96d5
Robin Freyler <robin.freyler@gmail.com>
2020-05-14 13:59:54 +0200

    [core] move forward_supported_array_lens macro to usage site

--- core/src/storage2/traits/impls/mod.rs
+++ core/src/storage2/traits/impls/mod.rs
@@ -12,18 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#[rustfmt::skip]
-macro_rules! forward_supported_array_lens {
-    ( $mac:ident ) => {
-        $mac! {
-                 1,  2,  3,  4,  5,  6,  7,  8,  9,
-            10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-            20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
-            30, 31, 32,
-        }
-    };
-}
-
 macro_rules! impl_always_packed_layout {
     ( $name:ident < $($frag:ident),+ >, deep: $deep:expr ) => {
         const _: () = {
