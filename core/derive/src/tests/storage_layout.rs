955b9ee99aae887cc2c7e12d8a16ff668ab69e19
Robin Freyler <robin.freyler@gmail.com>
2020-05-31 13:09:56 +0200

    [core/derive] add unit test for MixedEnum

--- core/derive/src/tests/storage_layout.rs
+++ core/derive/src/tests/storage_layout.rs
@@ -156,3 +156,87 @@ fn clike_enum_works() {
         }
     }
 }
+
+#[test]
+fn mixed_enum_works() {
+    synstructure::test_derive! {
+        storage_layout_derive {
+            enum MixedEnum {
+                A,
+                B(bool, u32, i64),
+                C{
+                    a: bool,
+                    b: u32,
+                    c: i64,
+                }
+            }
+        }
+        expands to {
+            #[allow(non_upper_case_globals)]
+            const _DERIVE_ink_core_storage2_traits_StorageLayout_FOR_MixedEnum: () = {
+                impl ::ink_core::storage2::traits::StorageLayout for MixedEnum {
+                    fn layout(__key_ptr: &mut ::ink_core::storage2::traits::KeyPtr) -> ::ink_abi::layout2::Layout {
+                        let dispatch_key = __key_ptr.advance_by(1);
+                        ::ink_abi::layout2::Layout::Enum(
+                            ::ink_abi::layout2::EnumLayout::new(
+                                ::ink_abi::layout2::LayoutKey::from(dispatch_key),
+                                vec![
+                                    {
+                                        let mut __variant_key_ptr = __key_ptr.clone();
+                                        let mut __key_ptr = &mut __variant_key_ptr;
+                                        (
+                                            ::ink_abi::layout2::Discriminant::from(0usize),
+                                            ::ink_abi::layout2::StructLayout::new(vec![]),
+                                        )
+                                    },
+                                    {
+                                        let mut __variant_key_ptr = __key_ptr.clone();
+                                        let mut __key_ptr = &mut __variant_key_ptr;
+                                        (
+                                            ::ink_abi::layout2::Discriminant::from(1usize),
+                                            ::ink_abi::layout2::StructLayout::new(vec![
+                                                ::ink_abi::layout2::FieldLayout::new(
+                                                    None,
+                                                    <bool as ::ink_core::storage2::traits::StorageLayout>::layout(__key_ptr),
+                                                ),
+                                                ::ink_abi::layout2::FieldLayout::new(
+                                                    None,
+                                                    <u32 as ::ink_core::storage2::traits::StorageLayout>::layout(__key_ptr),
+                                                ),
+                                                ::ink_abi::layout2::FieldLayout::new(
+                                                    None,
+                                                    <i64 as ::ink_core::storage2::traits::StorageLayout>::layout(__key_ptr),
+                                                ),
+                                            ]),
+                                        )
+                                    },
+                                    {
+                                        let mut __variant_key_ptr = __key_ptr.clone();
+                                        let mut __key_ptr = &mut __variant_key_ptr;
+                                        (
+                                            ::ink_abi::layout2::Discriminant::from(2usize),
+                                            ::ink_abi::layout2::StructLayout::new(vec![
+                                                ::ink_abi::layout2::FieldLayout::new(
+                                                    Some("a"),
+                                                    <bool as ::ink_core::storage2::traits::StorageLayout>::layout(__key_ptr),
+                                                ),
+                                                ::ink_abi::layout2::FieldLayout::new(
+                                                    Some("b"),
+                                                    <u32 as ::ink_core::storage2::traits::StorageLayout>::layout(__key_ptr),
+                                                ),
+                                                ::ink_abi::layout2::FieldLayout::new(
+                                                    Some("c"),
+                                                    <i64 as ::ink_core::storage2::traits::StorageLayout>::layout(__key_ptr),
+                                                ),
+                                            ]),
+                                        )
+                                    },
+                                ]
+                            )
+                        )
+                    }
+                }
+            };
+        }
+    }
+}
