575df0e0e97ae88978f41da30b2403877c80a47d
Robin Freyler <robin.freyler@gmail.com>
2020-06-13 12:51:04 +0200

    [examples] fix bug that DNS example still used type-metadata crate

--- examples/dns/lib.rs
+++ examples/dns/lib.rs
@@ -86,7 +86,7 @@ mod dns {
 
     /// Errors that can occur upon calling this contract.
     #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
-    #[cfg_attr(feature = "std", derive(::type_metadata::Metadata))]
+    #[cfg_attr(feature = "std", derive(::scale_info::Metadata))]
     pub enum Error {
         /// Returned if the name already exists upon registration.
         NameAlreadyExists,
