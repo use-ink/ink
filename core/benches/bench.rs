4c93028f1ae46bd5e1f8ced6e16a345cee7e528e
Michael Mueller <mich@elmueller.net>
2020-06-18 13:03:31 +0200

    [core] Reduce benchmark overhead by preventing cache writes

--- core/benches/bench.rs
+++ core/benches/bench.rs
@@ -19,6 +19,7 @@ use criterion::{
     Criterion,
 };
 
+use core::mem::ManuallyDrop;
 use ink_core::{
     env,
     storage2::{
@@ -66,43 +67,7 @@ fn pop_all(test_values: &[u8]) {
     assert!(vec.is_empty());
 }
 
-/// In this case we lazily instantiate a `StorageVec` by first `push_spread`-ing
-/// onto the contract storage. We then load the vec from storage lazily using
-/// `pull_spread`. This will just load lazily and won't pull anything from the
-/// storage.
-fn clear_lazy(test_values: &[u8]) {
-    let _ = env::test::run_test::<env::DefaultEnvTypes, _>(|_| {
-        let vec = vec_from_slice(&test_values);
-        let root_key = Key::from([0x00; 32]);
-        SpreadLayout::push_spread(&vec, &mut KeyPtr::from(root_key));
-        let mut vec =
-            <StorageVec<u8> as SpreadLayout>::pull_spread(&mut KeyPtr::from(root_key));
-        vec.clear();
-        assert!(vec.is_empty());
-        Ok(())
-    })
-    .unwrap();
-}
-
-/// In this case we lazily instantiate a `StorageVec` by first `push_spread`-ing
-/// onto the contract storage. We then load the vec from storage lazily using
-/// `pull_spread`. This will just load lazily and won't pull anything from the
-/// storage. `pop` will then result in loading from storage.
-fn pop_all_lazy(test_values: &[u8]) {
-    let _ = env::test::run_test::<env::DefaultEnvTypes, _>(|_| {
-        let vec = vec_from_slice(&test_values);
-        let root_key = Key::from([0x00; 32]);
-        SpreadLayout::push_spread(&vec, &mut KeyPtr::from(root_key));
-        let mut vec =
-            <StorageVec<u8> as SpreadLayout>::pull_spread(&mut KeyPtr::from(root_key));
-        while let Some(_) = vec.pop() {}
-        assert!(vec.is_empty());
-        Ok(())
-    })
-    .unwrap();
-}
-
-fn put(test_values: &[u8]) {
+fn put_cached(test_values: &[u8]) {
     let mut vec = vec_from_slice(&test_values);
     for (index, _value) in test_values.iter().enumerate() {
         vec.set(index as u32, b'X');
@@ -110,7 +75,7 @@ fn put(test_values: &[u8]) {
     assert_eq_slice(&vec, &[b'X', b'X', b'X', b'X', b'X', b'X']);
 }
 
-fn deref(test_values: &[u8]) {
+fn deref_cached(test_values: &[u8]) {
     let mut vec = vec_from_slice(&test_values);
     for (index, _value) in test_values.iter().enumerate() {
         *vec.get_mut(index as u32).unwrap() = b'X';
@@ -118,49 +83,20 @@ fn deref(test_values: &[u8]) {
     assert_eq_slice(&vec, &[b'X', b'X', b'X', b'X', b'X', b'X']);
 }
 
-/// In this case we lazily instantiate a `StorageVec` by first `push_spread`-ing
-/// onto the contract storage. We then load the vec from storage lazily using
-/// `pull_spread`. This will just load lazily and won't pull anything from the
-/// storage. The `vec.set()` will not load anything from storage.
-fn put_lazy(test_values: &[u8]) {
-    let _ = env::test::run_test::<env::DefaultEnvTypes, _>(|_| {
-        let vec = vec_from_slice(&test_values);
-        let root_key = Key::from([0x00; 32]);
-        SpreadLayout::push_spread(&vec, &mut KeyPtr::from(root_key));
-        let mut vec =
-            <StorageVec<u8> as SpreadLayout>::pull_spread(&mut KeyPtr::from(root_key));
-        for (index, _value) in test_values.iter().enumerate() {
-            vec.set(index as u32, b'X');
-        }
-        assert_eq_slice(&vec, &[b'X', b'X', b'X', b'X', b'X', b'X']);
-        Ok(())
-    })
-    .unwrap();
-}
-
-/// In this case we lazily instantiate a `StorageVec` by first `push_spread`-ing
-/// onto the contract storage. We then load the vec from storage lazily using
-/// `pull_spread`. This will just load lazily and won't pull anything from the
-/// storage. The `deref` will then load from storage.
-fn deref_lazy(test_values: &[u8]) {
-    let _ = env::test::run_test::<env::DefaultEnvTypes, _>(|_| {
-        let vec = vec_from_slice(&test_values);
-        let root_key = Key::from([0x00; 32]);
-        SpreadLayout::push_spread(&vec, &mut KeyPtr::from(root_key));
-        let mut vec =
-            <StorageVec<u8> as SpreadLayout>::pull_spread(&mut KeyPtr::from(root_key));
-        for (index, _value) in test_values.iter().enumerate() {
-            *vec.get_mut(index as u32).unwrap() = b'X';
-        }
-        assert_eq_slice(&vec, &[b'X', b'X', b'X', b'X', b'X', b'X']);
-        Ok(())
-    })
-    .unwrap();
+fn bench_put_cached(c: &mut Criterion) {
+    let mut group = c.benchmark_group("PutMustOutperformDerefInCachedCase");
+    let test_values = [b'A', b'B', b'C', b'D', b'E', b'F'];
+    group.bench_with_input(BenchmarkId::new("Put", 0), &test_values, |b, i| {
+        b.iter(|| put_cached(i))
+    });
+    group.bench_with_input(BenchmarkId::new("Deref", 0), &test_values, |b, i| {
+        b.iter(|| deref_cached(i))
+    });
+    group.finish();
 }
 
 fn bench_clear_cached(c: &mut Criterion) {
     let mut group = c.benchmark_group("ClearMustOutperformPopInCachedCase");
-
     let test_values = [b'A', b'B', b'C', b'D', b'E', b'F'];
     group.bench_with_input(BenchmarkId::new("Clear", 0), &test_values, |b, i| {
         b.iter(|| clear(i))
@@ -171,41 +107,109 @@ fn bench_clear_cached(c: &mut Criterion) {
     group.finish();
 }
 
+/// The manual drop is used to prevent the `vec` from being written back to storage.
+/// This is so that it can be reused in the next benchmark instance, without the
+/// storage flush overhead.
+fn manual_drop(vec: StorageVec<u8>) {
+    ManuallyDrop::new(vec);
+}
+
+/// In this case we lazily load the vec from storage using `pull_spread`.
+/// This will just load lazily and won't pull anything from the storage.
+fn clear_lazy() {
+    let root_key = Key::from([0x00; 32]);
+    let mut vec =
+        <StorageVec<u8> as SpreadLayout>::pull_spread(&mut KeyPtr::from(root_key));
+    vec.clear();
+    assert!(vec.is_empty());
+    manual_drop(vec);
+}
+
+/// In this case we lazily load the vec from storage using `pull_spread`.
+/// This will just load lazily and won't pull anything from the storage.
+/// `pop` will then result in loading from storage.
+fn pop_all_lazy() {
+    let root_key = Key::from([0x00; 32]);
+    let mut vec =
+        <StorageVec<u8> as SpreadLayout>::pull_spread(&mut KeyPtr::from(root_key));
+    while let Some(_) = vec.pop() {}
+    assert!(vec.is_empty());
+    manual_drop(vec);
+}
+
+/// In this case we lazily load the vec from storage using `pull_spread`.
+/// This will just load lazily and won't pull anything from the storage.
+/// The `deref` will then load from storage.
+fn deref_lazy() {
+    let root_key = Key::from([0x00; 32]);
+    let mut vec =
+        <StorageVec<u8> as SpreadLayout>::pull_spread(&mut KeyPtr::from(root_key));
+    for index in 0..vec.len() {
+        *vec.get_mut(index).unwrap() = b'X';
+    }
+    assert_eq_slice(&vec, &[b'X', b'X', b'X', b'X', b'X', b'X']);
+    manual_drop(vec);
+}
+
+/// In this case we lazily load the vec from storage using `pull_spread`.
+/// This will just load lazily and won't pull anything from the storage.
+/// The `vec.set()` will not load anything from storage.
+fn put_lazy() {
+    let root_key = Key::from([0x00; 32]);
+    let mut vec =
+        <StorageVec<u8> as SpreadLayout>::pull_spread(&mut KeyPtr::from(root_key));
+    for index in 0..vec.len() {
+        vec.set(index, b'X');
+    }
+    assert_eq_slice(&vec, &[b'X', b'X', b'X', b'X', b'X', b'X']);
+    manual_drop(vec);
+}
+
+/// Create a vec and push it to storage.
+fn create_and_store() {
+    let test_values = [b'A', b'B', b'C', b'D', b'E', b'F'];
+    let vec = vec_from_slice(&test_values);
+    let root_key = Key::from([0x00; 32]);
+    SpreadLayout::push_spread(&vec, &mut KeyPtr::from(root_key));
+}
+
+/// In this case we lazily instantiate a `StorageVec` by first `create_and_store`-ing
+/// into the contract storage. We then load the vec from storage lazily in each
+/// benchmark iteration.
 fn bench_clear_lazy(c: &mut Criterion) {
     let mut group = c.benchmark_group("ClearMustOutperformPopInLazyCase");
-
-    let test_values = [b'A', b'B', b'C', b'D', b'E', b'F'];
-    group.bench_with_input(BenchmarkId::new("Clear", 0), &test_values, |b, i| {
-        b.iter(|| clear_lazy(i))
-    });
-    group.bench_with_input(BenchmarkId::new("PopAll", 0), &test_values, |b, i| {
-        b.iter(|| pop_all_lazy(i))
-    });
+    let _ = env::test::run_test::<env::DefaultEnvTypes, _>(|_| {
+        create_and_store();
+        group.bench_function(BenchmarkId::new("Clear", 0), |b| b.iter(|| clear_lazy()));
+        Ok(())
+    })
+    .unwrap();
+    let _ = env::test::run_test::<env::DefaultEnvTypes, _>(|_| {
+        create_and_store();
+        group
+            .bench_function(BenchmarkId::new("PopAll", 0), |b| b.iter(|| pop_all_lazy()));
+        Ok(())
+    })
+    .unwrap();
     group.finish();
 }
 
-fn bench_put_cached(c: &mut Criterion) {
-    let mut group = c.benchmark_group("PutMustOutperformDerefInCachedCase");
-
-    let test_values = [b'A', b'B', b'C', b'D', b'E', b'F'];
-    group.bench_with_input(BenchmarkId::new("Put", 0), &test_values, |b, i| {
-        b.iter(|| put(i))
-    });
-    group.bench_with_input(BenchmarkId::new("Deref", 0), &test_values, |b, i| {
-        b.iter(|| deref(i))
-    });
-    group.finish();
-}
-
+/// In this case we lazily instantiate a `StorageVec` by first `create_and_store`-ing
+/// into the contract storage. We then load the vec from storage lazily in each
+/// benchmark iteration.
 fn bench_put_lazy(c: &mut Criterion) {
     let mut group = c.benchmark_group("PutMustOutperformDerefInLazyCase");
-
-    let test_values = [b'A', b'B', b'C', b'D', b'E', b'F'];
-    group.bench_with_input(BenchmarkId::new("Put", 0), &test_values, |b, i| {
-        b.iter(|| put_lazy(i))
-    });
-    group.bench_with_input(BenchmarkId::new("Deref", 0), &test_values, |b, i| {
-        b.iter(|| deref_lazy(i))
-    });
+    let _ = env::test::run_test::<env::DefaultEnvTypes, _>(|_| {
+        create_and_store();
+        group.bench_function(BenchmarkId::new("Put", 0), |b| b.iter(|| put_lazy()));
+        Ok(())
+    })
+    .unwrap();
+    let _ = env::test::run_test::<env::DefaultEnvTypes, _>(|_| {
+        create_and_store();
+        group.bench_function(BenchmarkId::new("Deref", 0), |b| b.iter(|| deref_lazy()));
+        Ok(())
+    })
+    .unwrap();
     group.finish();
 }
