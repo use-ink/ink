7c445d02f1808641d167ec612e3c76efff35edc7
Andrew Jones <ascjones@gmail.com>
2020-06-22 12:20:35 +0100

    Update remaining examples to use generate metadata function

--- examples/incrementer/.ink/abi_gen/main.rs
+++ examples/incrementer/.ink/abi_gen/main.rs
@@ -4,6 +4,12 @@ extern "Rust" {
     fn __ink_generate_metadata() -> ink_abi::InkProject;
 }
 
+extern crate contract;
+
+extern "Rust" {
+    fn __ink_generate_metadata() -> ink_abi::InkProject;
+}
+
 fn main() -> Result<(), std::io::Error> {
     let ink_project = unsafe { __ink_generate_metadata() };
     let contents = serde_json::to_string_pretty(&ink_project)?;
