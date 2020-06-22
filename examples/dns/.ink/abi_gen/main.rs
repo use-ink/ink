9eb92645d113b3bb6759fbb22746282a191b26c1
Andrew Jones <ascjones@gmail.com>
2020-06-22 14:31:03 +0100

    Extern function to generate metadata (#460)
    * Hack to see if #[start] works
    
    * Hack to see if #[no_mangle] works
    
    * Remove incrementer bin target
    
    * Use Rust extern api and pass back InkProject struct directly
    
    * Uglify name
    
    * Update remaining examples to use generate metadata function
    
    * Do not generate for ink-as-dependency to make delegator work
    
    * Remove explicit version from abi dependency
    
    * Remove duplication
    
    * Remove GenerateAbi trait

--- examples/dns/.ink/abi_gen/main.rs
+++ examples/dns/.ink/abi_gen/main.rs
@@ -1,6 +1,12 @@
+extern crate contract;
+
+extern "Rust" {
+    fn __ink_generate_metadata() -> ink_abi::InkProject;
+}
+
 fn main() -> Result<(), std::io::Error> {
-    let abi = <::contract::DomainNameService as ::ink_lang::GenerateAbi>::generate_abi();
-    let contents = serde_json::to_string_pretty(&abi)?;
+    let ink_project = unsafe { __ink_generate_metadata() };
+    let contents = serde_json::to_string_pretty(&ink_project)?;
     std::fs::create_dir("target").ok();
     std::fs::write("target/metadata.json", contents)?;
     Ok(())
