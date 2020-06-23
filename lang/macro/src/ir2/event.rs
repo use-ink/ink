16d49cfec3368b61c730223e622ba4227a4c8ca8
Robin Freyler <robin.freyler@gmail.com>
2020-06-23 21:22:27 +0200

    [lang/macro] add initial implementation of ink! event's TryFrom

--- lang/macro/src/ir2/event.rs
+++ lang/macro/src/ir2/event.rs
@@ -12,8 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use crate::ir2;
+use crate::{
+    error::ExtError as _,
+    ir2,
+};
 use core::convert::TryFrom;
+use proc_macro2::Ident;
+use syn::spanned::Spanned as _;
 
 pub struct Event {
     ast: syn::ItemStruct,
@@ -23,7 +28,70 @@ impl TryFrom<syn::ItemStruct> for Event {
     type Error = syn::Error;
 
     fn try_from(item_struct: syn::ItemStruct) -> Result<Self, Self::Error> {
-        todo!()
+        let struct_span = item_struct.span();
+        let (ink_attrs, other_attrs) = ir2::partition_attributes(item_struct.attrs)?;
+        let normalized = ir2::InkAttribute::from_expanded(ink_attrs).map_err(|err| {
+            err.into_combine(format_err_span!(struct_span, "at this invokation",))
+        })?;
+        normalized
+            .ensure_first(&ir2::AttributeArgKind::Event)
+            .map_err(|err| {
+                err.into_combine(format_err_span!(
+                    struct_span,
+                    "expected `#[ink(event)]` as first ink! attribute argument",
+                ))
+            })?;
+        normalized
+            .ensure_no_conflicts(|arg| arg.kind() != &ir2::AttributeArgKind::Event)?;
+        if !item_struct.generics.params.is_empty() {
+            return Err(format_err!(
+                item_struct.generics.params,
+                "generic ink! event structs are not supported",
+            ))
+        }
+        match &item_struct.vis {
+            syn::Visibility::Inherited
+            | syn::Visibility::Restricted(_)
+            | syn::Visibility::Crate(_) => {
+                return Err(format_err!(
+                    &item_struct.vis,
+                    "non `pub` ink! event structs are not supported",
+                ))
+            }
+            _ => (),
+        }
+        for field in item_struct.fields.iter() {
+            let field_span = field.span();
+            let (ink_attrs, _) = ir2::partition_attributes(field.attrs.clone())?;
+            if ink_attrs.is_empty() {
+                continue;
+            }
+            let normalized =
+                ir2::InkAttribute::from_expanded(ink_attrs).map_err(|err| {
+                    err.into_combine(format_err_span!(field_span, "at this invokation",))
+                })?;
+            if normalized.first().kind() != &ir2::AttributeArgKind::Topic {
+                return Err(format_err_span!(
+                    field_span,
+                    "first optional ink! attribute of an event field must be #[ink(topic)]",
+                ))
+            }
+            for arg in normalized.args() {
+                return Err(format_err_span!(
+                    arg.span(),
+                    "encountered conflicting ink! attribute for event field",
+                ))
+            }
+        }
+        Ok(Self {
+            ast: syn::ItemStruct {
+                attrs: other_attrs,
+                ..item_struct
+            },
+        })
+    }
+}
+
 impl Event {
     /// Returns the identifier of the event struct.
     pub fn ident(&self) -> &Ident {
