bdfb400c9408898de48338fea1e9e226c04a845b
Robin Freyler <robin.freyler@gmail.com>
2020-06-27 13:03:57 +0200

    [lang/macro] add module doc to callable.rs

--- lang/macro/src/ir2/impl_block/callable.rs
+++ lang/macro/src/ir2/impl_block/callable.rs
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//! Utilities and helper routines that are useful for both ink! messages
+//! and ink! constructors.
+
 use super::{
     Constructor,
     Message,
