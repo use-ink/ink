18bf56adf7a47e1d1bb7fff3a1e8ff35aacfdab9
Andrew Jones <ascjones@gmail.com>
2020-07-16 16:43:02 +0100

    Copy in some reference impls for inspiration

--- core/src/storage2/collections/binary_heap/mod.rs
+++ core/src/storage2/collections/binary_heap/mod.rs
@@ -94,10 +94,95 @@ where
         self.elems.get(0)
     }
 
+    // The implementations of sift_up and sift_down use unsafe blocks in
+    // order to move an element out of the vector (leaving behind a
+    // hole), shift along the others and move the removed element back into the
+    // vector at the final location of the hole.
+    // The `Hole` type is used to represent this, and make sure
+    // the hole is filled back at the end of its scope, even on panic.
+    // Using a hole reduces the constant factor compared to using swaps,
+    // which involves twice as many moves.
+    fn sift_up(&mut self, start: usize, pos: usize) -> usize {
+        unsafe {
+            // Take out the value at `pos` and create a hole.
+            let mut hole = Hole::new(&mut self.data, pos);
+
+            while hole.pos() > start {
+                let parent = (hole.pos() - 1) / 2;
+                if hole.element() <= hole.get(parent) {
+                    break;
+                }
+                hole.move_to(parent);
+            }
+            hole.pos()
+        }
+    }
+
+    /// Take an element at `pos` and move it all the way down the heap,
+    /// then sift it up to its position.
+    ///
+    /// Note: This is faster when the element is known to be large / should
+    /// be closer to the bottom.
+    fn sift_down_to_bottom(&mut self, mut pos: usize) {
+        let end = self.len();
+        let start = pos;
+        unsafe {
+            let mut hole = Hole::new(&mut self.data, pos);
+            let mut child = 2 * pos + 1;
+            while child < end {
+                let right = child + 1;
+                // compare with the greater of the two children
+                if right < end && hole.get(child) <= hole.get(right) {
+                    child = right;
+                }
+                hole.move_to(child);
+                child = 2 * hole.pos() + 1;
+            }
+            pos = hole.pos;
+        }
+        self.sift_up(start, pos);
+    }
+
+    fn max_heapify(&mut self, pos: u32) {
+        // Max-Heapify (A, i):
+        //     left ← 2×i     // ← means "assignment"
+        // right ← 2×i + 1
+        // largest ← i
+        //
+        // if left ≤ heap_length[A] and A[left] > A[largest] then:
+        //     largest ← left
+        //
+        // if right ≤ heap_length[A] and A[right] > A[largest] then:
+        //     largest ← right
+        //
+        // if largest ≠ i then:
+        //     swap A[i] and A[largest]
+        // Max-Heapify(A, largest)
+
+        let left = 2 * pos + 1;
+        let right = 2 * pos + 2;
+        let mut largest = pos;
+
+        if left < self.len() && self.elems.get(left) > self.elems.get(largest) {
+            largest = left
+        }
+
+        if right < self.len() && self.elems.get(right) > self.elems.get(largest) {
+            largest = right
+        }
+
+        if largest != pos {
+            self.elems.swap(pos, largest);
+        }
+        self.max_heapify(largest)
+    }
+
     /// Pops greatest element from the heap and returns it
     ///
     /// Returns `None` if the heap is empty
     pub fn pop(&mut self) -> Option<T> {
+        // replace the root of the heap with the last element on the last level
+
         self.elems.put_get(0, None)
     }
 }
@@ -108,7 +193,11 @@ where
 {
     // todo: optimize!
     fn bubble_up(&mut self, index: u32) {
-        let parent_index = index.saturating_sub(1) / 2;
+        assert!(
+            index > 0,
+            "cannot bubble up the root element"
+        );
+        let parent_index = (index - 1) / 2;
         let parent = self.elems.get(parent_index)
             .expect("parent must exist in fully compacted sequence of elements");
         let child = self.elems.get(index)
@@ -122,6 +211,7 @@ where
 
     /// Pushes the given element to the binary heap.
     pub fn push(&mut self, value: T) {
+        // todo: put on the left i.e. if len is even then add 1?
         let index = self.len();
         assert!(
             index < core::u32::MAX,
