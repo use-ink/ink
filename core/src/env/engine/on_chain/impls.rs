c38648f9a3df30e760eb0f13181d105c4890cd3b
Robin Freyler <robin.freyler@gmail.com>
2020-07-23 22:40:27 +0200

    [core] on-chain: add new utilities for static buffer modifications
    - EncodeScope: for efficiently appending encoded values
    - ScopedBuffer: for efficiently chunking buffers

--- core/src/env/engine/on_chain/impls.rs
+++ core/src/env/engine/on_chain/impls.rs
@@ -42,7 +42,109 @@ impl From<ext::Error> for EnvError {
         }
     }
 }
+
+pub struct EncodeScope<'a> {
+    buffer: &'a mut [u8],
+    len: usize,
+}
+
+impl<'a> From<&'a mut [u8]> for EncodeScope<'a> {
+    fn from(buffer: &'a mut [u8]) -> Self {
+        Self { buffer, len: 0 }
+    }
+}
+
+impl<'a> EncodeScope<'a> {
+    /// Returns the capacity of the encoded scope.
+    pub fn capacity(&self) -> usize {
+        self.buffer.len()
+    }
+
+    /// Returns the length of the encoded scope.
+    pub fn len(&self) -> usize {
+        self.len
+    }
+
+    /// Returns the internal mutable byte slice.
+    pub fn into_buffer(self) -> &'a mut [u8] {
+        self.buffer
+    }
+}
+
+impl<'a> scale::Output for EncodeScope<'a> {
+    fn write(&mut self, bytes: &[u8]) {
+        assert!(self.len() + bytes.len() <= self.capacity());
+        let start = self.len;
+        let len_bytes = bytes.len();
+        self.buffer[start..(start + len_bytes)].copy_from_slice(bytes);
+        self.len += len_bytes;
+    }
+
+    fn push_byte(&mut self, byte: u8) {
+        assert_ne!(self.len(), self.capacity());
+        self.buffer[self.len] = byte;
+        self.len += 1;
+    }
+}
+
+/// Scoped access to an underlying bytes buffer.
+///
+/// # Note
+///
+/// This is used to efficiently chunk up ink!'s internal static 16kB buffer
+/// into smaller sub buffers for processing different parts of computations.
+#[derive(Debug)]
+pub struct ScopedBuffer<'a> {
+    buffer: &'a mut [u8],
+}
+
+impl<'a> From<&'a mut [u8]> for ScopedBuffer<'a> {
+    fn from(buffer: &'a mut [u8]) -> Self {
+        Self { buffer }
+    }
+}
+
+impl<'a> ScopedBuffer<'a> {
+    /// Returns the first `len` bytes of the buffer as mutable slice.
+    pub fn take(&mut self, len: usize) -> &'a mut [u8] {
+        assert!(len <= self.buffer.len());
+        let len_before = self.buffer.len();
+        let buffer = core::mem::take(&mut self.buffer);
+        let (lhs, rhs) = buffer.split_at_mut(len);
+        self.buffer = rhs;
+        debug_assert_eq!(lhs.len(), len);
+        let len_after = self.buffer.len();
+        debug_assert_eq!(len_before - len_after, len);
+        lhs
+    }
+
+    /// Encode the given value into the scoped buffer and return the sub slice
+    /// containing all the encoded bytes.
+    pub fn take_encoded<T>(&mut self, value: &T) -> &'a mut [u8]
+    where
+        T: scale::Encode,
+    {
+        let buffer = core::mem::take(&mut self.buffer);
+        let mut encode_scope = EncodeScope::from(buffer);
+        scale::Encode::encode_to(&value, &mut encode_scope);
+        let encode_len = encode_scope.len();
+        core::mem::replace(&mut self.buffer, encode_scope.into_buffer());
+        self.take(encode_len)
+    }
+
+    /// Returns all of the remaining bytes of the buffer as mutable slice.
+    pub fn take_rest(self) -> &'a mut [u8] {
+        assert!(!self.buffer.is_empty());
+        self.buffer
+    }
+}
+
 impl EnvInstance {
+    /// Returns a new scoped buffer for the entire scope of the static 16kB buffer.
+    fn scoped_buffer(&mut self) -> ScopedBuffer {
+        ScopedBuffer::from(&mut self.buffer[..])
+    }
+
     /// Empties the contract-side scratch buffer.
     ///
     /// # Note
