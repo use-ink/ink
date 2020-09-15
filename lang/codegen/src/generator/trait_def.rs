bc62ccd120fca6f1ef650acfbabb7c72dd1c3473
Robin Freyler <robin.freyler@gmail.com>
2020-09-15 13:24:53 +0200

    [lang/codegen] refine codegen for ink! trait definitions

--- lang/codegen/src/generator/trait_def.rs
+++ lang/codegen/src/generator/trait_def.rs
@@ -12,28 +12,44 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use crate::{
-    generator,
-    GenerateCode,
-    GenerateCodeUsing,
-};
-use derive_more::From;
+use crate::GenerateCode;
 use proc_macro2::TokenStream as TokenStream2;
 use quote::{
-    quote,
+    format_ident,
     quote_spanned,
 };
-use syn::spanned::Spanned as _;
 
 /// Generator to create the ink! storage struct and important trait impls.
-#[derive(From)]
 pub struct TraitDefinition<'a> {
     trait_def: &'a ir::InkTrait,
 }
 
-impl<'a> AsRef<ir::InkTrait> for TraitDefinition<'_> {
-    fn as_ref(&self) -> &ir::InkTrait {
-        &self.trait_def
+impl<'a> TraitDefinition<'a> {
+    fn generate_for_constructor(
+        constructor: ir::InkTraitConstructor<'a>,
+    ) -> TokenStream2 {
+        let span = constructor.span();
+        let attrs = constructor.attrs();
+        let sig = constructor.sig();
+        let ident = &sig.ident;
+        let inputs = &sig.inputs;
+        quote_spanned!(span =>
+            #(#attrs)*
+            fn #ident(#inputs) -> Self::Output;
+        )
+    }
+
+    fn generate_for_message(message: ir::InkTraitMessage<'a>) -> TokenStream2 {
+        let span = message.span();
+        let attrs = message.attrs();
+        let sig = message.sig();
+        let ident = &sig.ident;
+        let inputs = &sig.inputs;
+        let output = &sig.output;
+        quote_spanned!(span =>
+            #(#attrs)*
+            fn #ident(#inputs) -> #output;
+        )
     }
 }
 
@@ -41,47 +57,45 @@ impl GenerateCode for TraitDefinition<'_> {
     fn generate_code(&self) -> TokenStream2 {
         let span = self.trait_def.span();
         let attrs = self.trait_def.attrs();
+        let hash = self.trait_def.verify_hash();
         let ident = self.trait_def.ident();
-        let verify_hash = self.trait_def.verify_hash();
+        let helper_ident = format_ident!(
+            "__ink_Checked{}_0x{:X}{:X}{:X}{:X}",
+            ident,
+            hash[0],
+            hash[1],
+            hash[2],
+            hash[3]
+        );
+        let verify_hash_id = u32::from_be_bytes([hash[0], hash[1], hash[2], hash[3]]);
         let constructors = self
             .trait_def
             .iter_items()
             .flat_map(ir::InkTraitItem::filter_map_constructor)
-            .map(|constructor| {
-                let span = constructor.span();
-                let attrs = constructor.attrs();
-                let sig = constructor.sig();
-                let ident = &sig.ident;
-                let inputs = &sig.inputs;
-                quote_spanned!(span =>
-                    #(#attrs)*
-                    fn #ident(#inputs) -> Self::Output;
-                )
-            });
+            .map(Self::generate_for_constructor);
         let messages = self
             .trait_def
             .iter_items()
             .flat_map(ir::InkTraitItem::filter_map_message)
-            .map(|message| {
-                let span = message.span();
-                let attrs = message.attrs();
-                let sig = message.sig();
-                let ident = &sig.ident;
-                let inputs = &sig.inputs;
-                let output = &sig.output;
-                quote_spanned!(span =>
-                    #(#attrs)*
-                    fn #ident(#inputs) -> #output;
-                )
-            });
+            .map(Self::generate_for_message);
         quote_spanned!(span =>
             #(#attrs)*
-            pub trait #ident {
+            pub trait #ident: ::ink_lang::Checked<[(); #verify_hash_id]> {
                 type Output;
+                #[doc(hidden)]
+                type __ink_Checksum: #helper_ident;
 
                 #(#constructors)*
                 #(#messages)*
             }
+
+            #[doc(hidden)]
+            #[allow(non_camel_case_types)]
+            pub unsafe trait #helper_ident {}
+
+            const _: () = {
+                unsafe impl #helper_ident for [(); #verify_hash_id] {}
+            };
         )
     }
 }
