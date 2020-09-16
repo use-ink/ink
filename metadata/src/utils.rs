3ca858d4c0e944e9340cb7b8e3046d45702d3aef
Andrew Jones <ascjones@gmail.com>
2020-09-16 13:08:24 +0100

    WIP decode hex

--- metadata/src/utils.rs
+++ metadata/src/utils.rs
@@ -30,3 +30,12 @@ where
     }
     serializer.serialize_str(&hex)
 }
+
+// pub fn decode_hex(s: &str) -> Result<Vec<u8>, ParseIntError> {
+//     (0..s.len())
+//         .step_by(2)
+//         .map(|i| u8::from_str_radix(&s[i..i + 2], 16))
+//         .collect()
+// }
+
+// pub fn deserialize_byte_str()
