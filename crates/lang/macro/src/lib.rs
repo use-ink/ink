522d64f8e3419fe712f477ebc415e3ddbb39a21b
Robin Freyler <robin.freyler@gmail.com>
2020-10-05 17:10:17 +0200

    [lang/macro] fix payable messages example

--- crates/lang/macro/src/lib.rs
+++ crates/lang/macro/src/lib.rs
@@ -335,14 +335,14 @@ use proc_macro::TokenStream;
 ///         # }
 ///     impl Flipper {
 ///         #[ink(constructor)]
-///         #[ink(selector = "0xDEADBEEF")]
+///         #[ink(selector = "0xDEADBEEF")] // Works on constructors as well.
 ///         pub fn new(initial_value: bool) -> Self {
 ///             Flipper { value: false }
 ///         }
 ///
 ///         # /// Flips the current value.
 ///         # #[ink(message)]
-///         # #[ink(payable)] // You can either specify payable out-of-line.
+///         # #[ink(selector = "0xCAFEBABE")] // You can either specify selector out-of-line.
 ///         # pub fn flip(&mut self) {
 ///         #     self.value = !self.value;
 ///         # }
