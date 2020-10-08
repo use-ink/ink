0829b0e65fc4a8d50c2e90a3ab733f5f3bca8c9d
Denis P <denis.pisarev@parity.io>
2020-10-08 17:12:57 +0200

    change (CI): CARGO_HOME goes to branch level to avoid collapses; pre-population for it

--- scripts/pre_cache.sh
+++ scripts/pre_cache.sh
@@ -7,14 +7,29 @@ set -u
 # copy recursively all the files from the newest dir which has $CI_JOB_NAME, if it exists
 if [[ ! -d $CARGO_TARGET_DIR ]]; then
   mkdir -p "/ci-cache/$CI_PROJECT_NAME/targets/$CI_COMMIT_REF_NAME";
-  FRESH_CACHE=$(find "/ci-cache/$CI_PROJECT_NAME/targets" -mindepth 2 -maxdepth 2 \
+  FRESH_TARGET_CACHE=$(find "/ci-cache/$CI_PROJECT_NAME/targets" -mindepth 2 -maxdepth 2 \
     -type d -name "$CI_JOB_NAME"  -exec stat --printf="%Y\t%n\n" {} \; |sort -n -r |head -1 |cut -f2);
-  if [[ -d $FRESH_CACHE ]]; then
-    echo "____Using" "$FRESH_CACHE" "to prepopulate the cache____";
-    time cp -r "$FRESH_CACHE" "$CARGO_TARGET_DIR";
+  if [[ -d $FRESH_TARGET_CACHE ]]; then
+    echo "____Using" "$FRESH_TARGET_CACHE" "to prepopulate the cache____";
+    time cp -r "$FRESH_TARGET_CACHE" "$CARGO_TARGET_DIR";
   else
     echo "_____No such targets dir, proceeding from scratch_____";
   fi
 else
   echo "____No need to prepopulate CARGO_TARGET_DIR cache____";
+fi
+
+# same for CARGO_HOME cache prepopulation
+if [[ ! -d $CARGO_HOME ]]; then
+  mkdir -p "/ci-cache/$CI_PROJECT_NAME/cargo/$CI_COMMIT_REF_NAME";
+  FRESH_HOME_CACHE=$(find "/ci-cache/$CI_PROJECT_NAME/cargo" -mindepth 2 -maxdepth 2 \
+    -type d -name "$CI_JOB_NAME"  -exec stat --printf="%Y\t%n\n" {} \; |sort -n -r |head -1 |cut -f2);
+  if [[ -d $FRESH_HOME_CACHE ]]; then
+    echo "____Using" "$FRESH_HOME_CACHE" "to prepopulate the cache____";
+    time cp -r "$FRESH_HOME_CACHE" "$CARGO_HOME";
+  else
+    echo "_____No such targets dir, proceeding from scratch_____";
+  fi
+else
+  echo "____No need to prepopulate CARGO_HOME cache____";
 fi
