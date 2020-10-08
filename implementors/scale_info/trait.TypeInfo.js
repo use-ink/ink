de24342f42ea205edb0cafbe82083ba2b0a7ecc3
devops-parity <devops-team@parity.io>
2020-10-08 19:13:29 +0000

    Updated docs for master and pushed to gh-pages

--- implementors/scale_info/trait.TypeInfo.js
+++ implementors/scale_info/trait.TypeInfo.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["ink_env"] = [{"text":"impl TypeInfo for DefaultEnvTypes","synthetic":false,"types":[]},{"text":"impl TypeInfo for AccountId","synthetic":false,"types":[]},{"text":"impl TypeInfo for Hash","synthetic":false,"types":[]}];
+implementors["ink_env"] = [{"text":"impl TypeInfo for DefaultEnvironment","synthetic":false,"types":[]},{"text":"impl TypeInfo for AccountId","synthetic":false,"types":[]},{"text":"impl TypeInfo for Hash","synthetic":false,"types":[]}];
 implementors["ink_primitives"] = [{"text":"impl TypeInfo for Key","synthetic":false,"types":[]}];
 implementors["ink_storage"] = [{"text":"impl TypeInfo for DynamicAllocation","synthetic":false,"types":[]},{"text":"impl TypeInfo for VacantEntry","synthetic":false,"types":[]},{"text":"impl&lt;T:&nbsp;TypeInfo + 'static&gt; TypeInfo for Entry&lt;T&gt;","synthetic":false,"types":[]}];
 implementors["scale_info"] = [];
