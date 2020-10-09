93c6dc751cbac903c1537935ecb2410b33fd1909
Robin Freyler <robin.freyler@gmail.com>
2020-10-09 20:34:01 +0200

    [lang, storage] add extern crate proc_macro
    This is somehow needed for cargo publishing.

--- crates/storage/derive/src/lib.rs
+++ crates/storage/derive/src/lib.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+extern crate proc_macro;
+
 mod packed_layout;
 mod spread_layout;
 mod storage_layout;
