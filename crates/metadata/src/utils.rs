e31840a3777373fdc3b0f8a4491c588e110f13f9
Andrew Jones <ascjones@gmail.com>
2020-10-15 15:40:31 +0100

    Add 0x prefix if missing

--- crates/metadata/src/utils.rs
+++ crates/metadata/src/utils.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use crate::serde_hex;
+
 /// Serializes the given bytes as byte string.
 pub fn serialize_as_byte_str<S>(bytes: &[u8], serializer: S) -> Result<S::Ok, S::Error>
 where
@@ -21,5 +23,37 @@ where
         // Return empty string without prepended `0x`.
         return serializer.serialize_str("")
     }
-    crate::serde_hex::serialize(bytes, serializer)
+    serde_hex::serialize(bytes, serializer)
+}
+
+/// Deserializes the given hex string with optional `0x` prefix.
+pub fn deserialize_from_byte_str<'de, D>(deserializer: D) -> Result<Vec<u8>, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+{
+    struct Visitor;
+
+    impl<'b> serde::de::Visitor<'b> for Visitor {
+        type Value = Vec<u8>;
+
+        fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+            write!(formatter, "hex string with optional 0x prefix")
+        }
+
+        fn visit_str<E: serde::de::Error>(self, v: &str) -> Result<Self::Value, E> {
+            let result =
+                if v.starts_with("0x") {
+                    serde_hex::from_hex(v)
+                } else {
+                    serde_hex::from_hex(&format!("0x{}", v))
+                };
+            result.map_err(E::custom)
+        }
+
+        fn visit_string<E: serde::de::Error>(self, v: String) -> Result<Self::Value, E> {
+            self.visit_str(&v)
+        }
+    }
+
+    deserializer.deserialize_str(Visitor)
 }
