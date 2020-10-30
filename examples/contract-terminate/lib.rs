9778815967021aec21586e30cb2a4bfd9db9af8a
Michael Mueller <mich@elmueller.net>
2020-10-30 14:44:50 +0100

    [examples] Add contract-terminate

---
+++ examples/contract-terminate/lib.rs
@@ -0,0 +1,96 @@
+// Copyright 2018-2020 Parity Technologies (UK) Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! A smart contract which demonstrates behavior of the `self.env().terminate()`
+//! function. It terminates itself once `terminate_me()` is called.
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use ink_lang as ink;
+
+#[ink::contract]
+pub mod just_terminates {
+    /// No storage is needed for this simple contract.
+    #[ink(storage)]
+    pub struct JustTerminate {}
+
+    impl JustTerminate {
+        /// Creates a new instance of this contract.
+        #[ink(constructor)]
+        pub fn new() -> Self {
+            Self {}
+        }
+
+        /// Terminates with the caller as beneficiary.
+        #[ink(message)]
+        pub fn terminate_me(&mut self) {
+            self.env().terminate_contract(self.env().caller());
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        use ink_env::{
+            call,
+            test,
+        };
+        use ink_lang as ink;
+
+        #[ink::test]
+        fn terminating_works() {
+            // given
+            let accounts = default_accounts();
+            let contract_id = ink_env::test::get_current_contract_account_id::<
+                ink_env::DefaultEnvironment,
+            >()
+            .expect("Cannot get contract id");
+            set_sender(accounts.alice);
+            set_balance(contract_id, 100);
+            let mut contract = JustTerminate::new();
+
+            // when
+            let should_terminate = move || contract.terminate_me();
+
+            // then
+            ink_env::assert_contract_termination!(should_terminate, accounts.alice, 100);
+        }
+
+        fn default_accounts(
+        ) -> ink_env::test::DefaultAccounts<ink_env::DefaultEnvironment> {
+            ink_env::test::default_accounts::<ink_env::DefaultEnvironment>()
+                .expect("Off-chain environment should have been initialized already")
+        }
+
+        fn set_sender(sender: AccountId) {
+            let callee = ink_env::account_id::<ink_env::DefaultEnvironment>()
+                .unwrap_or([0x0; 32].into());
+            test::push_execution_context::<Environment>(
+                sender,
+                callee,
+                1000000,
+                1000000,
+                test::CallData::new(call::Selector::new([0x00; 4])), // dummy
+            );
+        }
+
+        fn set_balance(account_id: AccountId, balance: Balance) {
+            ink_env::test::set_account_balance::<ink_env::DefaultEnvironment>(
+                account_id, balance,
+            )
+            .expect("Cannot set account balance");
+        }
+    }
+}
