e96f22068de37cbe68f337a5353dfa3e2255202d
Michael Mueller <mich@elmueller.net>
2020-11-11 10:18:06 +0100

    Fail `build-all` if one of the individual builds fails

--- examples/delegator/build-all.sh
+++ examples/delegator/build-all.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -eu
+
 pushd accumulator && cargo +nightly contract build --generate code-only && popd &&
 pushd adder && cargo +nightly contract build --generate code-only && popd &&
 pushd subber && cargo +nightly contract build --generate code-only && popd &&
