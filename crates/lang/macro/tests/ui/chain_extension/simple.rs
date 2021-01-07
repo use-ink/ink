95fa7302b04ac76d61ab2106a365b189642fb161
Robin Freyler <robin.freyler@gmail.com>
2021-01-08 00:06:48 +0100

    [lang] update simple chain extension test
    Compiles now again.

--- crates/lang/macro/tests/ui/chain_extension/simple.rs
+++ crates/lang/macro/tests/ui/chain_extension/simple.rs
@@ -4,12 +4,32 @@ use ink_lang as ink;
 /// Custom chain extension to read to and write from the runtime.
 #[ink::chain_extension]
 pub trait RuntimeReadWrite {
+    type ErrorCode = ReadWriteErrorCode;
+
     /// Reads from runtime storage.
-    #[ink(extension = 1)]
-    fn read(key: &[u8]) -> Vec<u8>;
+    #[ink(extension = 1, expect_ok)]
+    fn read(key: &[u8]) -> Result<Vec<u8>, ReadWriteErrorCode>;
     /// Writes into runtime storage.
-    #[ink(extension = 2)]
-    fn write(key: &[u8], value: &[u8]);
+    #[ink(extension = 2, expect_ok)]
+    fn write(key: &[u8], value: &[u8]) -> Result<(), ReadWriteErrorCode>;
+}
+
+/// The shared error code for the read write chain extension.
+#[derive(Debug, Copy, Clone, PartialEq, Eq, scale::Encode, scale::Decode, scale_info::TypeInfo)]
+pub enum ReadWriteErrorCode {
+    KeyDoesNotExist,
+    CannotWriteToThatKey,
+}
+
+impl ink_env::chain_extension::FromStatusCode for ReadWriteErrorCode {
+    fn from_status_code(status_code: u32) -> Result<(), Self> {
+        match status_code {
+            0 => Ok(()),
+            1 => Err(Self::KeyDoesNotExist),
+            2 => Err(Self::CannotWriteToThatKey),
+            _ => panic!("encountered unknown status code")
+        }
+    }
 }
 
 pub enum CustomEnvironment {}
@@ -30,6 +50,8 @@ impl Environment for CustomEnvironment {
 
 #[ink::contract(env_types = crate::CustomEnvironment)]
 mod read_writer {
+    use super::ReadWriteErrorCode;
+
     #[ink(storage)]
     pub struct ReadWriter {}
 
@@ -40,7 +62,7 @@ mod read_writer {
         }
 
         #[ink(message)]
-        pub fn read(&self, key: Vec<u8>) -> Vec<u8> {
+        pub fn read(&self, key: Vec<u8>) -> Result<Vec<u8>, ReadWriteErrorCode> {
             self.env()
                 .extension()
                 .read(&key)
@@ -48,7 +70,7 @@ mod read_writer {
         }
 
         #[ink(message)]
-        pub fn write(&self, key: Vec<u8>, value: Vec<u8>) {
+        pub fn write(&self, key: Vec<u8>, value: Vec<u8>) -> Result<(), ReadWriteErrorCode> {
             self.env()
                 .extension()
                 .write(&key, &value)
