ca7d51242c65686b467b42494d8bf016c80f6fa2
Robin Freyler <robin.freyler@gmail.com>
2021-01-08 15:23:38 +0100

    extend chain extension test case to cover all cases

--- crates/lang/macro/tests/ui/chain_extension/simple.rs
+++ crates/lang/macro/tests/ui/chain_extension/simple.rs
@@ -9,16 +9,89 @@ pub trait RuntimeReadWrite {
     /// Reads from runtime storage.
     #[ink(extension = 1, expect_ok)]
     fn read(key: &[u8]) -> Result<Vec<u8>, ReadWriteErrorCode>;
+
+    /// Reads from runtime storage.
+    ///
+    /// Returns the number of bytes written and up to 16 bytes of the
+    /// read value. Unwritten bytes are set to 0.
+    ///
+    /// # Errors
+    ///
+    /// If the runtime storage cell stores a value that requires more than
+    /// 16 bytes.
+    #[ink(extension = 2)]
+    fn read_small(key: &[u8]) -> Result<(u32, [u8; 32]), ReadWriteError>;
+
     /// Writes into runtime storage.
-    #[ink(extension = 2, expect_ok)]
+    #[ink(extension = 3, expect_ok)]
     fn write(key: &[u8], value: &[u8]) -> Result<(), ReadWriteErrorCode>;
+
+    /// Returns the access allowed for the key for the caller.
+    #[ink(extension = 4, expect_ok, expect_output)]
+    fn access(key: &[u8]) -> Option<Access>;
+
+    /// Unlocks previously aquired permission to access key.
+    ///
+    /// # Errors
+    ///
+    /// If the permission was not granted.
+    #[ink(extension = 5, expect_output)]
+    fn unlock_access(key: &[u8], access: Access) -> Result<(), UnlockAccessError>;
 }
 
 /// The shared error code for the read write chain extension.
-#[derive(Debug, Copy, Clone, PartialEq, Eq, scale::Encode, scale::Decode, scale_info::TypeInfo)]
+#[derive(
+    Debug, Copy, Clone, PartialEq, Eq, scale::Encode, scale::Decode, scale_info::TypeInfo,
+)]
 pub enum ReadWriteErrorCode {
     KeyDoesNotExist,
-    CannotWriteToThatKey,
+    CannotWriteToKey,
+    CannotReadFromKey,
+}
+
+/// Returned by `read_small` in case there were too few bytes available in the buffer.
+///
+/// Provides the number of bytes required to read the storage cell.
+#[derive(
+    Debug, Copy, Clone, PartialEq, Eq, scale::Encode, scale::Decode, scale_info::TypeInfo,
+)]
+pub enum ReadWriteError {
+    ErrorCode(ReadWriteErrorCode),
+    BufferTooSmall { required_bytes: u32 },
+}
+
+impl From<ReadWriteErrorCode> for ReadWriteError {
+    fn from(error_code: ReadWriteErrorCode) -> Self {
+        Self::ErrorCode(error_code)
+    }
+}
+
+impl From<scale::Error> for ReadWriteError {
+    fn from(_: scale::Error) -> Self {
+        panic!("encountered unexpected invalid SCALE encoding")
+    }
+}
+
+/// Returned by `unlock_access` if permission to access key was not granted with reason.
+#[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode, scale_info::TypeInfo)]
+pub struct UnlockAccessError {
+    reason: String,
+}
+
+impl From<scale::Error> for UnlockAccessError {
+    fn from(_: scale::Error) -> Self {
+        panic!("encountered unexpected invalid SCALE encoding")
+    }
+}
+
+/// The kind of access allows for a storage cell.
+#[derive(
+    Debug, Copy, Clone, PartialEq, Eq, scale::Encode, scale::Decode, scale_info::TypeInfo,
+)]
+pub enum Access {
+    ReadWrite,
+    ReadOnly,
+    WriteOnly,
 }
 
 impl ink_env::chain_extension::FromStatusCode for ReadWriteErrorCode {
@@ -26,8 +99,9 @@ impl ink_env::chain_extension::FromStatusCode for ReadWriteErrorCode {
         match status_code {
             0 => Ok(()),
             1 => Err(Self::KeyDoesNotExist),
-            2 => Err(Self::CannotWriteToThatKey),
-            _ => panic!("encountered unknown status code")
+            2 => Err(Self::CannotWriteToKey),
+            3 => Err(Self::CannotReadFromKey),
+            _ => panic!("encountered unknown status code"),
         }
     }
 }
@@ -41,8 +115,7 @@ impl Environment for CustomEnvironment {
     type AccountId = <ink_env::DefaultEnvironment as Environment>::AccountId;
     type Balance = <ink_env::DefaultEnvironment as Environment>::Balance;
     type Hash = <ink_env::DefaultEnvironment as Environment>::Hash;
-    type BlockNumber =
-        <ink_env::DefaultEnvironment as Environment>::BlockNumber;
+    type BlockNumber = <ink_env::DefaultEnvironment as Environment>::BlockNumber;
     type Timestamp = <ink_env::DefaultEnvironment as Environment>::Timestamp;
 
     type ChainExtension = RuntimeReadWrite;
@@ -70,7 +143,11 @@ mod read_writer {
         }
 
         #[ink(message)]
-        pub fn write(&self, key: Vec<u8>, value: Vec<u8>) -> Result<(), ReadWriteErrorCode> {
+        pub fn write(
+            &self,
+            key: Vec<u8>,
+            value: Vec<u8>,
+        ) -> Result<(), ReadWriteErrorCode> {
             self.env()
                 .extension()
                 .write(&key, &value)
