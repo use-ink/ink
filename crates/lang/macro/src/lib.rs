8a0a9bff68a1a6ed677ba2967f03ccd113cc3326
Robin Freyler <robin.freyler@gmail.com>
2021-01-11 14:16:32 +0100

    add docs to #[ink::chain_extension] proc. macro

--- crates/lang/macro/src/lib.rs
+++ crates/lang/macro/src/lib.rs
@@ -635,6 +635,302 @@ pub fn test(attr: TokenStream, item: TokenStream) -> TokenStream {
     ink_test::generate(attr.into(), item.into()).into()
 }
 
+/// Defines the interface for a chain extension.
+///
+/// # Structure
+///
+/// The interface consists of an error code that indicates lightweight errors
+/// as well as the definition of some chain extension methods.
+///
+/// The overall structure follows that of a simple Rust trait definition.
+/// The error code is defined as an associated type definition of the trait definition.
+/// The methods are defined as associated trait methods without implementation.
+///
+/// Chain extension methods must not have a `self` receiver such as `&self` or `&mut self`
+/// and must have inputs and output that implement SCALE codec. Their return value follows
+/// specific rules that can be altered using the `expect_output` and `expect_ok` attributes
+/// which are described in more detail below.
+///
+/// # Usage
+///
+/// Usually the chain extension definition using this proc. macro is provided
+/// by the author of the chain extension in a separate crate.
+/// ink! smart contracts using this chain extension simple depend on this crate
+/// and use its associated environment definition in order to make use of
+/// the methods provided by the chain extension.
+///
+/// # Attributes
+///
+/// There are three different attributes with which the chain extension methods
+/// can be flagged:
+///
+/// | Attribute | Required | Description |
+/// |:----------|:--------:|:------------|
+/// | `ink(extension = N: u32)` | Yes | Determines the unique function ID of the chain extension method. |
+/// | `ink(expect_output)` | Optional | Assumes that the returned status code of the chain extension method always indicates success and therefore always loads and decodes the output buffer of the call. |
+/// | `ink(expect_ok)` | Optional | By default chain extension methods are assumed to return a `Result<T, E>` in the output buffer. Using `expect_ok` this check is disabled and the chain extension method may return any other type. |
+///
+/// As with all ink! attributes multiple of them can either appear in a contiguous list:
+/// ```
+/// # type Access = i32;
+/// # use ink_lang as ink;
+/// # #[ink::chain_extension]
+/// # pub trait MyChainExtension {
+/// #     type ErrorCode = i32;
+/// #[ink(extension = 5, expect_output, expect_ok)]
+/// fn key_access_for_account(key: &[u8], account: &[u8]) -> Access;
+/// # }
+/// ```
+/// ... or as multiple stand alone ink! attributes applied to the same item:
+/// ```
+/// # type Access = i32;
+/// # use ink_lang as ink;
+/// # #[ink::chain_extension]
+/// # pub trait MyChainExtension {
+/// #     type ErrorCode = i32;
+/// #[ink(extension = 5)]
+/// #[ink(expect_output)]
+/// #[ink(expect_ok)]
+/// fn key_access_for_account(key: &[u8], account: &[u8]) -> Access;
+/// # }
+/// ```
+///
+/// ## Details: `expect_output`
+///
+/// By default all chain extension methods return a `Result<T, E>` where `E: From<Self::ErrorCode>`.
+/// The `Self::ErrorCode` represents the error code of the chain extension.
+/// This means that a smart contract calling such a chain extension method first queries the returned
+/// status code of the chain extension method and only loads and decodes the output if the retuend
+/// status code indicates a successful call.
+/// This design was chosen as it is a more efficient for when no output besides the error
+/// code is required for a chain extension call. When designing a chain extension try to utilize the
+/// error code to return errors and only use the output buffer for information that does not fit in
+/// a single `u32` value.
+///
+/// A chain extension method that is flagged with `expect_output` assumes that the returned error code
+/// will always indicate success. Therefore it will always load and decode the output buffer and loses
+/// the `E: From<Self::ErrorCode` constraint for the call.
+///
+/// ## Details: `expect_ok`
+///
+/// By default chain extension methods are assumed to return a value of type `Result<T, E>` through the
+/// output buffer. Using `expect_ok` this check is disabled and the chain extension method may return
+/// any other type.
+///
+/// Note that if a chain extension method is attributed with `expect_ok` but not with `expect_output` it
+/// will still return a value of type `Result<T, Self::ErrorCode>`.
+///
+/// ## Usage: `expect_output` + `expect_ok`
+///
+/// Use both `expect_output` and `expect_ok` for the same chain extension method if a call to it
+/// may never fail and never returns a `Result` type.
+///
+/// # Combinations
+///
+/// Due to the possibility to flag a chain extension method with `expect_output` and `expect_ok`
+/// there are 4 different cases with slightly varying semantics:
+///
+/// | `expect_output` | `expect_ok` | Effects |
+/// |:---------------:|:-----------:|:--------|
+/// | `false`         | `false`     | The chain extension method is required to return a value of type `Result<T, E>` where `E: From<Self::ErrorCode>`. A call will always check if the returned status code indicates success and only then will load and decode the value in the output buffer. |
+/// | `false`         | `true`      | The chain extension method may return any non-`Result` type. A call will always check if the returned status code indicates success and only then will load and decode the value in the output buffer. The actual return type of the chain extension method is still `Result<T, Self::ErrorCode>` when the chain extension method was defined to return a value of type `T`. |
+/// | `true`          | `false`     | The chain extension method is required to return a value of type `Result<T, E>`. A call will always assume that the returned status code indicates success and therefore always load and decode the output buffer directly. |
+/// | `true`          | `true`      | The chain extension method may return any non-`Result` type. A call will always assume that the returned status code indicates success and therefore always load and decode the output buffer directly. |
+///
+/// # Error Code
+///
+/// Every chain extension defines exactly one `ErrorCode` using the following syntax:
+///
+/// ```
+/// use ink_lang as ink;
+///
+/// #[ink::chain_extension]
+/// pub trait MyChainExtension {
+///     type ErrorCode = MyErrorCode;
+///
+///     // more definitions ...
+/// }
+/// ```
+///
+/// The defined `ErrorCode` must implement `FromStatusCode` which should be implemented as a
+/// more or less trivial conversion from the `u32` status code to a `Result<(), Self::ErrorCode>`.
+/// The `Ok(())` value indicates that the call to the chain extension method was successful.
+///
+/// By convention an error code of `0` represents success.
+/// However, chain extension authors may use whatever suits their needs.
+///
+/// # Example: Definition
+///
+/// In the below example a chain extension is defined that allows its users to read and write
+/// from and to the runtime storage using access privileges:
+///
+/// ```no_compile
+/// use ink_lang as ink;
+///
+/// /// Custom chain extension to read to and write from the runtime.
+/// #[ink::chain_extension]
+/// pub trait RuntimeReadWrite {
+///     type ErrorCode = ReadWriteErrorCode;
+///
+///     /// Reads from runtime storage.
+///     ///
+///     /// # Note
+///     ///
+///     /// Actually returns a value of type `Result<Vec<u8>, Self::ErrorCode>`.
+///     #[ink(extension = 1, expect_ok)]
+///     fn read(key: &[u8]) -> Vec<u8>;
+///
+///     /// Reads from runtime storage.
+///     ///
+///     /// Returns the number of bytes read and up to 32 bytes of the
+///     /// read value. Unused bytes in the output are set to 0.
+///     ///
+///     /// # Errors
+///     ///
+///     /// If the runtime storage cell stores a value that requires more than
+///     /// 32 bytes.
+///     ///
+///     /// # Note
+///     ///
+///     /// This requires `ReadWriteError` to implement `From<ReadWriteErrorCode>`
+///     /// and may potentially return any `Self::ErrorCode` through its return value.
+///     #[ink(extension = 2)]
+///     fn read_small(key: &[u8]) -> Result<(u32, [u8; 32]), ReadWriteError>;
+///
+///     /// Writes into runtime storage.
+///     ///
+///     /// # Note
+///     ///
+///     /// Actually returns a value of type `Result<(), Self::ErrorCode>`.
+///     #[ink(extension = 3, expect_ok)]
+///     fn write(key: &[u8], value: &[u8]);
+///
+///     /// Returns the access allowed for the key for the caller.
+///     ///
+///     /// # Note
+///     ///
+///     /// Assumes to never fail the call and therefore always returns `Option<Access`.
+///     #[ink(extension = 4, expect_ok, expect_output)]
+///     fn access(key: &[u8]) -> Option<Access>;
+///
+///     /// Unlocks previously aquired permission to access key.
+///     ///
+///     /// # Errors
+///     ///
+///     /// If the permission was not granted.
+///     ///
+///     /// # Note
+///     ///
+///     /// Assumes the call to never fail and therefore does _NOT_ require `UnlockAccessError`
+///     /// to implement `From<Self::ErrorCode>` as in the `read_small` method above.
+///     #[ink(extension = 5, expect_output)]
+///     fn unlock_access(key: &[u8], access: Access) -> Result<(), UnlockAccessError>;
+/// }
+/// ```
+///
+/// All the error types and other utility types used in the chain extension definition
+/// above are often required to implement various traits such as SCALE's `Encode` and `Decode`
+/// as well as `scale-info`'s `TypeInfo` trait.
+///
+/// A full example of the above chain extension definition can be seen
+/// [here](https://github.com/paritytech/ink/blob/robin-impl-chain-extension/crates/lang/macro/tests/ui/chain_extension/simple.rs).
+///
+/// # Example: Environment
+///
+/// In order to allow ink! smart contracts to use the above defined chain extension it needs
+/// to be integrated into an `Environment` definition as shown below:
+///
+/// ```
+/// # type RuntimeReadWrite = i32;
+/// #
+/// use ink_env::{Environment, DefaultEnvironment};
+///
+/// pub enum CustomEnvironment {}
+///
+/// impl Environment for CustomEnvironment {
+///     const MAX_EVENT_TOPICS: usize =
+///         <DefaultEnvironment as Environment>::MAX_EVENT_TOPICS;
+///
+///     type AccountId = <DefaultEnvironment as Environment>::AccountId;
+///     type Balance = <DefaultEnvironment as Environment>::Balance;
+///     type Hash = <DefaultEnvironment as Environment>::Hash;
+///     type BlockNumber = <DefaultEnvironment as Environment>::BlockNumber;
+///     type Timestamp = <DefaultEnvironment as Environment>::Timestamp;
+///
+///     type ChainExtension = RuntimeReadWrite;
+/// }
+/// ```
+///
+/// Above we defined the `CustomEnvironment` which defaults to ink!'s `DefaultEnvironment`
+/// for all constants and types but the `ChainExtension` type which is assigned to our newly
+/// defined chain extension.
+///
+/// # Example: Usage
+///
+/// An ink! smart contract can use the above defined chain extension through the `Environment`
+/// definition defined in the last example section using the `env_types` macro parameter as
+/// shown below:
+///
+/// ```no_compile
+/// #[ink::contract(env_types = CustomEnvironment)]
+/// mod read_writer {
+///     use super::{Access, ReadWriteErrorCode, ReadWriteError, UnlockAccessError};
+///
+///     #[ink(storage)]
+///     pub struct ReadWriter {}
+///
+///     impl ReadWriter {
+///         #[ink(constructor)]
+///         pub fn new() -> Self { Self {} }
+///
+///         #[ink(message)]
+///         pub fn read(&self, key: Vec<u8>) -> Result<Vec<u8>, ReadWriteErrorCode> {
+///             self.env()
+///                 .extension()
+///                 .read(&key)
+///         }
+///
+///         #[ink(message)]
+///         pub fn read_small(&self, key: Vec<u8>) -> Result<(u32, [u8; 32]), ReadWriteError> {
+///             self.env()
+///                 .extension()
+///                 .read_small(&key)
+///         }
+///
+///         #[ink(message)]
+///         pub fn write(
+///             &self,
+///             key: Vec<u8>,
+///             value: Vec<u8>,
+///         ) -> Result<(), ReadWriteErrorCode> {
+///             self.env()
+///                 .extension()
+///                 .write(&key, &value)
+///         }
+///
+///         #[ink(message)]
+///         pub fn access(&self, key: Vec<u8>) -> Option<Access> {
+///             self.env()
+///                 .extension()
+///                 .access(&key)
+///         }
+///
+///         #[ink(message)]
+///         pub fn unlock_access(&self, key: Vec<u8>, access: Access) -> Result<(), UnlockAccessError> {
+///             self.env()
+///                 .extension()
+///                 .unlock_access(&key, access)
+///         }
+///     }
+/// }
+/// ```
+///
+/// # Technical Limitations
+///
+/// Due to technical limitations it is not possible to refer to the `ErrorCode` associated type
+/// using `Self::ErrorCode` anywhere within the chain extension and its defined methods.
+/// Instead chain extension authors should directly use the error code type when required.
+/// This limitation might be lifted in future versions of ink!.
 #[proc_macro_attribute]
 pub fn chain_extension(attr: TokenStream, item: TokenStream) -> TokenStream {
     chain_extension::generate(attr.into(), item.into()).into()
