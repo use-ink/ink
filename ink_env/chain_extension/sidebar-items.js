452e91eaca6c159d72cdff83388ed7adfaf1f69c
devops-parity <devops-team@parity.io>
2021-01-12 19:42:59 +0000

    Updated docs for master and pushed to gh-pages

---
+++ ink_env/chain_extension/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"mod":[["state","Type states of the chain extension method instance."]],"struct":[["ChainExtensionMethod","A concrete instance of a chain extension method."]],"trait":[["FromStatusCode","Implemented by error codes in order to construct them from status codes."]]});
