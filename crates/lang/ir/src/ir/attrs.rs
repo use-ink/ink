fa010044696903249b14b6fa4040b686e081f42a
Hero Bird <robin.freyler@gmail.com>
2021-01-13 09:33:33 +0100

    fix docs (review suggestion)
    Co-authored-by: Michael Müller <michi@parity.io>

--- crates/lang/ir/src/ir/attrs.rs
+++ crates/lang/ir/src/ir/attrs.rs
@@ -577,7 +577,7 @@ where
 ///
 /// The `is_conflicting_attr` closure returns `Ok` if the attribute does not conflict,
 /// returns `Err(None)` if the attribute conflicts but without providing further reasoning
-/// and `Err(Some(reason))` if the attribute conflict given additional context information.
+/// and `Err(Some(reason))` if the attribute conflicts given additional context information.
 ///
 /// # Errors
 ///
