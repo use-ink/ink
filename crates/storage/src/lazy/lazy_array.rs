f35e08fc595c0f8ee460e0429de6c33a80c09442
Michael Mueller <mich@elmueller.net>
2021-02-04 11:51:12 +0100

    Allow dead code in `LazyArray` when `ink-unstable` missing

--- crates/storage/src/lazy/lazy_array.rs
+++ crates/storage/src/lazy/lazy_array.rs
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![cfg(not(feature = "ink-unstable"))]
+#![allow(dead_code)]
+
 use super::{
     CacheCell,
     EntryState,
