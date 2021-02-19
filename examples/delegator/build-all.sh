feb7bc86b3e69466899087a64ae58e1ff18761d7
Michael Mueller <mich@elmueller.net>
2021-02-19 13:53:58 +0100

    Make Andrew happy

--- examples/delegator/build-all.sh
+++ examples/delegator/build-all.sh
@@ -5,4 +5,4 @@ set -eu
 cargo +nightly contract build --manifest-path accumulator/Cargo.toml
 cargo +nightly contract build --manifest-path adder/Cargo.toml
 cargo +nightly contract build --manifest-path subber/Cargo.toml
-cargo +nightly contract build --manifest-path Cargo.toml
+cargo +nightly contract build
