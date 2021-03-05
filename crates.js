5a0d762e1321aebbf909e94b9d47e41110c26a4b
paritytech-ci <devops-team@parity.io>
2021-03-05 18:46:28 +0000

    Updated docs for master and pushed to gh-pages

--- crates.js
+++
@@ -1,1 +0,0 @@
-window.ALL_CRATES = ["ink_env","ink_lang","ink_lang_codegen","ink_lang_ir","ink_lang_macro","ink_metadata","ink_prelude","ink_primitives","ink_storage","ink_storage_derive","scale_info"];
