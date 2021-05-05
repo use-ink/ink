c2f18cd8e1a180819ba7f5ad7c00370dc51f7a1a
Robin Freyler <robin.freyler@gmail.com>
2021-05-05 16:49:25 +0200

    add new utility traits for upcoming ink! codegen

--- crates/lang/src/traits.rs
+++ crates/lang/src/traits.rs
@@ -161,3 +161,80 @@ unsafe impl<E, const N: usize> CheckedInkTrait<[(); N]> for ConcreteImplementers
 /// Read more about its use [here][TraitDefinitionRegistry].
 #[doc(hidden)]
 pub enum NoConcreteImplementer {}
+
+/// The global call builder type for an ink! trait definition.
+pub trait TraitCallBuilder {
+    /// The call builder type.
+    type Builder;
+
+    /// Returns a shared reference to the global call builder type.
+    ///
+    /// This allows to call `&self` ink! trait messages.
+    fn call(&self) -> &Self::Builder;
+
+    /// Returns an exclusive reference to the global call builder type.
+    ///
+    /// This allows to call any ink! trait message.
+    fn call_mut(&mut self) -> &mut Self::Builder;
+}
+
+/// Implemented by the global trait info provider.
+///
+/// This communicates the `u32` number that uniquely identifies
+/// the ink! trait definition.
+pub trait TraitUniqueId {
+    /// The unique trait `u32` identifier.
+    const ID: u32;
+}
+
+/// Implemented by the global trait info provider.
+///
+/// It is used to query the global trait call forwarder.
+/// There is one global trait call forwarder that implements
+/// the call forwarding (short- and long-form) for all calls
+/// to this trait in `ink-as-dependency` configuration.
+pub trait TraitCallForwarder {
+    /// The call forwarder type.
+    type Forwarder: TraitCallBuilder;
+}
+
+/// Implemented by call builders of smart contracts.
+///
+/// These might be implementing multiple different ink! traits.
+/// The codegen makes them implement this trait once for every
+/// ink! trait they have to implement.
+///
+/// While the trait is not necessary it encapsulates a lot of
+/// utility and auxiliary code required for the actual ink! trait
+/// implementations.
+pub trait TraitCallForwarderFor<const ID: u32> {
+    type Forwarder: TraitCallBuilder;
+
+    /// Forwards the `&self` call.
+    ///
+    /// # Note
+    ///
+    /// This is used for the short-hand calling syntax.
+    fn forward(&self) -> &Self::Forwarder;
+
+    /// Forwards the `&mut self` call.
+    ///
+    /// # Note
+    ///
+    /// This is used for the short-hand calling syntax.
+    fn forward_mut(&mut self) -> &mut Self::Forwarder;
+
+    /// Builds up the `&self` call.
+    ///
+    /// # Note
+    ///
+    /// This is used for the long-hand calling syntax.
+    fn build(&self) -> &<Self::Forwarder as TraitCallBuilder>::Builder;
+
+    /// Builds up the `&mut self` call.
+    ///
+    /// # Note
+    ///
+    /// This is used for the long-hand calling syntax.
+    fn build_mut(&mut self) -> &mut <Self::Forwarder as TraitCallBuilder>::Builder;
+}
