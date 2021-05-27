0cb9ee40524e8979869b6371091e41baaf1bfea0
Robin Freyler <robin.freyler@gmail.com>
2021-05-27 09:49:37 +0200

    make use of enforced_error module

--- crates/lang/codegen/src/generator/cross_calling.rs
+++ crates/lang/codegen/src/generator/cross_calling.rs
@@ -28,45 +28,7 @@ use quote::{
     quote_spanned,
 };
 use syn::spanned::Spanned as _;
-
-/// Errors which may occur when forwarding a call is not allowed.
-///
-/// We insert markers for these errors in the generated contract code.
-/// This is necessary since we can't check these errors at compile time
-/// of the contract.
-/// `cargo-contract` checks the contract code for these error markers
-/// when building a contract and fails if it finds markers.
-#[derive(scale::Encode, scale::Decode)]
-pub enum EnforcedErrors {
-    /// The below error represents calling a `&mut self` message in a context that
-    /// only allows for `&self` messages. This may happen under certain circumstances
-    /// when ink! trait implementations are involved with long-hand calling notation.
-    #[codec(index = 1)]
-    CannotCallTraitMessage {
-        /// The trait that defines the called message.
-        trait_ident: String,
-        /// The name of the called message.
-        message_ident: String,
-        /// The selector of the called message.
-        message_selector: [u8; 4],
-        /// Is `true` if the `self` receiver of the ink! message is `&mut self`.
-        message_is_mut: bool,
-    },
-    /// The below error represents calling a constructor in a context that does
-    /// not allow calling it. This may happen when the constructor defined in a
-    /// trait is cross-called in another contract.
-    /// This is not allowed since the contract to which a call is forwarded must
-    /// already exist at the point when the call to it is made.
-    #[codec(index = 2)]
-    CannotCallTraitConstructor {
-        /// The trait that defines the called constructor.
-        trait_ident: String,
-        /// The name of the called constructor.
-        constructor_ident: String,
-        /// The selector of the called constructor.
-        constructor_selector: [u8; 4],
-    },
-}
+use super::enforced_error::EnforcedErrors;
 
 /// Generates `#[cfg(..)]` code to guard against compilation under `ink-as-dependency`.
 #[derive(From)]
