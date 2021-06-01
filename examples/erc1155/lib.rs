a71ab5043a8b7e89ed409124f3a68b1a132916d8
Hernando Castano <hernando@hcastano.com>
2021-06-01 14:56:37 -0400

    Add basic contract skeleton

---
+++ examples/erc1155/lib.rs
@@ -0,0 +1,51 @@
+// Copyright 2018-2021 Parity Technologies (UK) Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use ink_lang as ink;
+
+#[ink::contract]
+mod erc1155 {
+    /// An ERC-1155 contract.
+    #[ink(storage)]
+    pub struct Erc1155 {}
+
+    impl Erc1155 {
+        #[ink(constructor)]
+        pub fn new() -> Self {
+            Self {}
+        }
+
+        #[ink(message)]
+        pub fn foo(&self) {
+            todo!()
+        }
+    }
+
+    /// Unit tests.
+    #[cfg(not(feature = "ink-experimental-engine"))]
+    #[cfg(test)]
+    mod tests {
+        /// Imports all the definitions from the outer scope so we can use them here.
+        use super::*;
+
+        use ink_lang as ink;
+
+        #[ink::test]
+        fn default_works() {
+            assert!(true);
+        }
+    }
+}
