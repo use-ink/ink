82adb2f92faa763df91d2c3d32ed1e052041f434
Hernando Castano <hernando@hcastano.com>
2021-06-02 17:54:38 -0400

    Implement simple token transfer

--- examples/erc1155/lib.rs
+++ examples/erc1155/lib.rs
@@ -23,7 +23,14 @@ type Balance = <ink_env::DefaultEnvironment as ink_env::Environment>::Balance;
 #[ink::trait_definition]
 pub trait Erc1155 {
     #[ink(message)]
-    fn safe_transfer_from(&mut self);
+    fn safe_transfer_from(
+        &mut self,
+        from: ink_env::AccountId,
+        to: ink_env::AccountId,
+        token_id: TokenId,
+        value: Balance,
+        data: Vec<u8>,
+    );
 
     #[ink(message)]
     fn safe_batch_transfer_from(&mut self);
@@ -48,7 +55,14 @@ pub trait Erc1155 {
 #[ink::trait_definition]
 pub trait Erc1155TokenReceiver {
     #[ink(message)]
-    fn on_erc_1155_received(&mut self);
+    fn on_erc_1155_received(
+        &mut self,
+        operator: ink_env::AccountId,
+        from: ink_env::AccountId,
+        token_id: TokenId,
+        value: Balance,
+        data: Vec<u8>,
+    ) -> Vec<u8>;
 
     #[ink(message)]
     fn on_erc_1155_batch_received(&mut self);
@@ -59,6 +73,15 @@ mod erc1155 {
     use super::*;
 
     use ink_prelude::collections::BTreeMap;
+
+    #[ink(event)]
+    pub struct TransferSingle {
+        operator: AccountId,
+        from: AccountId,
+        to: AccountId,
+        token_id: TokenId,
+        value: Balance,
+    }
 
     /// An ERC-1155 contract.
     #[ink(storage)]
@@ -74,9 +97,93 @@ mod erc1155 {
     }
 
     impl super::Erc1155 for Contract {
+        /*
+            @notice Transfers `_value` amount of an `_id` from the `_from` address to the `_to` address specified (with safety call).
+            @dev Caller must be approved to manage the tokens being transferred out of the `_from`
+                 account (see "Approval" section of the standard).
+            MUST revert if `_to` is the zero address.
+            MUST revert if balance of holder for token `_id` is lower than the `_value` sent.
+            MUST revert on any other error.
+            MUST emit the `TransferSingle` event to reflect the balance change (see "Safe Transfer Rules" section of the standard).
+                After the above conditions are met, this function MUST check if `_to` is a smart contract
+                (e.g. code size > 0). If so, it MUST call `onERC1155Received` on `_to` and act
+                appropriately (see "Safe Transfer Rules" section of the standard).
+
+            @param _from    Source address
+            @param _to      Target address
+            @param _id      ID of the token type
+            @param _value   Transfer amount
+            @param _data    Additional data with no specified format, MUST be sent unaltered in call to `onERC1155Received` on `_to`
+        */
         #[ink(message)]
-        fn safe_transfer_from(&mut self) {
-            todo!()
+        fn safe_transfer_from(
+            &mut self,
+            from: ink_env::AccountId,
+            to: ink_env::AccountId,
+            token_id: TokenId,
+            value: Balance,
+            data: Vec<u8>,
+        ) {
+            // TODO: Need to make sure self.env().caller is "Approved" on behalf of `from`
+
+            // Q: Would a call be reverted if I return an Error vs. just panicking?
+            assert!(
+                to != AccountId::default(),
+                "Cannot send tokens to the zero-address."
+            );
+
+            assert!(
+                self.balance_of(from, token_id) >= value,
+                "Insufficent token balance for transfer."
+            );
+
+            self.balances
+                .get_mut(&from)
+                .and_then(|b| b.get_mut(&token_id))
+                .and_then(|b| Some(*b -= value));
+
+            self.balances
+                .get_mut(&to)
+                .and_then(|b| b.get_mut(&token_id))
+                .and_then(|b| Some(*b += value));
+
+            self.env().emit_event(TransferSingle {
+                operator: self.env().caller(),
+                from: from.clone(),
+                to: to.clone(),
+                token_id,
+                value,
+            });
+
+            // We call this _after_ the balance has been updated and the event has been fired
+            //
+            // Check if `to` is a smart contract
+            // use ink_env::call::{build_call, ExecutionInput, Selector};
+            // let magic_value = if let Err(e) = build_call::<ink_env::DefaultEnvironment>()
+            //     .callee(to)
+            //     .gas_limit(5000)
+            //     .transferred_value(10)
+            //     .exec_input(ExecutionInput::new(Selector::new([0; 4])))
+            //     .returns::<()>()
+            //     .fire()
+            // {
+            //     match e {
+            //         ink_env::Error::CodeNotFound => self.on_erc_1155_received(
+            //             self.env().caller(),
+            //             from,
+            //             token_id,
+            //             value,
+            //             data,
+            //         ),
+            //         _ => todo!("tbh, not sure"),
+            //     }
+            // } else {
+            //     ink_prelude::vec![]
+            // };
+
+            // if magic_value != ink_prelude::vec![0] {
+            //     todo!()
+            // }
         }
 
         #[ink(message)]
@@ -122,7 +229,14 @@ mod erc1155 {
 
     impl super::Erc1155TokenReceiver for Contract {
         #[ink(message)]
-        fn on_erc_1155_received(&mut self) {
+        fn on_erc_1155_received(
+            &mut self,
+            operator: ink_env::AccountId,
+            from: ink_env::AccountId,
+            token_id: TokenId,
+            value: Balance,
+            data: Vec<u8>,
+        ) -> Vec<u8> {
             todo!()
         }
 
@@ -191,6 +305,61 @@ mod erc1155 {
                 erc.balance_of_batch(vec![alice, bob, charlie], vec![1, 2]),
                 vec![10, 20, 10, 20, 0, 0]
             );
+        }
+
+        #[ink::test]
+        fn can_send_tokens_between_accounts() {
+            let alice: AccountId = [1; 32].into();
+            let bob: AccountId = [2; 32].into();
+
+            let mut balances = BTreeMap::new();
+            balances.insert(1, 10);
+            balances.insert(2, 20);
+
+            let mut accounts = BTreeMap::new();
+            accounts.insert(alice, balances.clone());
+            accounts.insert(bob, balances);
+
+            let mut erc = Contract::new(accounts);
+
+            erc.safe_transfer_from(alice, bob, 1, 5, vec![]);
+            assert_eq!(erc.balance_of(alice, 1), 5);
+            assert_eq!(erc.balance_of(bob, 1), 15);
+        }
+
+        #[ink::test]
+        #[should_panic]
+        fn sending_too_many_tokens_fails() {
+            let alice: AccountId = [1; 32].into();
+            let bob: AccountId = [2; 32].into();
+
+            let mut balances = BTreeMap::new();
+            balances.insert(1, 10);
+            balances.insert(2, 20);
+
+            let mut accounts = BTreeMap::new();
+            accounts.insert(alice, balances.clone());
+            accounts.insert(bob, balances);
+
+            let mut erc = Contract::new(accounts);
+
+            erc.safe_transfer_from(alice, bob, 1, 11, vec![]);
+        }
+
+        #[ink::test]
+        #[should_panic]
+        fn sending_tokens_to_zero_address_fails() {
+            let burn: AccountId = [0; 32].into();
+            let alice: AccountId = [1; 32].into();
+
+            let mut balances = BTreeMap::new();
+            balances.insert(1, 10);
+
+            let mut accounts = BTreeMap::new();
+            accounts.insert(alice, balances.clone());
+
+            let mut erc = Contract::new(accounts);
+            erc.safe_transfer_from(alice, burn, 1, 11, vec![]);
         }
     }
 }
