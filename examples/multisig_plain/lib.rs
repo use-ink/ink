690bbdd6a7767837da6c14e556c501a4bb585f00
Michael Mueller <mich@elmueller.net>
2021-06-17 08:17:00 +0200

    Revert "Enable sending payments to `fn invoke_transaction()`"
    This reverts commit 9b051d2c388522a69db3dac64287fa07d5955805.

--- examples/multisig_plain/lib.rs
+++ examples/multisig_plain/lib.rs
@@ -486,7 +486,7 @@ mod multisig_plain {
         ///
         /// Its return value indicates whether the called transaction was successful.
         /// This can be called by anyone.
-        #[ink(message, payable)]
+        #[ink(message)]
         pub fn invoke_transaction(
             &mut self,
             trans_id: TransactionId,
