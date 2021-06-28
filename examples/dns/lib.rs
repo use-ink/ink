0ad2ad37bee09952f69759b035eac263572e839b
Michael Mueller <mich@elmueller.net>
2021-06-28 18:48:35 +0200

    Make `gas` be of type `u64` instead of `Balance`

--- examples/dns/lib.rs
+++ examples/dns/lib.rs
@@ -185,7 +185,7 @@ mod dns {
 
         const DEFAULT_CALLEE_HASH: [u8; 32] = [0x07; 32];
         const DEFAULT_ENDOWMENT: Balance = 1_000_000;
-        const DEFAULT_GAS_LIMIT: Balance = 1_000_000;
+        const DEFAULT_GAS_LIMIT: u64 = 1_000_000;
 
         fn default_accounts(
         ) -> ink_env::test::DefaultAccounts<ink_env::DefaultEnvironment> {
