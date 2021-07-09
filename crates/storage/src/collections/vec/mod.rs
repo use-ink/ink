991177f15f0d33926586955bce02e85ef05ee586
Hernando Castano <hernando@hcastano.com>
2021-07-09 14:28:46 -0400

    RustFmt the doc examples

--- crates/storage/src/collections/vec/mod.rs
+++ crates/storage/src/collections/vec/mod.rs
@@ -223,7 +223,10 @@ where
     /// ```
     /// use ink_storage::Vec as StorageVec;
     ///
-    /// let s: StorageVec<i32> = [0, 1, 1, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55].into_iter().copied().collect();
+    /// let s: StorageVec<i32> = [0, 1, 1, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+    ///     .into_iter()
+    ///     .copied()
+    ///     .collect();
     ///
     /// assert_eq!(s.binary_search(&13),  Ok(9));
     /// assert_eq!(s.binary_search(&4),   Err(7));
@@ -266,7 +269,10 @@ where
     /// ```
     /// use ink_storage::Vec as StorageVec;
     ///
-    /// let s: StorageVec<i32> = [0, 1, 1, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55].into_iter().copied().collect();
+    /// let s: StorageVec<i32> = [0, 1, 1, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+    ///     .into_iter()
+    ///     .copied()
+    ///     .collect();
     ///
     /// let seek = 13;
     /// assert_eq!(s.binary_search_by(|probe| probe.cmp(&seek)), Ok(9));
@@ -337,9 +343,24 @@ where
     /// ```
     /// use ink_storage::Vec as StorageVec;
     ///
-    /// let s: StorageVec<(i32, i32)> = [(0, 0), (2, 1), (4, 1), (5, 1), (3, 1),
-    ///          (1, 2), (2, 3), (4, 5), (5, 8), (3, 13),
-    ///          (1, 21), (2, 34), (4, 55)].into_iter().copied().collect();
+    /// let s: StorageVec<(i32, i32)> = [
+    ///     (0, 0),
+    ///     (2, 1),
+    ///     (4, 1),
+    ///     (5, 1),
+    ///     (3, 1),
+    ///     (1, 2),
+    ///     (2, 3),
+    ///     (4, 5),
+    ///     (5, 8),
+    ///     (3, 13),
+    ///     (1, 21),
+    ///     (2, 34),
+    ///     (4, 55),
+    /// ]
+    /// .into_iter()
+    /// .copied()
+    /// .collect();
     ///
     /// assert_eq!(s.binary_search_by_key(&13, |&(a, b)| b),  Ok(9));
     /// assert_eq!(s.binary_search_by_key(&4, |&(a, b)| b),   Err(7));
