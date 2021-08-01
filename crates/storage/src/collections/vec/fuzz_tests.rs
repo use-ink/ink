470cc6df2845a5a81d01cf847386d981ef12f694
Michael Mueller <mich@elmueller.net>
2021-08-01 16:26:10 +0200

    Allow `type_complexity`

--- crates/storage/src/collections/vec/fuzz_tests.rs
+++ crates/storage/src/collections/vec/fuzz_tests.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![allow(clippy::type_complexity)]
+
 use super::Vec as StorageVec;
 use crate::{
     test_utils::FuzzCollection,
