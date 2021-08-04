1546e3caa681339dfded29d45a08d5834e4ac533
Michael Mueller <mich@elmueller.net>
2021-08-04 10:06:27 +0200

    Ignore test

--- crates/lang/macro/tests/unique_topics.rs
+++ crates/lang/macro/tests/unique_topics.rs
@@ -66,32 +66,33 @@ mod my_contract {
         use ink_env::test::EmittedEvent;
         use ink_lang as ink;
 
-        // // The following test unfortunately has to be deactivated until
-        // // https://github.com/rust-lang/cargo/issues/6915 is fixed or until
-        // // we make `ink-experimental-engine` the default.
-        // // The reason is that the mentioned issue currently prevents crates
-        // // from activating features in dev-dependencies of child crates.
-        // // See also the `Cargo.toml` of this crate.
-        // #[ink::test]
-        // #[cfg(feature = "ink-experimental-engine")]
-        // fn event_must_have_unique_topics() {
-        //     // given
-        //     let my_contract = MyContract::new();
-        //
-        //     // when
-        //     MyContract::emit_my_event(&my_contract);
-        //
-        //     // then
-        //     // all topics must be unique
-        //     let emitted_events =
-        //         ink_env::test::recorded_events().collect::<Vec<EmittedEvent>>();
-        //     let mut encoded_topics: std::vec::Vec<&[u8]> = emitted_events[0]
-        //         .topics
-        //         .iter()
-        //         .map(|topic| topic.as_slice())
-        //         .collect();
-        //     assert!(!has_duplicates(&mut encoded_topics));
-        // }
+        // The following test unfortunately has to be ignored until
+        // https://github.com/rust-lang/cargo/issues/6915 is fixed or until
+        // we make `ink-experimental-engine` the default.
+        // The reason is that the mentioned issue currently prevents crates
+        // from activating features in dev-dependencies of child crates.
+        // See also the `Cargo.toml` of this crate.
+        #[ignore]
+        #[ink::test]
+        #[cfg(feature = "ink-experimental-engine")]
+        fn event_must_have_unique_topics() {
+            // given
+            let my_contract = MyContract::new();
+
+            // when
+            MyContract::emit_my_event(&my_contract);
+
+            // then
+            // all topics must be unique
+            let emitted_events =
+                ink_env::test::recorded_events().collect::<Vec<EmittedEvent>>();
+            let mut encoded_topics: std::vec::Vec<&[u8]> = emitted_events[0]
+                .topics
+                .iter()
+                .map(|topic| topic.as_slice())
+                .collect();
+            assert!(!has_duplicates(&mut encoded_topics));
+        }
 
         #[ink::test]
         fn event_must_have_unique_topics() {
