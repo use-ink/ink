7e1c5b52c10ec0d6ac4579c1d81fc5d98931196b
paritytech-ci <devops-team@parity.io>
2021-09-21 15:32:26 +0000

    Updated docs for master and pushed to gh-pages

---
+++ implementors/ink_lang_ir/trait.HexLiteral.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["ink_lang_ir"] = [];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
