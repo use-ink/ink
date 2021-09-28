df3abd414911cf67ec1d86cbfc5811aeff9f59df
Robin Freyler <robin.freyler@gmail.com>
2021-09-28 12:54:11 +0200

    add UI tests for #[ink::trait_definition] with async and const messages

--- crates/lang/macro/tests/compile_tests.rs
+++ crates/lang/macro/tests/compile_tests.rs
@@ -122,4 +122,6 @@ fn trait_definition() {
     t.compile_fail("tests/ui/trait_def/fail/message_receiver_invalid_1.rs");
     t.compile_fail("tests/ui/trait_def/fail/message_receiver_invalid_2.rs");
     t.compile_fail("tests/ui/trait_def/fail/message_receiver_invalid_3.rs");
+    t.compile_fail("tests/ui/trait_def/fail/message_async_invalid.rs");
+    t.compile_fail("tests/ui/trait_def/fail/message_const_invalid.rs");
 }
