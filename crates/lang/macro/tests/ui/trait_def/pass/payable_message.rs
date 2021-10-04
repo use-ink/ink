95604c1aaca96d179b0667843a17a63c3f250281
Robin Freyler <robin.freyler@gmail.com>
2021-10-04 22:45:59 +0200

    modernize payable_message UI trait_def passing test

--- crates/lang/macro/tests/ui/trait_def/pass/payable_message.rs
+++ crates/lang/macro/tests/ui/trait_def/pass/payable_message.rs
@@ -15,55 +15,39 @@ pub trait PayableDefinition {
     fn unpayable_mut(&mut self);
 }
 
-/// Computed using `local_id("payable")`.
-const PAYABLE_ID: u32 = 0xFDDBE704;
+use ink_lang::selector_id;
 
-/// Computed using `local_id("payable_mut")`.
-const PAYABLE_MUT_ID: u32 = 0x1CE6275F;
-
-/// Computed using `local_id("unpayable")`.
-const UNPAYABLE_ID: u32 = 0x511647A5;
-
-/// Computed using `local_id("unpayable_mut")`.
-const UNPAYABLE_MUT_ID: u32 = 0x4A60F1E1;
+const PAYABLE_ID: u32 = selector_id!("payable");
+const PAYABLE_MUT_ID: u32 = selector_id!("payable_mut");
+const UNPAYABLE_ID: u32 = selector_id!("unpayable");
+const UNPAYABLE_MUT_ID: u32 = selector_id!("unpayable_mut");
 
 fn main() {
-    use ink_lang_ir as ir;
-    /// Returns the local ID for the given name.
-    #[allow(dead_code)]
-    fn local_id(name: &str) -> u32 {
-        ir::utils::local_message_id(&syn::Ident::new(
-            name,
-            proc_macro2::Span::call_site(),
-        ))
-    }
-    // Uncomment these in order to print out the local IDs of
-    // all the ink! trait messages for this test.
-    //
-    // println!("local_id(\"payable\")       = {:X}", local_id("payable"));
-    // println!("local_id(\"payable_mut\")   = {:X}", local_id("payable_mut"));
-    // println!("local_id(\"unpayable\")     = {:X}", local_id("unpayable"));
-    // println!("local_id(\"unpayable_mut\") = {:X}", local_id("unpayable_mut"));
+    use ink_env::DefaultEnvironment;
+    use ink_lang::reflect::{
+        TraitDefinitionRegistry,
+        TraitMessageInfo,
+    };
     assert!(
-        <<::ink_lang::reflect::TraitDefinitionRegistry<::ink_env::DefaultEnvironment>
+        <<TraitDefinitionRegistry<DefaultEnvironment>
             as PayableDefinition>::__ink_TraitInfo
-            as ::ink_lang::reflect::TraitMessageInfo<PAYABLE_ID>>::PAYABLE,
+            as TraitMessageInfo<PAYABLE_ID>>::PAYABLE,
     );
     assert!(
-        <<::ink_lang::reflect::TraitDefinitionRegistry<::ink_env::DefaultEnvironment>
+        <<TraitDefinitionRegistry<DefaultEnvironment>
             as PayableDefinition>::__ink_TraitInfo
-            as ::ink_lang::reflect::TraitMessageInfo<PAYABLE_MUT_ID>>::PAYABLE,
+            as TraitMessageInfo<PAYABLE_MUT_ID>>::PAYABLE,
     );
     assert_eq!(
-        <<::ink_lang::reflect::TraitDefinitionRegistry<::ink_env::DefaultEnvironment>
+        <<TraitDefinitionRegistry<DefaultEnvironment>
             as PayableDefinition>::__ink_TraitInfo
-            as ::ink_lang::reflect::TraitMessageInfo<UNPAYABLE_ID>>::PAYABLE,
+            as TraitMessageInfo<UNPAYABLE_ID>>::PAYABLE,
         false
     );
     assert_eq!(
-        <<::ink_lang::reflect::TraitDefinitionRegistry<::ink_env::DefaultEnvironment>
+        <<TraitDefinitionRegistry<DefaultEnvironment>
             as PayableDefinition>::__ink_TraitInfo
-            as ::ink_lang::reflect::TraitMessageInfo<UNPAYABLE_MUT_ID>>::PAYABLE,
+            as TraitMessageInfo<UNPAYABLE_MUT_ID>>::PAYABLE,
         false
     );
 }
