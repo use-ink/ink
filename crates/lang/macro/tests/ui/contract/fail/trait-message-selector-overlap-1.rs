60de455164240cc16479627c4809ee1fc8b4ae0f
Robin Freyler <robin.freyler@gmail.com>
2021-10-05 18:46:56 +0200

    add fail UI tests for ink! trait definitions concerning overlapping selectors

---
+++ crates/lang/macro/tests/ui/contract/fail/trait-message-selector-overlap-1.rs
@@ -0,0 +1,51 @@
+mod foo {
+    use ink_lang as ink;
+
+    #[ink::trait_definition]
+    pub trait TraitDefinition {
+        #[ink(message)]
+        fn message(&self);
+    }
+}
+
+mod bar {
+    use ink_lang as ink;
+
+    #[ink::trait_definition]
+    pub trait TraitDefinition {
+        #[ink(message)]
+        fn message(&self);
+    }
+}
+
+use ink_lang as ink;
+
+#[ink::contract]
+pub mod contract {
+    use super::{
+        bar::TraitDefinition as TraitDefinition2,
+        foo::TraitDefinition as TraitDefinition1,
+    };
+
+    #[ink(storage)]
+    pub struct Contract {}
+
+    impl Contract {
+        #[ink(constructor)]
+        pub fn constructor() -> Self {
+            Self {}
+        }
+    }
+
+    impl TraitDefinition1 for Contract {
+        #[ink(message)]
+        fn message(&self) {}
+    }
+
+    impl TraitDefinition2 for Contract {
+        #[ink(message)]
+        fn message(&self) {}
+    }
+}
+
+fn main() {}
