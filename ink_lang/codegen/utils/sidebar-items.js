8a2ce50055ddf9b96fab1d76ee5d2c01a780e7b5
paritytech-ci <devops-team@parity.io>
2021-10-20 22:20:59 +0000

    Updated docs for master and pushed to gh-pages

---
+++ ink_lang/codegen/utils/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["consume_type","Takes a generic type as input and just consumes it while doing nothing."]],"struct":[["IsSameType","Can be used to check equality of types."]]});
