bb5a83dc2f15fb029b2f548d6a7798d0d7555217
Hernando Castano <hernando@hcastano.com>
2022-02-25 17:51:30 -0800

    Remove mentions in code level docs

--- crates/lang/macro/src/lib.rs
+++ crates/lang/macro/src/lib.rs
@@ -119,53 +119,6 @@ pub fn selector_bytes(input: TokenStream) -> TokenStream {
 /// The `#[ink::contract]` macro can be provided with some additional comma-separated
 /// header arguments:
 ///
-/// - `dynamic_storage_allocator: bool`
-///
-///     Tells the ink! code generator to allow usage of ink!'s built-in dynamic
-///     storage allocator.
-///     - `true`: Use the dynamic storage allocator provided by ink!.
-///     - `false`: Do NOT use the dynamic storage allocator provided by ink!.
-///
-///     This feature is generally only needed for smart contracts that try to model
-///     their data in a way that contains storage entities within other storage
-///     entities.
-///
-///     Contract writers should try to write smart contracts that do not depend on the
-///     dynamic storage allocator since enabling it comes at a cost of increased Wasm
-///     file size. Although it will enable interesting use cases. Use it with care!
-///
-///     An example for this is the following type that could potentially be used
-///     within a contract's storage struct definition:
-///
-///
-///     ```ignore
-///     # // Tracking issue [#1119]: Right now we've hidden the `StorageVec` from public access so
-///     # // this doesn't compile.
-///     # use ink_storage as storage;
-///     # type _unused =
-///     storage::Vec<storage::Vec<i32>>
-///     # ;
-///     ```
-///
-///     **Usage Example:**
-///     ```
-///     # use ink_lang as ink;
-///     #[ink::contract(dynamic_storage_allocator = true)]
-///     mod my_contract {
-///         # #[ink(storage)]
-///         # pub struct MyStorage;
-///         # impl MyStorage {
-///         #     #[ink(constructor)]
-///         #     pub fn construct() -> Self { MyStorage {} }
-///         #     #[ink(message)]
-///         #     pub fn message(&self) {}
-///         # }
-///         // ...
-///     }
-///     ```
-///
-///     **Default value:** `false`
-///
 /// - `compile_as_dependency: bool`
 ///
 ///     Tells the ink! code generator to **always** or **never**
@@ -710,7 +663,7 @@ pub fn contract(attr: TokenStream, item: TokenStream) -> TokenStream {
 ///     pub trait TraitDefinition {
 ///         #[ink(message)]
 ///         fn message1(&self);
-///     
+///
 ///         #[ink(message, selector = 42)]
 ///         fn message2(&self);
 ///     }
