ae53a047361bb36a31e8a3035c35142f6083bba1
Alexander Gryaznov <hi@agryaznov.com>
2022-05-10 11:53:29 +0300

    stabilize funcs

--- crates/env/src/engine/on_chain/ext.rs
+++ crates/env/src/engine/on_chain/ext.rs
@@ -237,6 +237,8 @@ mod sys {
             output_len_ptr: Ptr32Mut<u32>,
         ) -> ReturnCode;
 
+        pub fn seal_contains_storage(key_ptr: Ptr32<[u8]>) -> ReturnCode;
+
         pub fn seal_clear_storage(key_ptr: Ptr32<[u8]>);
 
         pub fn seal_call_chain_extension(
@@ -359,6 +361,12 @@ mod sys {
             output_ptr: Ptr32Mut<[u8]>,
             output_len_ptr: Ptr32Mut<u32>,
         ) -> ReturnCode;
+
+        pub fn seal_set_storage(
+            key_ptr: Ptr32<[u8]>,
+            value_ptr: Ptr32<[u8]>,
+            value_len: u32,
+        ) -> ReturnCode;
     }
 
     #[link(wasm_import_module = "__unstable__")]
@@ -369,14 +377,6 @@ mod sys {
             // 32 bytes hash of the message
             message_hash_ptr: Ptr32<[u8]>,
             output_ptr: Ptr32Mut<[u8]>,
-        ) -> ReturnCode;
-
-        pub fn seal_contains_storage(key_ptr: Ptr32<[u8]>) -> ReturnCode;
-
-        pub fn seal_set_storage(
-            key_ptr: Ptr32<[u8]>,
-            value_ptr: Ptr32<[u8]>,
-            value_len: u32,
         ) -> ReturnCode;
     }
 }
