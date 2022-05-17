b8e74bdd186d7f106c6ea7f0db8202cecac04f10
xgreenx <xgreenx9999@gmail.com>
2022-05-17 03:14:05 +0100

    Update metadata to version 4

--- crates/metadata/src/lib.rs
+++ crates/metadata/src/lib.rs
@@ -73,12 +73,14 @@ pub enum MetadataVersioned {
     /// Version 2 of the contract metadata.
     V2(MetadataVersionDeprecated),
     /// Version 3 of the contract metadata.
-    V3(InkProject),
+    V3(MetadataVersionDeprecated),
+    /// Version 4 of the contract metadata.
+    V4(InkProject),
 }
 
 impl From<InkProject> for MetadataVersioned {
     fn from(ink_project: InkProject) -> Self {
-        MetadataVersioned::V3(ink_project)
+        MetadataVersioned::V4(ink_project)
     }
 }
 
