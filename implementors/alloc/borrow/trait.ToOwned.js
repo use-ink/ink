a126965dd4b9723ccbd38307de9d8f442990945a
paritytech-ci <paritytech-ci@parity.io>
2022-07-15 11:24:14 +0000

    Updated docs for master and pushed to gh-pages

--- implementors/alloc/borrow/trait.ToOwned.js
+++
@@ -1,4 +0,0 @@
-(function() {var implementors = {};
-implementors["ink_prelude"] = [];
-implementors["scale_info"] = [];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
