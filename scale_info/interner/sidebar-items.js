bac02812c660182324ebede5cf22aaf15d2da7a2
paritytech-ci <paritytech-ci@parity.io>
2022-07-16 05:20:23 +0000

    Updated docs for master and pushed to gh-pages

--- scale_info/interner/sidebar-items.js
+++ scale_info/interner/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Interner","Interning data structure generic over the element type."],["Symbol","A symbol from an interner."],["UntrackedSymbol","A symbol that is not lifetime tracked."]]});
+window.SIDEBAR_ITEMS = {"struct":[["Interner","Interning data structure generic over the element type."],["Symbol","A symbol from an interner."],["UntrackedSymbol","A symbol that is not lifetime tracked."]]};
