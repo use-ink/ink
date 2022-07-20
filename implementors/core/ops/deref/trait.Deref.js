fa3d6081eab23ab441d4a1c79eaef58b06956f07
paritytech-ci <paritytech-ci@parity.io>
2022-07-20 10:36:56 +0000

    Updated docs for master and pushed to gh-pages

--- implementors/core/ops/deref/trait.Deref.js
+++ implementors/core/ops/deref/trait.Deref.js
@@ -1,3 +1,4 @@
 (function() {var implementors = {};
 implementors["ink_lang_ir"] = [{"text":"impl&lt;'a, C&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"ink_lang_ir/struct.CallableWithSelector.html\" title=\"struct ink_lang_ir::CallableWithSelector\">CallableWithSelector</a>&lt;'a, C&gt;","synthetic":false,"types":["ink_lang_ir::ir::item_impl::callable::CallableWithSelector"]}];
+implementors["scale_info"] = [];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
