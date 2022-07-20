8b387764625adf0e9043afdca875a1ab8d7da81a
paritytech-ci <paritytech-ci@parity.io>
2022-07-20 09:30:11 +0000

    Updated docs for master and pushed to gh-pages

--- ink_storage/sidebar-items.js
+++ ink_storage/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["traits","Traits and interfaces to operate with storage entities."]],"struct":[["Mapping","A mapping of key-value pairs directly into contract storage."]]});
+window.SIDEBAR_ITEMS = {"mod":[["traits","Traits and interfaces to operate with storage entities."]],"struct":[["Mapping","A mapping of key-value pairs directly into contract storage."]]};
