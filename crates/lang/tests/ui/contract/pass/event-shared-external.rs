c6a8cecf600ec7b4658b44fbf8fc8f0ff641cdd2
ascjones <ascjones@gmail.com>
2022-08-30 09:41:22 +0100

    add todos

--- crates/lang/tests/ui/contract/pass/event-shared-external.rs
+++ crates/lang/tests/ui/contract/pass/event-shared-external.rs
@@ -1,5 +1,13 @@
 use ink_lang as ink;
 
+// todo: add the wiring to detect the events metadata, and in cargo-contract merge together
+// todo: consider the possibility of enforcing shared events to be an enum, and tied to an ink! trait def
+// only a single event type per interface which encapsulates all the possible events
+// inside an impl the emit_event is restricted to that trait impl type.
+// in libs there is no such restriction, can just ink_env::emit_event?
+// either way still need to do the metadata scanning
+// also with cross-contract call even those events will not be included in the metadata.
+
 #[ink::event_definition]
 pub struct SharedEvent {
     arg_1: u8,
