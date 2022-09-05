7bc882f2ee47c3e6e378b3bddfd09c7fe7589467
SkymanOne <german@parity.io>
2022-09-05 14:14:47 +0100

    format code to please CI

--- crates/metadata/src/utils.rs
+++ crates/metadata/src/utils.rs
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::str;
 use crate::serde_hex;
+use std::str;
 
 /// Serializes the given bytes as byte string.
 pub fn serialize_as_byte_str<S>(bytes: &[u8], serializer: S) -> Result<S::Ok, S::Error>
