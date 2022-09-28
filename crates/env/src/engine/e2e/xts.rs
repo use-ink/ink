f1082fa8d7800c745d8209dfa817c8abe15a1f70
Michael Mueller <mich@elmueller.net>
2022-09-28 18:52:33 +0200

    Apply `cargo fmt`

--- crates/env/src/engine/e2e/xts.rs
+++ crates/env/src/engine/e2e/xts.rs
@@ -302,13 +302,15 @@ where
         )
         .unvalidated();
 
-        self
-            .client
+        self.client
             .tx()
             .sign_and_submit_then_watch_default(&call, signer)
             .await
             .map(|tx_progress| {
-                log_info(&format!("signed and submitted call with extrinsic hash {:?}", tx_progress.extrinsic_hash()));
+                log_info(&format!(
+                    "signed and submitted call with extrinsic hash {:?}",
+                    tx_progress.extrinsic_hash()
+                ));
                 tx_progress
             })
             .unwrap_or_else(|err| {
@@ -320,18 +322,12 @@ where
             .wait_for_in_block()
             .await
             .unwrap_or_else(|err| {
-                panic!(
-                    "error on call `wait_for_in_block`: {:?}",
-                    err
-                );
+                panic!("error on call `wait_for_in_block`: {:?}", err);
             })
             .fetch_events()
             .await
             .unwrap_or_else(|err| {
-                panic!(
-                    "error on call `fetch_events`: {:?}",
-                    err
-                );
+                panic!("error on call `fetch_events`: {:?}", err);
             })
     }
 }
