802bb135fd3a314002aa4fb39f9f9c4000a6bea8
Michael Müller <mich@elmueller.net>
2022-09-28 19:25:26 +0200

    E2E: Add issue numbers + make E2E dependencies optional (#1424)
    * Update some issue numbers
    
    * Mark E2E deps as optional
    
    * Apply `cargo fmt`

--- crates/env/src/engine/e2e/client.rs
+++ crates/env/src/engine/e2e/client.rs
@@ -509,7 +509,7 @@ where
 
     /// Returns true if the `substrate-contracts-node` log under
     /// `/tmp/contracts-node.log` contains `msg`.
-    /// TODO(#xxx) Matches on any log entry currently, even if done
+    /// TODO(#1423) Matches on any log entry currently, even if done
     /// by a different test.
     pub fn node_log_contains(&self, msg: &str) -> bool {
         let output = std::process::Command::new("grep")
