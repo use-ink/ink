4d6766f9a053db4472f18dc05bd7f60accf5ea24
SkymanOne <german@parity.io>
2022-10-12 17:27:07 +0100

    ugly solution for failable contracts

--- crates/ink/codegen/src/generator/storage.rs
+++ crates/ink/codegen/src/generator/storage.rs
@@ -101,6 +101,7 @@ impl Storage<'_> {
             #[::ink::storage_item]
             #[cfg_attr(test, derive(::core::fmt::Debug))]
             #[cfg(not(feature = "__ink_dylint_Storage"))]
+            #[derive(scale::Encode)]
             pub struct #ident #generics {
                 #( #fields ),*
             }
