9254b772316e08597cce54027b25000a2b830e32
Alexander Gryaznov <hi@agryaznov.com>
2022-10-18 15:43:36 +0300

    fix tests

--- crates/lang/tests/ui/pay_with_call/pass/multiple_args.rs
+++ crates/lang/tests/ui/pay_with_call/pass/multiple_args.rs
@@ -1,5 +1,9 @@
+use ink_lang as ink;
+
 #[ink::contract]
 mod contract {
+    use ink_env as env;
+
     #[ink(storage)]
     pub struct Contract {}
 
@@ -19,9 +23,9 @@ mod contract {
         pub fn message2(&self, _arg1: u8, _arg2: (u8, AccountId)) {}
 
         fn check_compiles(&self) {
-            ink::env::pay_with_call!(self.message0(), 0);
-            ink::env::pay_with_call!(self.message1(0), 0);
-            ink::env::pay_with_call!(self.message2(0, (0, Self::env().account_id())), 0);
+            env::pay_with_call!(self.message0(), 0);
+            env::pay_with_call!(self.message1(0), 0);
+            env::pay_with_call!(self.message2(0, (0, Self::env().account_id())), 0);
         }
     }
 }
