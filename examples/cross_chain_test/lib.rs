c62ea254b3a5a4a8b62e1d1a746ef831a00e663b
Hernando Castano <hernando@hcastano.com>
2022-10-21 23:24:16 -0400

    Add example cross-chain contract

---
+++ examples/cross_chain_test/lib.rs
@@ -0,0 +1,30 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+#[ink::contract]
+mod cross_chain_test {
+
+    #[ink(storage)]
+    pub struct CrossChainTest {}
+
+    impl CrossChainTest {
+        #[ink(constructor)]
+        pub fn new() -> Self {
+            Self {}
+        }
+
+        #[ink(message)]
+        pub fn call(&mut self, address: AccountId, selector: [u8; 4]) {
+            use ink::env::{
+                call::{build_call, Call, ExecutionInput, Selector},
+                DefaultEnvironment,
+            };
+
+            build_call::<DefaultEnvironment>()
+                .call_type(Call::new().callee(address))
+                .exec_input(ExecutionInput::new(Selector::new(selector)))
+                .returns::<()>()
+                .fire()
+                .unwrap();
+        }
+    }
+}
