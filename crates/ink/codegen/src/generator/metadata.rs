364032a0e0ad91f777a6b3f19cd89286fd4af58a
xermicus <cyrill@parity.io>
2022-11-07 11:20:32 +0100

    fix some comments

--- crates/ink/codegen/src/generator/metadata.rs
+++ crates/ink/codegen/src/generator/metadata.rs
@@ -351,9 +351,9 @@ impl Metadata<'_> {
     }
 
     /// Generates ink! metadata for the given return type of a constructor.
-    /// If constructor result type is not `Result`,
+    /// If the constructor return type is not `Result`,
     /// the metadata will not display any metadata for return type.
-    /// Otherwise, the return type is `Result<(), E>`.
+    /// Otherwise, the return type spec is `Result<(), E>`.
     fn generate_constructor_return_type(ret_ty: Option<&syn::Type>) -> TokenStream2 {
         match ret_ty {
             None => {
@@ -378,8 +378,7 @@ impl Metadata<'_> {
         }
     }
 
-    /// Helper function which replace all occurrences of `Self`
-    /// with `()`.
+    /// Helper function to replace all occurrences of `Self` with `()`.
     fn replace_self_with_unit(ty: &syn::Type) -> TokenStream2 {
         if ty.to_token_stream().to_string().contains("< Self") {
             let s = ty.to_token_stream().to_string().replace("< Self", "< ()");
