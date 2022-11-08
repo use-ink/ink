e6441637f2d15c295b50eb7c6a8aecae3c73df5d
paritytech-ci <paritytech-ci@parity.io>
2022-11-08 09:23:55 +0000

    Updated docs for master and pushed to gh-pages

--- implementors/ink_storage_traits/storage/trait.Packed.js
+++ implementors/ink_storage_traits/storage/trait.Packed.js
@@ -1,4 +1,4 @@
-(function() {var implementors = {
-"ink":[],
-"ink_storage_traits":[]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {};
+implementors["ink"] = [];
+implementors["ink_storage_traits"] = [];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
