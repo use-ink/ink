82bd5eb8b84cb1cad6bdc5ca06478cddb089410b
paritytech-ci <paritytech-ci@parity.io>
2022-11-08 05:34:26 +0000

    Updated docs for master and pushed to gh-pages

--- implementors/ink_storage_traits/storage/trait.StorageKey.js
+++ implementors/ink_storage_traits/storage/trait.StorageKey.js
@@ -1,5 +1,5 @@
-(function() {var implementors = {};
-implementors["ink"] = [];
-implementors["ink_storage"] = [{"text":"impl&lt;K, V, KeyType&gt; StorageKey for <a class=\"struct\" href=\"ink_storage/struct.Mapping.html\" title=\"struct ink_storage::Mapping\">Mapping</a>&lt;K, V, KeyType&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;V: Packed,<br>&nbsp;&nbsp;&nbsp;&nbsp;KeyType: StorageKey,&nbsp;</span>","synthetic":false,"types":["ink_storage::lazy::mapping::Mapping"]},{"text":"impl&lt;V, KeyType&gt; StorageKey for <a class=\"struct\" href=\"ink_storage/struct.Lazy.html\" title=\"struct ink_storage::Lazy\">Lazy</a>&lt;V, KeyType&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;KeyType: StorageKey,&nbsp;</span>","synthetic":false,"types":["ink_storage::lazy::Lazy"]}];
-implementors["ink_storage_traits"] = [];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"ink":[],
+"ink_storage":[["impl&lt;K, V, KeyType&gt; StorageKey for <a class=\"struct\" href=\"ink_storage/struct.Mapping.html\" title=\"struct ink_storage::Mapping\">Mapping</a>&lt;K, V, KeyType&gt;<span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;V: Packed,<br>&nbsp;&nbsp;&nbsp;&nbsp;KeyType: StorageKey,</span>"],["impl&lt;V, KeyType&gt; StorageKey for <a class=\"struct\" href=\"ink_storage/struct.Lazy.html\" title=\"struct ink_storage::Lazy\">Lazy</a>&lt;V, KeyType&gt;<span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;KeyType: StorageKey,</span>"]],
+"ink_storage_traits":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
