164cab135835ee538c3e2b20f2d95a06dd2e45c4
paritytech-ci <paritytech-ci@parity.io>
2022-11-09 13:41:18 +0000

    Updated docs for master and pushed to gh-pages

--- implementors/core/iter/traits/collect/trait.IntoIterator.js
+++ implementors/core/iter/traits/collect/trait.IntoIterator.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {
-"ink_ir":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.65.0/core/iter/traits/collect/trait.IntoIterator.html\" title=\"trait core::iter::traits::collect::IntoIterator\">IntoIterator</a> for <a class=\"struct\" href=\"ink_ir/ast/struct.AttributeArgs.html\" title=\"struct ink_ir::ast::AttributeArgs\">AttributeArgs</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {};
+implementors["ink_ir"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.64.0/core/iter/traits/collect/trait.IntoIterator.html\" title=\"trait core::iter::traits::collect::IntoIterator\">IntoIterator</a> for <a class=\"struct\" href=\"ink_ir/ast/struct.AttributeArgs.html\" title=\"struct ink_ir::ast::AttributeArgs\">AttributeArgs</a>","synthetic":false,"types":["ink_ir::ast::attr_args::AttributeArgs"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/ink_primitives/types/trait.Clear.js
+++ implementors/ink_primitives/types/trait.Clear.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {
-"ink_primitives":[]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {};
+implementors["ink_primitives"] = [];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
