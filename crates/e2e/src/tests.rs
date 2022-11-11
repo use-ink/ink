54e2522f2112ba6095c2e70706ccfc34dff97a63
SkymanOne <german@parity.io>
2022-11-11 11:28:41 +0000

    use tokio test to clean unit test

--- crates/e2e/src/tests.rs
+++ crates/e2e/src/tests.rs
@@ -1,15 +1,10 @@
-#[test]
+#[tokio::test]
 #[should_panic(
     expected = "Error establishing connection to a node at ws://0.0.0.0:9944. Make sure you run a node behind the given url!"
 )]
-fn fail_initialization_with_no_node() {
-    let client_init =
-        crate::Client::<crate::PolkadotConfig, ink_env::DefaultEnvironment>::new(
-            "ws://0.0.0.0:9944",
-        );
-    tokio::runtime::Builder::new_current_thread()
-        .enable_all()
-        .build()
-        .unwrap_or_else(|err| panic!("Failed building the Runtime during test: {}", err))
-        .block_on(client_init);
+async fn fail_initialization_with_no_node() {
+    let _ = crate::Client::<crate::PolkadotConfig, ink_env::DefaultEnvironment>::new(
+        "ws://0.0.0.0:9944",
+    )
+    .await;
 }
