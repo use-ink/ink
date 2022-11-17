5eb1817ee87c75e5eeceb4256da9fc3d11fe2c1e
paritytech-ci <paritytech-ci@parity.io>
2022-11-17 21:14:21 +0000

    Updated docs for master and pushed to gh-pages

--- implementors/ink/reflect/dispatch/trait.ConstructorOutput.js
+++ implementors/ink/reflect/dispatch/trait.ConstructorOutput.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"ink_metadata":[]
+"ink":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
