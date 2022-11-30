b6c7a5f578dbcaa2bf727d420c6ddb45d5fab74d
Andrew Jones <ascjones@gmail.com>
2022-11-30 10:10:13 +0000

    Fix error

--- examples/contract-terminate/lib.rs
+++ examples/contract-terminate/lib.rs
@@ -92,8 +92,8 @@ pub mod just_terminates {
 
             let contains_event = |pallet_name: &str, variant_name: &str| {
                 call_res.events.iter().any(|event| {
-                    let evt = evt.unwrap();
-                    evt.pallet_name() == pallet_name && evt.variant_name() == variant_name
+                    let event = event.unwrap();
+                    event.pallet_name() == pallet_name && event.variant_name() == variant_name
                 })
             };
 
