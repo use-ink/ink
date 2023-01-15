d1d11a73f86b8b300632462023d80425e7f03d32
andrew <ascjones@gmail.com>
2023-01-15 14:08:54 +0000

    Make it compile

--- crates/ink/codegen/src/generator/as_dependency/contract_ref.rs
+++ crates/ink/codegen/src/generator/as_dependency/contract_ref.rs
@@ -423,7 +423,7 @@ impl ContractRef<'_> {
                 ::ink::env::call::utils::Set<::ink::env::call::utils::ReturnType<#ret_type>>,
                 Self,
             > {
-                ::ink::env::call::build_create::<Environment, Self>()
+                ::ink::env::call::build_create::<Environment, #ret_type, Self>()
                     .exec_input(
                         ::ink::env::call::ExecutionInput::new(
                             ::ink::env::call::Selector::new([ #( #selector_bytes ),* ])
