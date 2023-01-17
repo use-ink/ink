d08b65a5196aeba3bb6dd0c7dd6a5998a0a43f76
Artemka374 <fomiuk.artemka123@gmail.com>
2023-01-17 11:28:53 +0200

    add more types to example and update README

--- examples/complex-storage-structures/README.md
+++ examples/complex-storage-structures/README.md
@@ -35,9 +35,7 @@ and all of it's fields occupy a single storage cell.
 You can derive `Packed` trait for your type as in the following example:
 
 ```rust
-use ink::storage::traits::Packed;
-
-#[derive(Packed)]
+#[derive(scale::Encode, scale::Decode)]
 struct MyPackedStruct {
     first_field: u32,
     second_field: Vec<u8>,
@@ -54,6 +52,8 @@ struct MyNonPackedStruct {
 }
 ```
 The type is considered non-packed as `Mapping` always occupies it's own storage cell.
+
+Packed types don't have their storage keys, but non-packed types require calculating the storage key during compilation.
 
  - `#[ink::storage_item]` macro attribute prepares your type to be fully compatible and usable with a storage.
 It implements all necessary traits and calculates the storage key for types. You can set `#[ink::storage_item(derive = false)]` that will indicate that will disable auto-deriving of all required traits.
@@ -98,8 +98,8 @@ struct NonPacked {
 
 #[derive(scale::Decode, scale::Encode)]
 #[cfg_attr(
-feature = "std",
-derive(scale_info::TypeInfo, ink::storage::traits::StorageLayout)
+    feature = "std",
+    derive(scale_info::TypeInfo, ink::storage::traits::StorageLayout)
 )]
 struct Packed {
     s1: u128,
@@ -133,7 +133,20 @@ struct MyStruct {
 }
 ```
 
-- `StorageKey` is a trait that is used for calculating the storage key for types. It is implemented automatically for all types that implement `Packed` types.
+- `StorageKey` is a trait that is used for calculating the storage key for types. It is implemented automatically for all types that are non-packed.
+
+There also exists way to use `StorageKey` for types that are packed - you can just use `Lazy`, that is a wrapper around type,
+which allows to store it in separate storage cell under it's own storage key. You can use it like this:
+```rust
+
+#[ink::storage_item]
+struct MyStruct {
+    first_field: Lazy<u32>,
+    second_field: Mapping<u32, u32>,
+}
+```
+
+In this case, `first_field` will be stored in it's own storage cell.
 
 If you add generic that implements `StorageKey` to your type, it will be used as a storage key for this type, otherwise it will be 
 set to `AutoKey`. For example:
@@ -144,7 +157,7 @@ struct MyStruct {
     second_field: Mapping<u32, u32>,
 }
 ```
-In this case storage key will be calculated automatically. But if you want to customize it, you can do it like this:
+In this case storage key will be calculated automatically. But if you want to offset the key, you can do it like this:
 ```rust
 #[ink::storage_item]
 struct MyStruct<KEY: StorageKey> {
@@ -154,6 +167,9 @@ struct MyStruct<KEY: StorageKey> {
 ```
 
 In this case, you can set storage key that you prefer for instances of this type.
+
+The reason to do it in such way is that you can use the same type in different places and set different storage keys for them.
+
 For example if you want to use it in contract, you can do it like this:
 ```rust
 #[ink(storage)]
@@ -184,11 +200,13 @@ pub fn new() -> Self {
 }
 ```
 
-You will get an error like this:
+You will get an error that look similar to this:
 ```shell
 note: expected struct `Balances<ResolverKey<ManualKey<_, _>, ManualKey<4162912002>>>`
 found struct `Balances<ManualKey<_, _>>`
 ```
+
+That's so, because every type is unique and has it's own storage key after code generation.
 
 So, the way to fix it is to use `Default::default()` so it will generate right type:
 ```rust
--- examples/complex-storage-structures/lib.rs
+++ examples/complex-storage-structures/lib.rs
@@ -1,11 +1,5 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-pub use self::complex_structures::{
-    Balances,
-    Contract,
-    ContractRef,
-};
-
 #[ink::contract]
 pub mod complex_structures {
     use ink::storage::{
@@ -13,61 +7,126 @@ pub mod complex_structures {
             AutoKey,
             ManualKey,
             Packed,
+            Storable,
+            StorableHint,
             StorageKey,
         },
         Mapping,
     };
 
-    #[ink::storage_item]
-    #[derive(Default, Debug)]
-    pub struct Balances<KEY: StorageKey, T: Packed = u128> {
-        pub balance_state: T,
+    /// Non-packed type with
+    #[ink::storage_item(derive = false)]
+    #[derive(Storable, StorableHint, StorageKey, Default, Debug)]
+    #[cfg_attr(
+        feature = "std",
+        derive(scale_info::TypeInfo, ink::storage::traits::StorageLayout)
+    )]
+    pub struct TokenManagement<
+        KEY: StorageKey,
+        T: Packed + BalancesStateManagement = Balances,
+    > {
+        balances_state: T,
+        allowances: Allowances<ManualKey<100>>,
     }
 
-    impl<KEY: StorageKey> Balances<KEY> {
-        pub fn increase_state(&mut self, amount: u128) {
+    #[ink::storage_item]
+    #[derive(Default, Debug)]
+    pub struct Allowances<KEY: StorageKey> {
+        allowances: Mapping<(AccountId, AccountId), Balance, AutoKey>,
+    }
+
+    #[derive(scale::Encode, scale::Decode, Default, Debug)]
+    #[cfg_attr(
+        feature = "std",
+        derive(scale_info::TypeInfo, ink::storage::traits::StorageLayout)
+    )]
+    pub struct Balances {
+        pub balance_state: u128,
+    }
+
+    pub trait BalancesStateManagement {
+        fn increase_balance_state(&mut self, amount: u128);
+        fn decrease_balance_state(&mut self, amount: u128);
+        fn get_balance_state(&self) -> u128;
+    }
+
+    impl<KEY: StorageKey> Allowances<KEY> {
+        fn get_allowance(&self, owner: AccountId, spender: AccountId) -> Balance {
+            self.allowances.get(&(owner, spender)).unwrap_or(0)
+        }
+
+        fn set_allowance(
+            &mut self,
+            owner: AccountId,
+            spender: AccountId,
+            value: Balance,
+        ) {
+            self.allowances.insert(&(owner, spender), &value);
+        }
+    }
+
+    impl BalancesStateManagement for Balances {
+        fn increase_balance_state(&mut self, amount: u128) {
             self.balance_state += amount;
+        }
+
+        fn decrease_balance_state(&mut self, amount: u128) {
+            self.balance_state -= amount;
+        }
+
+        fn get_balance_state(&self) -> u128 {
+            self.balance_state
         }
     }
 
     #[ink(storage)]
     #[derive(Default)]
     pub struct Contract {
-        pub balances: Balances<ManualKey<123>>,
+        pub token_management: TokenManagement<ManualKey<123>>,
     }
 
     impl Contract {
         #[ink(constructor)]
         pub fn new() -> Self {
-            let mut instance = Self::default();
-
-            instance.balances = Default::default();
-
-            instance
+            Default::default()
         }
 
         #[ink(message)]
-        pub fn increase_state(&mut self, amount: u128) {
-            self.balances.increase_state(amount);
+        pub fn increase_balances_state(&mut self, amount: u128) {
+            self.token_management
+                .balances_state
+                .increase_balance_state(amount);
         }
-    }
-}
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use ink::{
-        env::DefaultEnvironment,
-        storage::traits::{
-            AutoKey,
-            ManualKey,
-            StorageKey,
-        },
-    };
 
-    #[test]
-    fn keys_work() {
-        assert_eq!(<Balances<AutoKey> as StorageKey>::KEY, 0);
-        assert_eq!(<Balances<ManualKey<123>> as StorageKey>::KEY, 123);
-        assert_eq!(<Balances{ balances_state: 1} as StorageKey>::KEY, 100);
+        #[ink(message)]
+        pub fn decrease_balances_state(&mut self, amount: u128) {
+            self.token_management
+                .balances_state
+                .decrease_balance_state(amount);
+        }
+
+        #[ink(message)]
+        pub fn get_balances_state(&self) -> u128 {
+            self.token_management.balances_state.get_balance_state()
+        }
+
+        #[ink(message)]
+        pub fn get_allowance(&self, owner: AccountId, spender: AccountId) -> u128 {
+            self.token_management
+                .allowances
+                .get_allowance(owner, spender)
+        }
+
+        #[ink(message)]
+        pub fn set_allowance(
+            &mut self,
+            owner: AccountId,
+            spender: AccountId,
+            value: u128,
+        ) {
+            self.token_management
+                .allowances
+                .set_allowance(owner, spender, value)
+        }
     }
 }
