6a429ed03b5d9f8fe4959c976018bb32e337abbf
paritytech-ci <paritytech-ci@parity.io>
2023-01-24 19:34:26 +0000

    Updated docs for master and pushed to gh-pages

--- implementors/ink_env/call/create_builder/trait.ConstructorReturnType.js
+++ implementors/ink_env/call/create_builder/trait.ConstructorReturnType.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"ink":[]
+"ink_env":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
