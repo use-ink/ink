aae51017046ffbc3d1802fb5d5d54ed86e729e8f
paritytech-ci <paritytech-ci@parity.io>
2023-01-28 05:29:19 +0000

    Updated docs for master and pushed to gh-pages

--- implementors/core/iter/traits/exact_size/trait.ExactSizeIterator.js
+++ implementors/core/iter/traits/exact_size/trait.ExactSizeIterator.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"ink_ir":[["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.1/core/iter/traits/exact_size/trait.ExactSizeIterator.html\" title=\"trait core::iter::traits::exact_size::ExactSizeIterator\">ExactSizeIterator</a> for <a class=\"struct\" href=\"ink_ir/struct.InputsIter.html\" title=\"struct ink_ir::InputsIter\">InputsIter</a>&lt;'a&gt;"]]
+"ink_ir":[["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.67.0/core/iter/traits/exact_size/trait.ExactSizeIterator.html\" title=\"trait core::iter::traits::exact_size::ExactSizeIterator\">ExactSizeIterator</a> for <a class=\"struct\" href=\"ink_ir/struct.InputsIter.html\" title=\"struct ink_ir::InputsIter\">InputsIter</a>&lt;'a&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
