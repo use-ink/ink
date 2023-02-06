9943cbe9657c25ba03a0cb4e1bfe64157330b4de
Piotr Mikołajczyk <piomiko41@gmail.com>
2023-02-06 15:11:32 +0100

    typo

--- examples/call-runtime/lib.rs
+++ examples/call-runtime/lib.rs
@@ -90,7 +90,7 @@ mod runtime_call {
         const TRANSFER_VALUE: Balance = 1_000_000_000;
 
         #[ink_e2e::test]
-        #[ignore = "Requires that the pallet contract is was configured with:\
+        #[ignore = "Requires that the pallet contract is configured with:\
             - `CallFilter` allowing for a transfer, e.g. `frame_support::traits::Nothing`,\
             - `UnsafeUnstableInterface = ConstBool<true>`"]
         async fn it_works(mut client: Client<C, E>) -> E2EResult<()> {
