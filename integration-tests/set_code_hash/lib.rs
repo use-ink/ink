502831ea74776f8b124dd26e0620c80467221998
Hernando Castano <hernando@hcastano.com>
2023-03-01 11:52:40 -0800

    Add an E2E test showing upgrade workflow

--- integration-tests/set_code_hash/lib.rs
+++ integration-tests/set_code_hash/lib.rs
@@ -1,5 +1,13 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
+//! Demonstrates how to use [`set_code_hash`](https://docs.rs/ink_env/latest/ink_env/fn.set_code_hash.html)
+//! to swap out the `code_hash` of an on-chain contract.
+//!
+//! We will swap the code of our `Incrementer` contract with that of the an `Incrementer` found in
+//! the `updated_incrementer` folder.
+//!
+//! See the included End-to-End tests an example update workflow.
+
 #[ink::contract]
 pub mod incrementer {
 
@@ -53,6 +61,66 @@ pub mod incrementer {
                 panic!("Failed to `set_code_hash` to {code_hash:?} due to {err:?}")
             });
             ink::env::debug_println!("Switched code hash to {:?}.", code_hash);
+        }
+    }
+
+    #[cfg(all(test, feature = "e2e-tests"))]
+    mod e2e_tests {
+        use super::*;
+        use ink_e2e::build_message;
+
+        type E2EResult<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+        #[ink_e2e::test(additional_contracts = "./updated_incrementer/Cargo.toml")]
+        async fn set_code_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
+            // Given
+            let constructor = IncrementerRef::new();
+            let contract_acc_id = client
+                .instantiate("incrementer", &ink_e2e::alice(), constructor, 0, None)
+                .await
+                .expect("instantiate failed")
+                .account_id;
+
+            let get = build_message::<IncrementerRef>(contract_acc_id.clone())
+                .call(|incrementer| incrementer.get());
+            let get_res = client.call_dry_run(&ink_e2e::alice(), &get, 0, None).await;
+            assert!(matches!(get_res.return_value(), 0));
+
+            // When
+            let new_code_hash = client
+                .upload("updated_incrementer", &ink_e2e::alice(), None)
+                .await
+                .expect("uploading `updated_incrementer` failed")
+                .code_hash;
+
+            let new_code_hash = new_code_hash.as_ref().try_into().unwrap();
+            let set_code = build_message::<IncrementerRef>(contract_acc_id.clone())
+                .call(|incrementer| incrementer.set_code(new_code_hash));
+
+            let _set_code_result = client
+                .call(&ink_e2e::alice(), set_code, 0, None)
+                .await
+                .expect("`set_code` failed");
+
+            // Then
+            // Note that our contract's `AccountId` (so `contract_acc_id`) has stayed the same
+            // between updates!
+            let inc = build_message::<IncrementerRef>(contract_acc_id.clone())
+                .call(|incrementer| incrementer.inc());
+
+            let _inc_result = client
+                .call(&ink_e2e::alice(), inc, 0, None)
+                .await
+                .expect("`inc` failed");
+
+            let get = build_message::<IncrementerRef>(contract_acc_id.clone())
+                .call(|incrementer| incrementer.get());
+            let get_res = client.call_dry_run(&ink_e2e::alice(), &get, 0, None).await;
+
+            // Remember, we updated our incrementer contract to increment by `4`.
+            assert!(matches!(get_res.return_value(), 4));
+
+            Ok(())
         }
     }
 }
