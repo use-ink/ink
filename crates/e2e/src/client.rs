b4857047747a4cae0597641e696360b1da844625
PierreOssun <35110271+PierreOssun@users.noreply.github.com>
2023-04-12 15:57:24 +0200

    E2E: Add runtime Call (#1736)
    * E2E: add runtime_call
    
    * Added integration test
    
    * Updated description
    
    * fix clippy
    
    * fmt
    
    * updated description
    
    * fixed expect mesg
    
    * Added backtick
    
    * Added derive default
    
    * updatd description
    
    * Added backtick
    
    * Update crates/e2e/src/client.rs
    
    * Update crates/e2e/src/client.rs
    
    * Update crates/e2e/src/xts.rs
    
    * Update integration-tests/e2e-call-runtime/lib.rs
    
    ---------
    
    Co-authored-by: Michael Müller <mich@elmueller.net>

--- crates/e2e/src/client.rs
+++ crates/e2e/src/client.rs
@@ -758,6 +758,48 @@ where
         })
     }
 
+    /// Executes a runtime call `call_name` for the `pallet_name`.
+    /// The `call_data` is a `Vec<Value>`
+    ///
+    /// Note:
+    /// - `pallet_name` must be in camel case, for example `Balances`.
+    /// - `call_name` must be snake case, for example `force_transfer`.
+    /// - `call_data` is a `Vec<subxt::dynamic::Value>` that holds a representation of
+    ///    some value.
+    ///
+    /// Returns when the transaction is included in a block. The return value
+    /// contains all events that are associated with this transaction.
+    pub async fn runtime_call<'a>(
+        &mut self,
+        signer: &Signer<C>,
+        pallet_name: &'a str,
+        call_name: &'a str,
+        call_data: Vec<Value>,
+    ) -> Result<ExtrinsicEvents<C>, Error<C, E>> {
+        let tx_events = self
+            .api
+            .runtime_call(signer, pallet_name, call_name, call_data)
+            .await;
+
+        for evt in tx_events.iter() {
+            let evt = evt.unwrap_or_else(|err| {
+                panic!("unable to unwrap event: {err:?}");
+            });
+
+            if is_extrinsic_failed_event(&evt) {
+                let metadata = self.api.client.metadata();
+                let dispatch_error = subxt::error::DispatchError::decode_from(
+                    evt.field_bytes(),
+                    &metadata,
+                );
+                log_error(&format!("extrinsic for call failed: {dispatch_error:?}"));
+                return Err(Error::CallExtrinsic(dispatch_error))
+            }
+        }
+
+        Ok(tx_events)
+    }
+
     /// Executes a dry-run `call`.
     ///
     /// Returns the result of the dry run, together with the decoded return value of the
