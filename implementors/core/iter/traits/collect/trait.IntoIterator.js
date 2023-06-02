a067d23c1047988a0a89f47bfe4a2a6dd256b0ea
paritytech-ci <paritytech-ci@parity.io>
2023-06-02 05:24:35 +0000

    Updated docs for master and pushed to gh-pages

--- implementors/core/iter/traits/collect/trait.IntoIterator.js
+++ implementors/core/iter/traits/collect/trait.IntoIterator.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"ink_ir":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.69.0/core/iter/traits/collect/trait.IntoIterator.html\" title=\"trait core::iter::traits::collect::IntoIterator\">IntoIterator</a> for <a class=\"struct\" href=\"ink_ir/ast/struct.AttributeArgs.html\" title=\"struct ink_ir::ast::AttributeArgs\">AttributeArgs</a>"]]
+"ink_ir":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.70.0/core/iter/traits/collect/trait.IntoIterator.html\" title=\"trait core::iter::traits::collect::IntoIterator\">IntoIterator</a> for <a class=\"struct\" href=\"ink_ir/ast/struct.AttributeArgs.html\" title=\"struct ink_ir::ast::AttributeArgs\">AttributeArgs</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
