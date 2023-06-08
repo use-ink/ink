589f6e29b7fbf388efdeee1d4633a4822df63c31
paritytech-ci <paritytech-ci@parity.io>
2023-06-08 09:58:20 +0000

    Updated docs for master and pushed to gh-pages

--- implementors/core/ops/drop/trait.Drop.js
+++ implementors/core/ops/drop/trait.Drop.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"ink_e2e":[["impl&lt;R&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.70.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"ink_e2e/struct.TestNodeProcess.html\" title=\"struct ink_e2e::TestNodeProcess\">TestNodeProcess</a>&lt;R&gt;<span class=\"where fmt-newline\">where\n    R: Config,</span>"]]
+"ink_e2e":[["impl&lt;R&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.69.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"ink_e2e/struct.TestNodeProcess.html\" title=\"struct ink_e2e::TestNodeProcess\">TestNodeProcess</a>&lt;R&gt;<span class=\"where fmt-newline\">where\n    R: Config,</span>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
