cd9167e24ae87091b74e111d26e746f9b21fd0f5
Andrew Jones <ascjones@gmail.com>
2023-06-15 17:50:53 +0100

    Failing test for decoding forward call value

--- integration-tests/call-builder-return-value/lib.rs
+++ integration-tests/call-builder-return-value/lib.rs
@@ -21,7 +21,6 @@ mod call_builder {
     use ink::{
         env::{
             call::{
-                build_call,
                 ExecutionInput,
                 Selector,
             },
@@ -85,16 +84,53 @@ mod call_builder {
                 Err(err) => Err(format!("Env Error: {:?}", err)),
             }
         }
+
+        /// Forward a call to the given contract/selector and return the result.
+        #[ink(message)]
+        pub fn forward_call(&mut self, address: AccountId, selector: [u8; 4]) -> i32 {
+            use ink::env::call::build_call;
+
+            build_call::<DefaultEnvironment>()
+                .call(address)
+                .exec_input(ExecutionInput::new(Selector::new(selector)))
+                .returns::<i32>()
+                .invoke()
+        }
+
+        /// Forward call to the given contract/selector and attempt to decode the return
+        /// value into an `i8`.
+        #[ink(message)]
+        pub fn forward_call_short_return_type(
+            &mut self,
+            address: AccountId,
+            selector: [u8; 4],
+        ) -> Result<i8, String> {
+            use ink::env::call::build_call;
+
+            let result = build_call::<DefaultEnvironment>()
+                .call(address)
+                .exec_input(ExecutionInput::new(Selector::new(selector)))
+                .returns::<i8>()
+                .try_invoke();
+
+            match result {
+                Ok(Ok(value)) => Ok(value),
+                Ok(Err(err)) => Err(format!("LangError: {:?}", err)),
+                Err(ink::env::Error::Decode(_)) => Err("Decode Error".to_string()),
+                Err(err) => Err(format!("Env Error: {:?}", err)),
+            }
+        }
     }
 
     #[cfg(all(test, feature = "e2e-tests"))]
     mod e2e_tests {
         use super::*;
+        use incrementer::IncrementerRef;
 
         type E2EResult<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
         #[ink_e2e::test]
-        async fn e2e_call_builder_return_value_returns_correct_value(
+        async fn e2e_delegate_call_return_value_returns_correct_value(
             mut client: ink_e2e::Client<C, E>,
         ) -> E2EResult<()> {
             let origin = client
@@ -132,7 +168,7 @@ mod call_builder {
         }
 
         #[ink_e2e::test]
-        async fn e2e_call_builder_return_value_errors_if_return_data_too_long(
+        async fn e2e_delegate_call_return_value_errors_if_return_data_too_long(
             mut client: ink_e2e::Client<C, E>,
         ) -> E2EResult<()> {
             let origin = client
@@ -153,7 +189,89 @@ mod call_builder {
                 .code_hash;
 
             let selector = ink::selector_bytes!("get");
-            let call = call_builder_call.delegate_call_short_return_type(code_hash, selector);
+            let call =
+                call_builder_call.delegate_call_short_return_type(code_hash, selector);
+            let call_result: Result<i8, String> = client
+                .call_dry_run(&origin, &call, 0, None)
+                .await
+                .return_value();
+
+            assert!(
+                call_result.is_err(),
+                "Should fail of decoding an `i32` into an `i8`"
+            );
+            assert_eq!(
+                "Decode Error".to_string(),
+                call_result.unwrap_err(),
+                "Should fail to decode short type if bytes remain from return data."
+            );
+
+            Ok(())
+        }
+
+        #[ink_e2e::test]
+        async fn e2e_forward_call_return_value_returns_correct_value(
+            mut client: ink_e2e::Client<C, E>,
+        ) -> E2EResult<()> {
+            let origin = client
+                .create_and_fund_account(&ink_e2e::alice(), 10_000_000_000_000)
+                .await;
+
+            let constructor = CallBuilderReturnValueRef::new(0);
+            let call_builder = client
+                .instantiate("call_builder_return_value", &origin, constructor, 0, None)
+                .await
+                .expect("instantiate failed");
+            let mut call_builder_call = call_builder.call::<CallBuilderReturnValue>();
+
+            let expected_value = 42;
+            let incrementer_constructor = IncrementerRef::new(expected_value);
+            let incrementer = client
+                .instantiate("incrementer", &origin, incrementer_constructor, 0, None)
+                .await
+                .expect("instantiate failed");
+
+            let selector = ink::selector_bytes!("get");
+            let call = call_builder_call.forward_call(incrementer.account_id, selector);
+            let call_result = client
+                .call(&origin, &call, 0, None)
+                .await
+                .expect("Client failed to call `call_builder::invoke`.")
+                .return_value();
+
+            assert_eq!(
+                call_result, expected_value,
+                "Decoded an unexpected value from the call."
+            );
+
+            Ok(())
+        }
+
+        #[ink_e2e::test]
+        async fn e2e_forward_call_return_value_errors_if_return_data_too_long(
+            mut client: ink_e2e::Client<C, E>,
+        ) -> E2EResult<()> {
+            let origin = client
+                .create_and_fund_account(&ink_e2e::alice(), 10_000_000_000_000)
+                .await;
+
+            let constructor = CallBuilderReturnValueRef::new(0);
+            let call_builder = client
+                .instantiate("call_builder_return_value", &origin, constructor, 0, None)
+                .await
+                .expect("instantiate failed");
+            let mut call_builder_call = call_builder.call::<CallBuilderReturnValue>();
+
+            let expected_value = 42;
+            let incrementer_constructor = IncrementerRef::new(expected_value);
+            let incrementer = client
+                .instantiate("incrementer", &origin, incrementer_constructor, 0, None)
+                .await
+                .expect("instantiate failed");
+
+            let selector = ink::selector_bytes!("get");
+            let call = call_builder_call
+                .forward_call_short_return_type(incrementer.account_id, selector);
             let call_result: Result<i8, String> = client
                 .call_dry_run(&origin, &call, 0, None)
                 .await
