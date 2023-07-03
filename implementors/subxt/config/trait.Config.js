3069fe463c68b59f7e598bd2539f414d69521f9b
paritytech-ci <paritytech-ci@parity.io>
2023-07-03 12:54:21 +0000

    Updated docs for master and pushed to gh-pages

--- implementors/subxt/config/trait.Config.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"ink_e2e":[["impl Config for <a class=\"enum\" href=\"ink_e2e/enum.SubstrateConfig.html\" title=\"enum ink_e2e::SubstrateConfig\">SubstrateConfig</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
