205f876284e4c8448879624b1e6eccb9ff1aa904
Andrew Jones <ascjones@gmail.com>
2023-07-05 15:50:56 +0100

    fmt

--- crates/metadata/src/lib.rs
+++ crates/metadata/src/lib.rs
@@ -172,8 +172,8 @@ pub fn collect_events() -> Vec<EventSpec> {
 
 /// Provides metadata about an ink! event.
 ///
-/// Implementations must be registered into the [`static@EVENTS`] distributed slice, in order to
-/// be included in the contract metadata. This is done automatically by the
+/// Implementations must be registered into the [`static@EVENTS`] distributed slice, in
+/// order to be included in the contract metadata. This is done automatically by the
 /// `#[derive(ink::EventMetadata)]`
 pub trait EventMetadata {
     /// The full path to the event type, usually provided by [`module_path`].
