0eaad9371271c8d0a8a3c6fcff4eb983b04fe04a
paritytech-ci <paritytech-ci@parity.io>
2023-07-24 14:24:48 +0000

    Updated docs for master and pushed to gh-pages

---
+++ ink_e2e/events/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["CodeStoredEvent","ContractEmitted","ContractInstantiatedEvent","EventWithTopics"]};
--- ink_macro/sidebar-items.js
+++ ink_macro/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"attr":["chain_extension","contract","storage_item","test","trait_definition"],"derive":["Storable","StorableHint","StorageKey","StorageLayout"],"macro":["blake2x256","selector_bytes","selector_id"]};
+window.SIDEBAR_ITEMS = {"attr":["chain_extension","contract","event","storage_item","test","trait_definition"],"derive":["Event","EventMetadata","Storable","StorableHint","StorageKey","StorageLayout"],"macro":["blake2x256","selector_bytes","selector_id"]};
