7902e6d11625baaba6a1cf2a7fcf4adafacaa749
Piotr Mikołajczyk <piomiko41@gmail.com>
2023-07-25 14:24:12 +0200

    Divide interface into general-chain and contracts-specific

--- crates/e2e/src/backend.rs
+++ crates/e2e/src/backend.rs
@@ -1,4 +1,13 @@
 use jsonrpsee::core::async_trait;
 
+/// Full E2E testing backend: combines general chain API and contract-specific operations.
 #[async_trait]
-pub trait E2EBackend {}
+pub trait E2EBackend: ChainBackend + ContractsBackend {}
+
+/// General chain operations useful in contract testing.
+#[async_trait]
+pub trait ChainBackend {}
+
+/// Contract-specific operations.
+#[async_trait]
+pub trait ContractsBackend {}
