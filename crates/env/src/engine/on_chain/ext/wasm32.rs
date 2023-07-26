939d5ee45e4b4532d0802f1767b5bac48aaf521f
SkymanOne <german@parity.io>
2023-07-26 23:26:09 +0200

    happy mid new year

--- crates/env/src/engine/on_chain/ext/wasm32.rs
+++ crates/env/src/engine/on_chain/ext/wasm32.rs
@@ -1,4 +1,4 @@
-// Copyright 2018-2022 Parity Technologies (UK) Ltd.
+// Copyright 2018-2023 Parity Technologies (UK) Ltd.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Copyright 2018-2022 Parity Technologies (UK) Ltd.
+// Copyright 2018-2023 Parity Technologies (UK) Ltd.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
