8bbfddb94002e91a1e7bf066fbeabaac9c03573b
paritytech-ci <paritytech-ci@parity.io>
2023-07-26 21:24:08 +0000

    Updated docs for master and pushed to gh-pages

---
+++ ink_e2e/sr25519/dev/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":["alice","bob","charlie","dave","eve","ferdie","one","two"]};
---
+++ ink_e2e/sr25519/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["Error"],"fn":["verify"],"mod":["dev"],"struct":["Keypair","PublicKey","Signature"],"type":["Seed"]};
