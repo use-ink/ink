9d0d198190ea4943aedcd7351b0833197a3a63a2
paritytech-ci <paritytech-ci@parity.io>
2023-07-31 14:44:33 +0000

    Updated docs for master and pushed to gh-pages

---
+++ implementors/ink_e2e/backend/trait.E2EBackend.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"ink_e2e":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
