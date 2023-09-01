99338aad8a2bca52f9ac34c8b520b28f424dd97a
Andrew Jones <ascjones@gmail.com>
2023-09-01 17:25:27 +0100

    Print success or failure

--- scripts/for_all_contracts_exec.sh
+++ scripts/for_all_contracts_exec.sh
@@ -16,9 +16,36 @@ EOF
 shopt -s globstar
 
 SCRIPTS_PATH=$( cd "$(dirname "$SCRIPT_NAME")" || exit; pwd -P )
+SUCCESSES=()
+FAILURES=()
 
 for manifest_path in "$FIND_PATH"/**/Cargo.toml;
   do if "$SCRIPTS_PATH"/is_contract.sh "$manifest_path"; then
     echo Running: "${@:2}" "$manifest_path";
+    "${@:2}" "$manifest_path";
+
+    if [ $? -eq 0 ]; then
+      SUCCESSES+=("$manifest_path")
+    else
+      FAILURES+=("$manifest_path")
+    fi
   fi
 done
+
+echo ""
+echo "Succeeded:" ${#SUCCESSES[@]}
+for success in "${SUCCESSES[@]}"; do
+  echo "$success"
+done
+
+echo ""
+echo "Failed:" ${#FAILURES[@]}
+for failure in "${FAILURES[@]}"; do
+  echo "$failure"
+done
+
+if [ ${#FAILURES[@]} -gt 0 ]; then
+  exit 1
+else
+  exit 0
+fi
