aa2efd85a9dbf77618615f9588245eb72af66b5e
Andrew Jones <ascjones@gmail.com>
2023-09-04 11:37:09 +0100

    Allow passing positional placeholder

--- scripts/for_all_contracts_exec.sh
+++ scripts/for_all_contracts_exec.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
 
-SCRIPT_NAME="${BASH_SOURCE[0]}"
-FIND_PATH=$1
-COMMAND=${@:2}
+script_name="${BASH_SOURCE[0]}"
+scripts_path=$( cd "$(dirname "$script_name")" || exit; pwd -P )
+find_path=$1
+command=( "${@:2}" )
 
 function usage {
   cat << EOF
-Usage: ${SCRIPT_NAME} FIND_PATH COMMAND [INITIAL-ARGS]
+Usage: ${script_name} FIND_PATH COMMAND [INITIAL-ARGS]
 
 Execute the supplied COMMAND with INITIAL-ARGS for all ink! contracts (recursively) found in the given path.
-The manifest path (full path to the Cargo.toml file) is passed as the last argument to the supplied command.
+The manifest path (full path to the Cargo.toml file) is passed either:
+  - in place of the "{}" placeholder in the command, if present
+  - as the last argument to the command, if "{}" is not present
 
 Returns 0 (success) if the command succeeds against *all* contract projects, if any fail returns 1 (failure).
 
-FIND_PATH
+find_path
   Path to recursively find contract projects for which to execute the supplied command
 
 EXAMPLES
-   ${SCRIPT_NAME} integration-tests cargo check --manifest-path
+   ${script_name} integration-tests cargo check --manifest-path
+   ${script_name} integration-tests cargo contract build --manifest-path {} --release
 
 EOF
 }
 
-if [ -z "$FIND_PATH" ] || [ -z "$COMMAND" ]; then
+if [ -z "$find_path" ] || [ "${#command[@]}" -le 0 ]; then
   usage
   exit 1
 fi
@@ -30,19 +34,29 @@ fi
 # enable recursive globs
 shopt -s globstar
 
-SCRIPTS_PATH=$( cd "$(dirname "$SCRIPT_NAME")" || exit; pwd -P )
-SUCCESSES=()
-FAILURES=()
+successes=()
+failures=()
 
-for manifest_path in "$FIND_PATH"/**/Cargo.toml;
-  do if "$SCRIPTS_PATH"/is_contract.sh "$manifest_path"; then
-    echo Running: "$COMMAND" "$manifest_path";
-    $COMMAND "$manifest_path";
+# default to adding the argument as the last argument to the command
+arg_index=${#command[@]}
+# find the index of the argument placeholder "{}", if present
+for i in "${!command[@]}"; do
+  if [ "${command[$i]}" = "{}" ]; then
+    arg_index=$i
+    break
+  fi
+done
+
+for manifest_path in "$find_path"/**/Cargo.toml;
+  do if "$scripts_path"/is_contract.sh "$manifest_path"; then
+    command[$arg_index]="$manifest_path"
+    echo Running: "${command[@]}"
+    "${command[@]}"
 
     if [ $? -eq 0 ]; then
-      SUCCESSES+=("$manifest_path")
+      successes+=("$manifest_path")
     else
-      FAILURES+=("$manifest_path")
+      failures+=("$manifest_path")
     fi
   fi
 done
@@ -51,17 +65,17 @@ GREEN='\033[1;32m'
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
-printf "\nSucceeded: %s\n" ${#SUCCESSES[@]}
-for success in "${SUCCESSES[@]}"; do
+printf "\nSucceeded: %s\n" ${#successes[@]}
+for success in "${successes[@]}"; do
   printf "  ${GREEN}\u2713${NC} %s \n" "$success"
 done
 
-printf "\nFailed: %s\n" ${#FAILURES[@]}
-for failure in "${FAILURES[@]}"; do
+printf "\nFailed: %s\n" ${#failures[@]}
+for failure in "${failures[@]}"; do
   printf "  ${RED}\u2717${NC} %s \n" "$failure"
 done
 
-if [ ${#FAILURES[@]} -gt 0 ]; then
+if [ ${#failures[@]} -gt 0 ]; then
   exit 1
 else
   exit 0
