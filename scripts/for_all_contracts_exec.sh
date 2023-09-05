0f32ff9a5c00f57d231c08b718c0525dcb152f06
Andrew Jones <ascjones@gmail.com>
2023-09-05 16:32:01 +0100

    Add option parsing with path

--- scripts/for_all_contracts_exec.sh
+++ scripts/for_all_contracts_exec.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-# enable recursive globs
-shopt -s globstar
-
-script_name="${BASH_SOURCE[0]}"
-scripts_path=$( cd "$(dirname "$script_name")" || exit; pwd -P )
-find_path=$1
-command=( "${@:2}" )
-
 function usage {
   cat << EOF
-Usage: ${script_name} FIND_PATH COMMAND [INITIAL-ARGS]
+Usage: ${script_name} [OPTION] --path PATH -- COMMAND [INITIAL-ARGS]
 
 Execute the supplied COMMAND with INITIAL-ARGS for all ink! contracts (recursively) found in the given path.
 The manifest path (full path to the Cargo.toml file) is passed either:
@@ -19,17 +11,58 @@ The manifest path (full path to the Cargo.toml file) is passed either:
 
 Returns 0 (success) if the command succeeds against *all* contract projects, if any fail returns 1 (failure).
 
-find_path
+OPTIONS
+  -i, --ignore
+  Path to ignore when recursively finding contract projects
+
+  -p, --path
   Path to recursively find contract projects for which to execute the supplied command
 
 EXAMPLES
-   ${script_name} integration-tests cargo check --manifest-path
-   ${script_name} integration-tests cargo contract build --manifest-path {} --release
+   ${script_name} -path integration-tests -- cargo check --manifest-path
+   ${script_name} -path integration-tests -- cargo contract build --manifest-path {} --release
 
 EOF
 }
 
-if [ -z "$find_path" ] || [ "${#command[@]}" -le 0 ]; then
+# enable recursive globs
+shopt -s globstar
+
+script_name="${BASH_SOURCE[0]}"
+scripts_path=$( cd "$(dirname "$script_name")" || exit; pwd -P )
+command=( "${@:2}" )
+
+options=$(getopt -o p:i: --long path:,ignore: -- "$@")
+[ $? -eq 0 ] || {
+    echo "Incorrect option provided"
+    usage
+    exit 1
+}
+eval set -- "$options"
+ignore=()
+while true; do
+    case "$1" in
+    -p|--path)
+        shift; # The arg is next in position args
+        path="$1"
+        ;;
+    -i|--ignore)
+        shift; # The arg is next in position args
+        ignore+=("$1")
+        ;;
+    --)
+        shift
+        break
+        ;;
+    esac
+    shift
+done
+
+command=("${@}")
+
+echo command "${command[@]}"
+
+if [ -z "$path" ] || [ "${#command[@]}" -le 0 ]; then
   usage
   exit 1
 fi
@@ -47,7 +80,7 @@ for i in "${!command[@]}"; do
   fi
 done
 
-for manifest_path in "$find_path"/**/Cargo.toml;
+for manifest_path in "$path"/**/Cargo.toml;
   do if "$scripts_path"/is_contract.sh "$manifest_path"; then
     command[$arg_index]="$manifest_path"
     echo Running: "${command[@]}"
