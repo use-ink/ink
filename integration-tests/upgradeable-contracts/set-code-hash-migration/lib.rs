b8e621a38d5a4e0195f4f8c5f414b01dec74f288
Andrew Jones <ascjones@gmail.com>
2023-09-13 11:42:45 +0100

    WIP

--- integration-tests/upgradeable-contracts/set-code-hash-migration/lib.rs
+++ integration-tests/upgradeable-contracts/set-code-hash-migration/lib.rs
@@ -94,7 +94,8 @@ pub mod incrementer {
 
             let get = call.get();
             let get_res = client.call_dry_run(&ink_e2e::alice(), &get, 0, None).await;
-            assert!(matches!(get_res.return_value(), 1));
+            let pre_migration_value = get_res.return_value();
+            assert!(matches!(pre_migration_value, 1));
 
             // Upload the code for the contract to be updated to after the migration.
             let new_code_hash = client
@@ -102,40 +103,39 @@ pub mod incrementer {
                 .await
                 .expect("uploading `updated-incrementer` failed")
                 .code_hash;
-
             let new_code_hash = new_code_hash.as_ref().try_into().unwrap();
 
-            // When
+            // Upload the code for the migration contract.
             let migration_contract = client
                 .upload("migration", &ink_e2e::alice(), None)
                 .await
                 .expect("uploading `migration` failed");
             let migration_code_hash = migration_contract.code_hash.as_ref().try_into().unwrap();
-            let migrate = migration_contract
-                .call::<migration::incrementer::Incrementer>()
-                .migrate();
 
-            let _migration_result = client
-                .call(
-                    &ink_e2e::alice(),
-                    &call.set_code(migration_code_hash),
-                    0,
-                    None,
-                )
-                .await
-                .expect("`set_code` failed");
+            // When
 
-            let set_code = call.set_code(new_code_hash);
-
+            // Set the code hash to the migration contract
+            let set_code = call.set_code(migration_code_hash);
             let _set_code_result = client
                 .call(&ink_e2e::alice(), &set_code, 0, None)
                 .await
                 .expect("`set_code` failed");
 
+            // Call the migration contract with a new value for `inc_by` and the code hash of the
+            // updated contract.
+            const NEW_INC_BY: u8 = 4;
+            let migrate = contract
+                .call::<migration::incrementer::Incrementer>()
+                .migrate(NEW_INC_BY, new_code_hash);
+
+            let _migration_result = client.call(&ink_e2e::alice(), &migrate, 0, None)
+                .await
+                .expect("`migrate` failed");
+
             // Then
             // Note that our contract's `AccountId` (so `contract_acc_id`) has stayed the
             // same between updates!
-            let inc = call.inc();
+            let inc = contract.call::<updated_incrementer::incrementer::Incrementer>().inc();
 
             let _inc_result = client
                 .call(&ink_e2e::alice(), &inc, 0, None)
@@ -146,7 +146,7 @@ pub mod incrementer {
             let get_res = client.call_dry_run(&ink_e2e::alice(), &get, 0, None).await;
 
             // Remember, we updated our incrementer contract to increment by `4`.
-            assert!(matches!(get_res.return_value(), 5));
+            assert!(matches!(get_res.return_value(), pre_migration_value + NEW_INC_BY.into()));
 
             Ok(())
         }
