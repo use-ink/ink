860a4cc648ea6395b8c84dc83e030b35d9b67173
andrew <ascjones@gmail.com>
2023-09-25 11:38:48 +0100

    awk vars

--- scripts/contract_sizes_diff.sh
+++ scripts/contract_sizes_diff.sh
@@ -8,5 +8,11 @@ NR==FNR {
   a[$1]=$2; next
 }
 $1 in a {
-  print "|" $1 "|" a[$1] "|" $2 "|" (($2 - a[$1]) / a[$1]) * 100 "|"
+  name=$1
+  up_kb=a[$1]/1000
+  pr_kb=$2/1000
+  diff=pr_kb - up_kb
+  diff_pc=(diff / up_kb) * 100
+
+  print "|" name "|" up_kb "|" pr_kb "|" diff_pc "|"
 }' "$upstream" "$pr_branch"
