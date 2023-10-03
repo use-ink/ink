ee3f98bbea64b567a38199ed9cab95822e4a8b45
Georgiy Komarov <jubnzv@gmail.com>
2023-10-03 19:09:45 +0700

    feat(test): Inserting in method arg

--- linting/ui/fail/storage_never_freed.rs
+++ linting/ui/fail/storage_never_freed.rs
@@ -11,9 +11,10 @@ pub mod storage_never_freed {
     pub struct StorageNeverFreed {
         // All the fields generate warnings, since there are `insert` operations for
         // them, but there are no `remove` operations.
-        vec_field: Vec<AccountId>,
+        vec_field_1: Vec<AccountId>,
         vec_field_subscription: Vec<AccountId>,
-        map_field: Mapping<AccountId, AccountId>,
+        map_field_1: Mapping<AccountId, AccountId>,
+        map_field_2: Mapping<AccountId, AccountId>,
         map_field_alias: MapAlias2<AccountId, AccountId>,
     }
 
@@ -21,18 +22,24 @@ pub mod storage_never_freed {
         #[ink(constructor)]
         pub fn new() -> Self {
             Self {
-                vec_field: Vec::new(),
+                vec_field_1: Vec::new(),
                 vec_field_subscription: Vec::new(),
-                map_field: Mapping::new(),
+                map_field_1: Mapping::new(),
+                map_field_2: Mapping::new(),
                 map_field_alias: Mapping::new(),
             }
         }
 
+        fn flip(a: bool) -> bool {
+            !a
+        }
+
         #[ink(message)]
         pub fn add_to_fields(&mut self, v: AccountId) {
-            self.vec_field.push(v);
-            self.map_field.insert(v, &v);
+            self.vec_field_1.push(v);
             self.vec_field_subscription[0] = v;
+            self.map_field_1.insert(v, &v);
+            let _ = Self::flip(self.map_field_2.insert(v, &v).is_some());
             self.map_field_alias.insert(v, &v);
         }
     }
