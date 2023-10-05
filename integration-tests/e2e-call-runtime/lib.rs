890c9a67c8d3b55b5b7096e22c6af9ee2f57b2bc
pgherveou <pgherveou@gmail.com>
2023-10-05 09:54:37 +0200

    Substrate contract node bump

--- integration-tests/e2e-call-runtime/lib.rs
+++ integration-tests/e2e-call-runtime/lib.rs
@@ -59,9 +59,15 @@ pub mod e2e_call_runtime {
                 .await
                 .return_value();
 
+
             // Send funds from Alice to the contract using Balances::transfer
             client
-                .runtime_call(&ink_e2e::alice(), "Balances", "transfer", call_data)
+                .runtime_call(
+                    &ink_e2e::alice(),
+                    "Balances",
+                    "transfer_allow_death",
+                    call_data,
+                )
                 .await
                 .expect("runtime call failed");
 
