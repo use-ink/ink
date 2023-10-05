96c9d6d3d269acd7e28d03388d31768d98b1a284
Andrew Jones <ascjones@gmail.com>
2023-10-05 12:44:05 +0100

    Generate markdown table from diff

--- scripts/contract_sizes_diff.sh
+++ scripts/contract_sizes_diff.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
+script_name="${BASH_SOURCE[0]}"
+scripts_path=$( cd "$(dirname "$script_name")" || exit; pwd -P )
+
 upstream=$1
 pr_branch=$2
 
-awk '
+csv=$(awk '
 NR==FNR {
   a[$1]=$2; next
 }
+BEGIN{print "Contract,Optimized Size (Upstream),Optimized Size (PR),Diff (kB),Diff (%)"};
 $1 in a {
   name=$1
   up_kb=a[$1]/1000
   pr_kb=$2/1000
-  diff=pr_kb - up_kb
-  diff_pc=(diff / up_kb) * 100
+  diff_kb=pr_kb - up_kb
+  diff_pc=(diff_kb / up_kb) * 100
 
-  print "|" name "|" up_kb "|" pr_kb "|" diff_pc "|"
-}' "$upstream" "$pr_branch"
+  print name","up_kb","pr_kb","diff_kb","diff_pc
+}' "$upstream" "$pr_branch")
+
+"${scripts_path}"/markdown-table.sh --csv <<< "${csv}"
