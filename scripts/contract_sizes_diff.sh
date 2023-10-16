233cf2d92b0c9d197f0207c34f15f66502550ced
Andrew Jones <ascjones@gmail.com>
2023-10-16 18:24:49 +0100

    Add change column

--- scripts/contract_sizes_diff.sh
+++ scripts/contract_sizes_diff.sh
@@ -10,7 +10,7 @@ csv=$(awk '
 NR==FNR {
   a[$1]=$2; next
 }
-BEGIN{print "Contract,Optimized Size (Upstream),Optimized Size (PR),Diff (kB),Diff (%)"};
+BEGIN{print "Contract,Upstream Size (kB),PR Size (kB),Diff (kB),Diff (%),Change"};
 $1 in a {
   name=$1
   up_kb=a[$1]/1000
@@ -18,7 +18,15 @@ $1 in a {
   diff_kb=pr_kb - up_kb
   diff_pc=(diff_kb / up_kb) * 100
 
-  print name","up_kb","pr_kb","diff_kb","diff_pc
+  if (diff_kb > 0) {
+    change=":chart_with_upwards_trend:"
+  } else if (diff_kb < 0) {
+    change=":chart_with_downwards_trend:"
+  } else {
+    change=":heavy_minus_sign:"
+  }
+
+  print name","up_kb","pr_kb","diff_kb","diff_pc","change
 }' "$upstream" "$pr_branch")
 
 "${scripts_path}"/markdown-table.sh --csv <<< "${csv}"
