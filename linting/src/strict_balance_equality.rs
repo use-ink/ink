a6fb3d0e9f0d87a65c16c8a2588ee03ace25dd08
Georgiy Komarov <jubnzv@gmail.com>
2023-11-03 19:13:23 +0700

    feat(linter): Automate the generation of documentation for ink-docs
    This commit introduces a script that extracts documentation from
    `ink_linting` lints and saves it to the local [ink-docs](https://github.com/paritytech/ink-docs) directory.
    
    The markdown formatting of the lints was also improved.

--- linting/src/strict_balance_equality.rs
+++ linting/src/strict_balance_equality.rs
@@ -72,22 +72,25 @@ use std::collections::{
 };
 
 declare_lint! {
-    /// **What it does:** Looks for strict equalities with balance in ink! contracts.
+    /// ## What it does
+    /// Looks for strict equalities with balance in ink! contracts.
     ///
-    /// **Why is this bad?** The problem with strict balance equality is that it is always possible
-    /// to forcibly send tokens to a contract. For example, using
+    /// ## Why is this bad?
+    /// The problem with strict balance equality is that it is always possible to forcibly send
+    /// tokens to a contract. For example, using
     /// [`terminate_contract`](https://paritytech.github.io/ink/ink_env/fn.terminate_contract.html).
     /// In such a case, the condition involving the contract balance will work incorrectly, what
     /// may lead to security issues, including DoS attacks and draining contract's gas.
     ///
-    /// **Known problems**: There are many ways to implement balance comparison in ink! contracts.
-    /// This lint is not trying to be exhaustive. Instead, it addresses the most common cases that
-    /// may occur in real-world contracts and focuses on precision and lack of false positives.
+    /// ## Known problems
+    /// There are many ways to implement balance comparison in ink! contracts. This lint is not
+    /// trying to be exhaustive. Instead, it addresses the most common cases that may occur in
+    /// real-world contracts and focuses on precision and lack of false positives.
     ///
-    /// **Example:**
-    ///
+    /// ## Example
     /// Assume, there is an attacker contract that sends all its funds to the target contract when
     /// terminated:
+    ///
     /// ```rust
     /// #[ink::contract]
     /// pub mod attacker {
@@ -101,6 +104,7 @@ declare_lint! {
     ///
     /// If the target contains a condition with strict balance equality, this may be manipulated by
     /// the attacker:
+    ///
     /// ```rust
     /// #[ink::contract]
     /// pub mod target {
@@ -116,6 +120,7 @@ declare_lint! {
     ///
     /// This could be mitigated using non-strict equality operators in the condition with the
     /// balance:
+    ///
     /// ```rust
     /// #[ink::contract]
     /// pub mod target {
---
+++ scripts/generate_linter_docs.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+usage() {
+  echo "Usage: $0 PATH"
+  echo "Creates or updates the documentation for ink_linting hosted at use.ink."
+  echo
+  echo "Arguments:"
+  echo "  PATH Path to the local clone of https://github.com/paritytech/ink-docs"
+  echo
+  echo "Examples:"
+  echo "  $0 ~/ink-docs"
+}
+
+if [ ! $# -eq 1 ]; then
+    usage
+    exit 1
+fi
+
+ink_docs_dir="$1"
+tmp_file=$(mktemp /tmp/linting_docs.XXXXXX.md)
+had_update=0
+for lint_src in linting/src/*.rs; do
+    lint_name="$(basename "${lint_src%.*}")"
+    lint_doc_file="$ink_docs_dir/docs/linter/rules/$lint_name.md"
+    lint_doc_filestring="$(sed -n '/^declare_lint! {$/,/^}$/p' "$lint_src")"
+    [[ -z "$lint_doc_filestring" ]] && continue
+
+    # Save the extracted documentation to the temporary file
+    truncate -s 0 "$tmp_file"
+    {
+        echo "---"
+        echo "title: $lint_name"
+        echo "hide_title: true"
+        echo "description: $lint_name lint documentation"
+        echo "---"
+        echo "# $lint_name"
+        printf "%s" "$lint_doc_filestring" | sed -n 's/^\s*\/\/\/\s\?\(.*\)$/\1/;T;p'
+    } >> "$tmp_file"
+
+    if [[ ! -f "$lint_doc_file" ]]; then
+        echo "$lint_name: created documentation"
+        had_update=1
+        mv "$tmp_file" "$lint_doc_file"
+        continue
+    fi
+
+    if cmp -s "$tmp_file" "$lint_doc_file"; then
+        echo "$lint_name: no changes"
+    else
+        echo "$lint_name: updated"
+        mv "$tmp_file" "$lint_doc_file"
+    fi
+done
+
+[[ $had_update -eq 1 ]] && echo -e "\nPlease add created documentation sections to $ink_docs_dir/sidebars.js"
