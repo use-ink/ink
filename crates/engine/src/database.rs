94f3484a3661eaa590a13190f4a7565ebc58e5e8
Brian Ramirez <ramirez7358@gmail.com>
2023-11-08 12:05:25 -0300

    own_code_hash implementation

--- crates/engine/src/database.rs
+++ crates/engine/src/database.rs
@@ -36,7 +36,7 @@ pub fn storage_of_contract_key(who: &[u8], key: &[u8]) -> [u8; 32] {
     hashed_key
 }
 
-pub fn code_hash_of_key(key: &Vec<u8>) -> [u8; 32]{
+pub fn code_hash_of_key(key: &Vec<u8>) -> [u8; 32] {
     let keyed = key.to_keyed_vec(CODE_HASH_OF);
     let mut hashed_key: [u8; 32] = [0; 32];
     super::hashing::blake2b_256(&keyed[..], &mut hashed_key);
@@ -138,7 +138,7 @@ impl Database {
     }
 
     pub fn set_code_hash(&mut self, account: &Vec<u8>, code_hash: &[u8]) {
-        let hashed_key = code_hash_of_key(&account);
+        let hashed_key = code_hash_of_key(account);
         self.hmap
             .entry(hashed_key.to_vec())
             .and_modify(|x| *x = code_hash.to_vec())
@@ -146,7 +146,7 @@ impl Database {
     }
 
     pub fn get_code_hash(&self, account: &Vec<u8>) -> Option<Vec<u8>> {
-        let hashed_key = code_hash_of_key(&account);
+        let hashed_key = code_hash_of_key(account);
         self.get(&hashed_key).cloned()
     }
 }
