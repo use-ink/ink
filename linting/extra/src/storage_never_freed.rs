b05e7f213d1299e4e38552fba4d5d8c30b81fb3a
Georgiy Komarov <jubnzv@gmail.com>
2024-01-18 05:00:16 -0500

    Linter: Automate the generation of documentation for `ink-docs` (#1972)
    * feat(linter): Automate the generation of documentation for ink-docs
    
    This commit introduces a script that extracts documentation from
    `ink_linting` lints and saves it to the local [ink-docs](https://github.com/paritytech/ink-docs) directory.
    
    The markdown formatting of the lints was also improved.
    
    * chore(script): Update after #2032 is merged
    
    * chore(linter): Wrap in docstring to improve appearance
    
    * Update scripts/generate_linter_docs.sh
    
    ---------
    
    Co-authored-by: Michael Müller <mich@elmueller.net>

--- linting/extra/src/storage_never_freed.rs
+++ linting/extra/src/storage_never_freed.rs
@@ -63,18 +63,17 @@ use std::collections::{
 };
 
 declare_lint! {
-    /// **What it does:**
+    /// ## What it does
     /// This lint ensures that for every storage field with a collection type, when there is an
     /// operation to insert new elements, there's also an operation for removing elements.
     ///
-    /// **Why is this bad?**
+    /// ## Why is this bad?
     /// When a user executes a contract function that writes to storage, they have to put a
     /// deposit down for the amount of storage space used. Whoever frees up that storage at some
     /// later point gets the deposit back. Therefore, it is always a good idea to make it possible
     /// for users to free up their storage space.
     ///
-    /// **Example:**
-    ///
+    /// ## Example
     /// In the following example there is a storage field with the `Mapping` type that has an
     /// function that inserts new elements:
     ///
