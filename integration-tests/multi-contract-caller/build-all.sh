9900cc92777095d264b5fe5f78b0fa3ebe8640f8
Michael Mueller <mich@elmueller.net>
2024-01-26 12:01:55 +0100

    Revert me: Run CI with isolated examples

--- integration-tests/multi-contract-caller/build-all.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu
-
-cargo contract build --manifest-path accumulator/Cargo.toml
-cargo contract build --manifest-path adder/Cargo.toml
-cargo contract build --manifest-path subber/Cargo.toml
-cargo contract build
