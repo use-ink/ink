1ea20a2f262e0999f696dc8d89001cdae5308290
Michael Müller <michi@parity.io>
2024-01-29 15:07:26 +0100

    [E2E] Add `create_call_builder` for testing on-chain contracts (#2075)
    * Reexport `ContractCallBuilder` from `ink_e2e`
    
    * Add `create_call_builder(AccountId)`
    
    * Revert "Reexport `ContractCallBuilder` from `ink_e2e`"
    
    This reverts commit 6377e2d11f78437b08ac7a6eb36268d90cc54302.
    
    * Apply `cargo fmt`
    
    * Add CI test
    
    * Improve code
    
    * Make spellchecker happy
    
    * Fix CI
    
    * Revert me: Run CI with isolated examples
    
    * Fix CI
    
    * Fix CI
    
    * Make `rustfmt` happy
    
    * Make `rustfmt` happy
    
    * Revert me: Install `subkey`
    
    * Revert me: Debug CI
    
    * Fix CI
    
    * Improve formatting
    
    * Remove local `subkey` installation
    
    * Revert "Revert me: Run CI with isolated examples"
    
    This reverts commit 9900cc92777095d264b5fe5f78b0fa3ebe8640f8.
    
    * Use newer Docker image
    
    * Improve variable name
    
    * Update UI tests for new Rust version from Docker
    
    * Improve structure
    
    * Update UI tests for new Rust version from Docker
    
    * Make `rustfmt` happy
    
    * Update  image to 1.75

--- crates/e2e/src/lib.rs
+++ crates/e2e/src/lib.rs
@@ -82,6 +82,12 @@ pub use {
     drink_client::Client as DrinkClient,
 };
 
+use ink::codegen::ContractCallBuilder;
+use ink_env::{
+    call::FromAccountId,
+    ContractEnv,
+    Environment,
+};
 use pallet_contracts_primitives::{
     ContractExecResult,
     ContractInstantiateResult,
@@ -126,6 +132,21 @@ pub fn log_error(msg: &str) {
 pub fn account_id(account: AccountKeyring) -> ink_primitives::AccountId {
     ink_primitives::AccountId::try_from(account.to_account_id().as_ref())
         .expect("account keyring has a valid account id")
+}
+
+/// Creates a call builder builder for `Contract`, based on an account id.
+pub fn create_call_builder<Contract>(
+    acc_id: <<Contract as ContractEnv>::Env as Environment>::AccountId,
+) -> <Contract as ContractCallBuilder>::Type
+where
+    <Contract as ContractEnv>::Env: Environment,
+    Contract: ContractCallBuilder,
+    Contract: ContractEnv,
+    Contract::Type: FromAccountId<<Contract as ContractEnv>::Env>,
+{
+    <<Contract as ContractCallBuilder>::Type as FromAccountId<
+        <Contract as ContractEnv>::Env,
+    >>::from_account_id(acc_id)
 }
 
 /// Builds a contract and imports its scaffolded structure as a module.
