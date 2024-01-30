af3f9cd4bcf278af4286aed36c6a3fdd17be0e35
Andrew Jones <ascjones@gmail.com>
2024-01-30 16:39:41 +0000

    Revert "Remove `basic_contract_caller` integration test, moved to #1909"
    This reverts commit 8f3ab318035735545af513893556658b709c5244.

---
+++ integration-tests/basic-contract-caller/e2e_tests.rs
@@ -0,0 +1,40 @@
+use super::basic_contract_caller::*;
+use ink_e2e::ContractsBackend;
+
+type E2EResult<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+#[ink_e2e::test]
+async fn flip_and_get<Client: E2EBackend>(
+    mut client: Client,
+) -> E2EResult<()> {
+    // given
+    let other_contract_code = client
+        .upload("other-contract", &ink_e2e::alice())
+        .submit()
+        .await
+        .expect("other_contract upload failed");
+
+    let mut constructor = BasicContractCallerRef::new(other_contract_code.code_hash);
+    let contract = client
+        .instantiate("basic-contract-caller", &ink_e2e::alice(), &mut constructor)
+        .submit()
+        .await
+        .expect("basic-contract-caller instantiate failed");
+    let mut call_builder = contract.call_builder::<BasicContractCaller>();
+    let call = call_builder.flip_and_get();
+
+    // when
+    let result = client
+        .call(
+            &ink_e2e::alice(),
+            &call,
+        )
+        .submit()
+        .await
+        .expect("Calling `flip_and_get` failed")
+        .return_value();
+
+    assert_eq!(result, false);
+
+    Ok(())
+}
