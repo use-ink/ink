16d14790c3d500577b83222d068b26cc75f56d75
Andrew Jones <ascjones@gmail.com>
2024-02-01 18:12:58 +0000

    WIP e2e tests

--- integration-tests/cross-contract-calls/lib.rs
+++ integration-tests/cross-contract-calls/lib.rs
@@ -2,16 +2,21 @@
 
 #[ink::contract]
 mod cross_contract_calls {
-    /// We import the generated `ContractRef` of our other contract.
-    ///
-    /// Note that the other contract must have re-exported it (`pub use
-    /// OtherContractRef`) for us to have access to it.
-    use other_contract::OtherContractRef;
+    use ink::{
+        codegen::ContractCallBuilder,
+        env::{
+            ContractEnv,
+            call::FromAccountId
+        },
+    };
+    use other_contract::{OtherContract, OtherContractRef};
+
+    type Env = <CrossContractCalls as ContractEnv>::Env;
+    type OtherContractCallBuilder = <OtherContract as ContractCallBuilder>::Type;
 
     #[ink(storage)]
     pub struct CrossContractCalls {
-        /// We specify that our contract will store a reference to the `OtherContract`.
-        other_contract: AccountId,
+        other_contract_call_builder: OtherContractCallBuilder,
     }
 
     impl CrossContractCalls {
@@ -19,15 +24,18 @@ mod cross_contract_calls {
         ///
         /// To do this we will use the uploaded `code_hash` of `OtherContract`.
         #[ink(constructor)]
-        pub fn new(other_contract: AccountId) -> Self {
-            // todo: need to add instantiate_v2 methods...
+        pub fn new(other_contract_code_hash: Hash) -> Self {
             let other_contract = OtherContractRef::new(true)
                 .code_hash(other_contract_code_hash)
                 .endowment(0)
                 .salt_bytes([0xDE, 0xAD, 0xBE, 0xEF])
                 .instantiate();
 
-            Self { other_contract }
+            let other_contract_call_builder = <OtherContractCallBuilder as FromAccountId<Env>>::from_account_id(
+                *other_contract.as_ref(),
+            );
+
+            Self { other_contract_call_builder }
         }
 
         /// Using the `ContractRef` we can call all the messages of the `OtherContract` as
@@ -35,9 +43,16 @@ mod cross_contract_calls {
         /// are!).
         #[ink(message)]
         pub fn flip_and_get(&mut self) -> bool {
-            // todo: add calls to `call_v2`
-            self.other_contract.flip();
-            self.other_contract.get()
+            self.other_contract_call_builder
+                .flip()
+                // .v2()
+                // .weight_limit(0, 0)
+                .invoke();
+
+            self.other_contract_call_builder
+                .get()
+                // .v2()
+                .invoke()
         }
     }
 }
