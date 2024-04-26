3588c6c2fc5312bbc6351d648a39ac67f2196f89
actions <actions@github.com>
2024-04-26 17:00:33 +0000

    Updated docs for master} and pushed to gh-pages

--- trait.impl/ink_sandbox/trait.Sandbox.js
+++ trait.impl/ink_sandbox/trait.Sandbox.js
@@ -1,3 +1,4 @@
 (function() {var implementors = {
+"ink_e2e":[["impl Sandbox for <a class=\"struct\" href=\"ink_e2e/preset/mock_network/struct.MockNetworkSandbox.html\" title=\"struct ink_e2e::preset::mock_network::MockNetworkSandbox\">MockNetworkSandbox</a>"]],
 "ink_sandbox":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
