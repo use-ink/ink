d647c8aefe2bc6ae4c41326a3d4b9ab07327c3d2
Michael Müller <michi@parity.io>
2024-04-26 12:03:54 +0200

    Update owners (#2222)
    * Update license owner
    
    * Update crate authors

--- crates/ink/src/option_info.rs
+++ crates/ink/src/option_info.rs
@@ -1,4 +1,4 @@
-// Copyright (C) Parity Technologies (UK) Ltd.
+// Copyright (C) Use Ink (UK) Ltd.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
