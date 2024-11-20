e0d834c2b1e517fcdaed4768dfdeadb6deb864f3
Andrew Jones <ascjones@gmail.com>
2024-11-20 12:05:20 +0000

    WIP e2e integration test

--- integration-tests/public/rlp/e2e_tests.rs
+++ integration-tests/public/rlp/e2e_tests.rs
@@ -3,25 +3,33 @@ use ink_e2e::ContractsBackend;
 
 type E2EResult<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
-#[ink_e2e::test]
-async fn get_contract_storage_consumes_entire_buffer<Client: E2EBackend>(
+#[ink_e2e::test(backend(runtime_only))]
+async fn call_rlp_encoded_message<Client: E2EBackend>(
     mut client: Client,
 ) -> E2EResult<()> {
     // given
     let mut constructor = RlpRef::new(false);
+    // the constructor is still SCALE encoded so use the E2E client still
     let contract = client
         .instantiate("rlp", &ink_e2e::alice(), &mut constructor)
         .submit()
         .await
         .expect("instantiate failed");
-    let mut call_builder = contract.call_builder::<Rlp>();
-
-    // when
-    let result = client
-        .call(&ink_e2e::alice(), &call_builder.set_value(true))
-        .submit()
-        .await
-        .expect("Calling `set_value` failed");
+    
+    let set_value_selector = Vec::<u8>::new(); // todo: calculate selector
+    let mut buf = Vec::new();
+    let set_value_data = ink::rlp::Encodable::encode(&true, &mut buf);
+    
+    let sandbox = ink_e2e::DefaultSandbox::default();
+    
+    // let mut call_builder = contract.call_builder::<Rlp>();
+    // 
+    // // when
+    // let result = client
+    //     .call(&ink_e2e::alice(), &call_builder.set_value(true))
+    //     .submit()
+    //     .await
+    //     .expect("Calling `set_value` failed");
 
     Ok(())
 }
