d86f5f7994f6afe1df78fdb137bab1062d2e8192
Andrew Jones <ascjones@gmail.com>
2024-11-27 11:01:17 +0000

    WIP e2e integration test

--- integration-tests/public/rlp/e2e_tests.rs
+++ integration-tests/public/rlp/e2e_tests.rs
@@ -7,20 +7,36 @@ type E2EResult<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 async fn call_rlp_encoded_message<Client: E2EBackend>(
     mut client: Client,
 ) -> E2EResult<()> {
+    let caller = ink_e2e::alice();
     // given
     let mut constructor = RlpRef::new(false);
-    // the constructor is still SCALE encoded so use the E2E client still
+    // todo: instantiate the contract via the sandbox instance...
     let contract = client
-        .instantiate("rlp", &ink_e2e::alice(), &mut constructor)
+        .instantiate("rlp", &caller, &mut constructor)
         .submit()
         .await
         .expect("instantiate failed");
     
-    let set_value_selector = Vec::<u8>::new(); // todo: calculate selector
-    let mut buf = Vec::new();
-    let set_value_data = ink::rlp::Encodable::encode(&true, &mut buf);
+    let set_value_selector = Vec::<u8>::new(); // todo: calculate and append selector
+    let mut set_value_data = Vec::new();
+    ink::rlp::Encodable::encode(&true, &mut set_value_data);
     
-    let sandbox = ink_e2e::DefaultSandbox::default();
+    let mut sandbox = ink_e2e::DefaultSandbox::default();
+    let account_id = (*<ink::primitives::AccountId as AsRef<[u8; 32]>>::as_ref(&contract.account_id)).into();
+
+    <ink_e2e::DefaultSandbox as ink_sandbox::api::contracts_api::ContractAPI>
+        ::call_contract(
+            &mut sandbox,
+            account_id,
+            0,
+            set_value_data,
+            caller.public_key().0.into(),
+            <ink_e2e::DefaultSandbox as ink_sandbox::Sandbox>::default_gas_limit(),
+            None,
+            pallet_contracts::Determinism::Enforced,
+        )
+        .result
+        .expect("sandbox call contract failed");
     
     // let mut call_builder = contract.call_builder::<Rlp>();
     // 
