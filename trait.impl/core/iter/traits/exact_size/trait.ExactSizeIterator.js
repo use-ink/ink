db325f3ec234d46486eb32d859633a29fd4bcfd1
actions <actions@github.com>
2025-01-24 14:54:03 +0000

    Updated docs for master} and pushed to gh-pages

--- trait.impl/core/iter/traits/exact_size/trait.ExactSizeIterator.js
+++ trait.impl/core/iter/traits/exact_size/trait.ExactSizeIterator.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["ink_ir",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/iter/traits/exact_size/trait.ExactSizeIterator.html\" title=\"trait core::iter::traits::exact_size::ExactSizeIterator\">ExactSizeIterator</a> for <a class=\"struct\" href=\"ink_ir/struct.InputsIter.html\" title=\"struct ink_ir::InputsIter\">InputsIter</a>&lt;'_&gt;"]]]]);
+    var implementors = Object.fromEntries([["ink_ir",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/exact_size/trait.ExactSizeIterator.html\" title=\"trait core::iter::traits::exact_size::ExactSizeIterator\">ExactSizeIterator</a> for <a class=\"struct\" href=\"ink_ir/struct.InputsIter.html\" title=\"struct ink_ir::InputsIter\">InputsIter</a>&lt;'_&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[351]}
+//{"start":57,"fragment_lengths":[352]}
--- trait.impl/core/ops/drop/trait.Drop.js
+++ trait.impl/core/ops/drop/trait.Drop.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["ink_e2e",[["impl&lt;R&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"ink_e2e/struct.TestNodeProcess.html\" title=\"struct ink_e2e::TestNodeProcess\">TestNodeProcess</a>&lt;R&gt;<div class=\"where\">where\n    R: Config,</div>"]]]]);
+    var implementors = Object.fromEntries([["ink_e2e",[["impl&lt;R&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"ink_e2e/struct.TestNodeProcess.html\" title=\"struct ink_e2e::TestNodeProcess\">TestNodeProcess</a>&lt;R&gt;<div class=\"where\">where\n    R: Config,</div>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[357]}
+//{"start":57,"fragment_lengths":[358]}
