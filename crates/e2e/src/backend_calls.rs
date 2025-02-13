d71c6fc1c7b2d6215feb4113c435fdb652b3aa6a
Skylar Ray <137945430+sky-coderay@users.noreply.github.com>
2025-02-13 11:38:11 +0200

    Update backend_calls.rs

--- crates/e2e/src/backend_calls.rs
+++ crates/e2e/src/backend_calls.rs
@@ -270,7 +270,7 @@ where
     /// # Notes
     ///
     /// Overwrites any values specified for `extra_gas_portion`.
-    /// The gas estimate fro dry-run will be ignored.
+    /// The gas estimate from dry-run will be ignored.
     pub fn gas_limit(&mut self, limit: Weight) -> &mut Self {
         if limit == Weight::from_parts(0, 0) {
             self.gas_limit = None
