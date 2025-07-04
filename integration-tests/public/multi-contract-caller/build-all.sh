2d6f15b188248bd80cc3062ef0eb7b4ddbc46dca
Michael Mueller <mich@elmueller.net>
2025-07-04 09:51:55 +0200

    Use `cargo-contract-nightly`

--- integration-tests/public/multi-contract-caller/build-all.sh
+++ integration-tests/public/multi-contract-caller/build-all.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-cargo-contract-nightly build --manifest-path accumulator/Cargo.toml
-cargo-contract-nightly build --manifest-path adder/Cargo.toml
-cargo-contract-nightly build --manifest-path subber/Cargo.toml
-cargo-contract-nightly build
+cargo-contract-nightly contract build --manifest-path accumulator/Cargo.toml
+cargo-contract-nightly contract build --manifest-path adder/Cargo.toml
+cargo-contract-nightly contract build --manifest-path subber/Cargo.toml
+cargo-contract-nightly contract build
