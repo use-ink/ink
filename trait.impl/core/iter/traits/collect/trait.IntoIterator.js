93c1b2285821f8ffe8974df2c0b018e1c0756124
actions <actions@github.com>
2025-07-27 11:59:19 +0000

    Updated docs for master} and pushed to gh-pages

--- trait.impl/core/iter/traits/collect/trait.IntoIterator.js
+++ trait.impl/core/iter/traits/collect/trait.IntoIterator.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["ink_ir",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/iter/traits/collect/trait.IntoIterator.html\" title=\"trait core::iter::traits::collect::IntoIterator\">IntoIterator</a> for <a class=\"struct\" href=\"ink_ir/ast/struct.AttributeArgs.html\" title=\"struct ink_ir::ast::AttributeArgs\">AttributeArgs</a>"]]]]);
+    var implementors = Object.fromEntries([["ink_ir",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/iter/traits/collect/trait.IntoIterator.html\" title=\"trait core::iter::traits::collect::IntoIterator\">IntoIterator</a> for <a class=\"struct\" href=\"ink_ir/ast/struct.AttributeArgs.html\" title=\"struct ink_ir::ast::AttributeArgs\">AttributeArgs</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- trait.impl/core/iter/traits/exact_size/trait.ExactSizeIterator.js
+++ trait.impl/core/iter/traits/exact_size/trait.ExactSizeIterator.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["ink_ir",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/iter/traits/exact_size/trait.ExactSizeIterator.html\" title=\"trait core::iter::traits::exact_size::ExactSizeIterator\">ExactSizeIterator</a> for <a class=\"struct\" href=\"ink_ir/struct.InputsIter.html\" title=\"struct ink_ir::InputsIter\">InputsIter</a>&lt;'_&gt;"]]]]);
+    var implementors = Object.fromEntries([["ink_ir",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/iter/traits/exact_size/trait.ExactSizeIterator.html\" title=\"trait core::iter::traits::exact_size::ExactSizeIterator\">ExactSizeIterator</a> for <a class=\"struct\" href=\"ink_ir/struct.InputsIter.html\" title=\"struct ink_ir::InputsIter\">InputsIter</a>&lt;'_&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
