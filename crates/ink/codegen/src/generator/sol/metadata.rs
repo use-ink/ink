49bedc30c5cec924e7a8c2903a072e161cafe546
Michael Mueller <mich@elmueller.net>
2025-08-29 23:42:39 +0200

    Make clippy happy

--- crates/ink/codegen/src/generator/sol/metadata.rs
+++ crates/ink/codegen/src/generator/sol/metadata.rs
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![cfg_attr(feature = "std", allow(dead_code))]
+#![cfg_attr(feature = "std", allow(unused))]
+
 use derive_more::From;
 use ir::{
     Callable as _,
