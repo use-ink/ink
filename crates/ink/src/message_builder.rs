c92cdcd324df9af42aee70a2edb80164fa14586f
Michael Mueller <mich@elmueller.net>
2025-08-29 22:20:07 +0200

    Add missing property

--- crates/ink/src/message_builder.rs
+++ crates/ink/src/message_builder.rs
@@ -78,6 +78,7 @@
 ///
 /// impl ink_env::Environment for CustomEnv {
 ///     const MAX_EVENT_TOPICS: usize = 3;
+///     const NATIVE_TO_ETH_RATIO: u32 = 100_000_000;
 ///     type AccountId = [u8; 32];
 ///     type Balance = u64;
 ///     type Hash = [u8; 32];
