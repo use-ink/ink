787a54c4bb9db93d186ae59fe02b88095f123cce
davidsemakula <hello@davidsemakula.com>
2025-08-30 14:47:47 +0300

    chore: Remove unused `ScopedBuffer::append_encoded`

--- crates/env/src/engine/on_chain/buffer.rs
+++ crates/env/src/engine/on_chain/buffer.rs
@@ -202,25 +202,6 @@ impl<'a> ScopedBuffer<'a> {
         self.take(encode_len)
     }
 
-    /// Appends the encoding of `value` to the scoped buffer.
-    ///
-    /// Does not return the buffer immediately so that other values can be appended
-    /// afterwards. The [`take_appended`] method shall be used to return the buffer
-    /// that includes all appended encodings as a single buffer.
-    #[inline(always)]
-    pub fn append_encoded<T>(&mut self, value: &T)
-    where
-        T: scale::Encode,
-    {
-        let offset = self.offset;
-        let buffer = core::mem::take(&mut self.buffer);
-        let mut encode_scope = EncodeScope::from(&mut buffer[offset..]);
-        scale::Encode::encode_to(&value, &mut encode_scope);
-        let encode_len = encode_scope.len();
-        self.offset = self.offset.checked_add(encode_len).unwrap();
-        let _ = core::mem::replace(&mut self.buffer, buffer);
-    }
-
     /// Appends the given bytes to the scoped buffer.
     ///
     /// Does not return the buffer immediately so that other values can be appended
