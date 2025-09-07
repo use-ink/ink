362d794561afb9142033ef72fe21757e982814eb
davidsemakula <hello@davidsemakula.com>
2025-09-07 21:43:13 +0300

    env: Limit implementations of `SomeRemainingTopics` and `EventTopicsAmount` to 4 topics

--- crates/env/src/event.rs
+++ crates/env/src/event.rs
@@ -210,13 +210,8 @@ macro_rules! impl_some_remaining_for {
         )*
     };
 }
-#[rustfmt::skip]
-impl_some_remaining_for!(
-             2,  3,  4,  5,  6,  7,  8,  9,
-    10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-    20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
-    30, 31, 32,
-);
+
+impl_some_remaining_for!(2, 3, 4);
 
 impl SomeRemainingTopics for [state::HasRemainingTopics; 1] {
     type Next = state::NoRemainingTopics;
