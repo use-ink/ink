5f1bf7106d91d5fb7c2cfe05487877b10518411a
actions <actions@github.com>
2025-10-08 23:10:14 +0000

    Updated docs for master and pushed to gh-pages

--- ink_primitives/sidebar-items.js
+++ ink_primitives/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["LangError"],"mod":["abi","contract","reflect","sol","types"],"struct":["H160","H256","KeyComposer","U256","Weight"],"type":["ConstructorResult","Key","MessageResult"]};
+window.SIDEBAR_ITEMS = {"enum":["CodeHashErr","LangError"],"mod":["abi","contract","reflect","sol","types"],"struct":["H160","H256","KeyComposer","U256","Weight"],"type":["ConstructorResult","Key","MessageResult"]};
