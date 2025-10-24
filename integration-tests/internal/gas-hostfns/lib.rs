f04d729c045988a9de691608d8a7337002341544
Lucas Grasso <lucasgrassoramos@gmail.com>
2025-10-24 01:05:07 -0300

    add tests for `gas_limit` host fn

---
+++ integration-tests/internal/gas-hostfns/lib.rs
@@ -0,0 +1,68 @@
+#![cfg_attr(not(feature = "std"), no_std, no_main)]
+#![allow(clippy::new_without_default)]
+
+#[ink::contract]
+mod gas_hostfns {
+    #[ink(storage)]
+    pub struct GasHostfns {}
+
+    impl GasHostfns {
+        #[ink(constructor)]
+        pub fn new() -> Self {
+            Self {}
+        }
+
+		/// Checks that the host function `gas_limit` works
+		#[ink(message)]
+		pub fn gas_limit(self) {
+			self.env.gas_limit()
+		}
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[ink::test]
+        fn works() {
+            let contract = GasHostfns::new();
+            contract.gas_limit();
+        }
+    }
+
+    #[cfg(all(test, feature = "e2e-tests"))]
+    mod e2e_tests {
+        use super::*;
+        use ink_e2e::ContractsBackend;
+
+        type E2EResult<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+        #[ink_e2e::test]
+        async fn e2e_gas_limit_works<Client: E2EBackend>(
+            mut client: Client,
+        ) -> E2EResult<()> {
+            // given
+            let contract = client
+                .instantiate(
+                    "gas_hostfns",
+                    &ink_e2e::alice(),
+                    &mut GasHostfnsRef::new(),
+                )
+                .submit()
+                .await
+                .expect("instantiate failed");
+            let call_builder = contract.call_builder::<GasHostfns>();
+
+            // then
+            let _call_res = client
+                .call(&ink_e2e::alice(), &call_builder.gas_limit())
+                .submit()
+                .await
+                .unwrap_or_else(|err| {
+                    panic!("call failed: {:#?}", err);
+                });
+
+            Ok(())
+        }
+    }
+}
