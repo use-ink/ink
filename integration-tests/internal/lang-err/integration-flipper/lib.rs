a0d9e63dac8a04ceb7809d9d9b175c4fd30f41f7
davidsemakula <hello@davidsemakula.com>
2025-11-03 20:15:09 +0300

    tests: Fix integration tests that re-export contract refs

--- integration-tests/internal/lang-err/integration-flipper/lib.rs
+++ integration-tests/internal/lang-err/integration-flipper/lib.rs
@@ -1,10 +1,5 @@
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
 
-pub use self::integration_flipper::{
-    Flipper,
-    FlipperRef,
-};
-
 #[ink::contract]
 pub mod integration_flipper {
     #[ink(storage)]
