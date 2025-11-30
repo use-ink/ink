c789618335586577340e05c699918cca134dad20
Michael Mueller <mich@elmueller.net>
2025-11-30 22:07:07 +0100

    Fix tests

--- crates/ink/tests/ui/selector_bytes/pass/string_input.rs
+++ crates/ink/tests/ui/selector_bytes/pass/string_input.rs
@@ -1,19 +1,28 @@
 use ink_ir as ir;
 
-macro_rules! assert_macro_eq {
-    ( $input:literal ) => {{
-        // We put it into a constant to verify that the computation is constant.
-        const HASH: [u8; 4] = ink::selector_bytes!(Abi::Ink, $input);
-        assert_eq!(
-            HASH,
-            ir::Selector::compute($input.as_bytes(), ir::SelectorAbi::Ink).to_bytes(),
-        );
-    }};
-}
-
 fn main() {
-    assert_macro_eq!("");
-    assert_macro_eq!("Hello, World!");
-    assert_macro_eq!("message");
-    assert_macro_eq!("constructor");
+    // We put it into a constant to verify that the computation is constant.
+    const HASH1: [u8; 4] = ink::selector_bytes!(Abi::Ink, "");
+    assert_eq!(
+        HASH1,
+        ir::Selector::compute("".as_bytes(), ir::SelectorAbi::Ink).to_bytes(),
+    );
+
+    const HASH2: [u8; 4] = ink::selector_bytes!(Abi::Ink, "Hello, World!");
+    assert_eq!(
+        HASH2,
+        ir::Selector::compute("Hello, World!".as_bytes(), ir::SelectorAbi::Ink).to_bytes(),
+    );
+
+    const HASH3: [u8; 4] = ink::selector_bytes!(Abi::Ink, "message");
+    assert_eq!(
+        HASH3,
+        ir::Selector::compute("message".as_bytes(), ir::SelectorAbi::Ink).to_bytes(),
+    );
+
+    const HASH4: [u8; 4] = ink::selector_bytes!(Abi::Ink, "constructor");
+    assert_eq!(
+        HASH4,
+        ir::Selector::compute("constructor".as_bytes(), ir::SelectorAbi::Ink).to_bytes(),
+    );
 }
--- crates/ink/tests/ui/selector_id/pass/bytestring_input.rs
+++ crates/ink/tests/ui/selector_id/pass/bytestring_input.rs
@@ -1,19 +1,28 @@
 use ink_ir as ir;
 
-macro_rules! assert_macro_eq {
-    ( $input:literal ) => {{
-        // We put it into a constant to verify that the computation is constant.
-        const HASH: u32 = ink::selector_id!(Abi::Ink, $input);
-        assert_eq!(
-            HASH,
-            ir::Selector::compute($input).into_be_u32(),
-        );
-    }};
-}
-
 fn main() {
-    assert_macro_eq!(b"");
-    assert_macro_eq!(b"Hello, World!");
-    assert_macro_eq!(b"message");
-    assert_macro_eq!(b"constructor");
+    // We put it into a constant to verify that the computation is constant.
+    const HASH1: u32 = ink::selector_id!(Abi::Ink, b"");
+    assert_eq!(
+        HASH1,
+        ir::Selector::compute(b"", ir::SelectorAbi::Ink).into_be_u32(),
+    );
+
+    const HASH2: u32 = ink::selector_id!(Abi::Ink, b"Hello, World!");
+    assert_eq!(
+        HASH2,
+        ir::Selector::compute(b"Hello, World!", ir::SelectorAbi::Ink).into_be_u32(),
+    );
+
+    const HASH3: u32 = ink::selector_id!(Abi::Ink, b"message");
+    assert_eq!(
+        HASH3,
+        ir::Selector::compute(b"message", ir::SelectorAbi::Ink).into_be_u32(),
+    );
+
+    const HASH4: u32 = ink::selector_id!(Abi::Ink, b"constructor");
+    assert_eq!(
+        HASH4,
+        ir::Selector::compute(b"constructor", ir::SelectorAbi::Ink).into_be_u32(),
+    );
 }
