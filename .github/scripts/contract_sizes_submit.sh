21c7cc5a5c2498faf93c1478ef079714f4ea1c44
David Semakula <davidsemakula@users.noreply.github.com>
2025-12-17 13:38:44 +0300

    ci: ABI specific size comparisons with master (#2755)

--- .github/scripts/contract_sizes_submit.sh
+++ .github/scripts/contract_sizes_submit.sh
@@ -16,10 +16,12 @@ set -o pipefail
 pr_comments_url=$1
 workflow_url=$2
 head_in_branch=$3
-diffs_markdown_table=$(cat $4)
-abi_sol_diffs_markdown_table=$(cat $5)
-abi_all_diffs_markdown_table=$(cat $6)
-mermaid_diagram=$(cat $7)
+master_v_pr_diffs_markdown_table=$(cat $4)
+master_v_pr_abi_sol_diffs_markdown_table=$(cat $5)
+master_v_pr_abi_all_diffs_markdown_table=$(cat $6)
+pr_abi_ink_v_sol_diffs_markdown_table=$(cat $7)
+pr_abi_ink_v_all_diffs_markdown_table=$(cat $8)
+mermaid_diagram=$(cat $9)
 
 # If there is already a comment by the user `github-actions[bot]` in the ink! PR which triggered
 # this run, then we can just edit this comment (using `PATCH` instead of `POST`).
@@ -51,11 +53,11 @@ body=$(cat << EOF
 ${master_ahead}
 These are the results when building the \`integration-tests/*\` contracts from this branch and comparing them to ink! \`master\`:
 
-<details><summary>Show overview</summary>
+<details><summary>Show contract sizes: PR vs master</summary>
 
 Using the ABI denoted in the contract manifest.
 
-${diffs_markdown_table}
+${master_v_pr_diffs_markdown_table}
 
 </details>
 
@@ -65,19 +67,35 @@ ${mermaid_diagram}
 
 </details>
 
+<details><summary>Show contract sizes: PR vs master (with enforced Solidity ABI)</summary>
+
+The following table shows how the contract sizes _on this branch_ change compared to the \`master\` branch when using the _Solidity ABI_.
+
+${master_v_pr_abi_sol_diffs_markdown_table}
+
+</details>
+
+<details><summary>Show contract sizes: PR vs master (with enforced all ABI mode)</summary>
+
+The following table shows how the contract sizes _on this branch_ change compared to the \`master\` branch when using the _all ABI_ mode.
+
+${master_v_pr_abi_all_diffs_markdown_table}
+
+</details>
+
 <details><summary>Show contract sizes: ink! vs Solidity ABI</summary>
 
 The following table shows how the contract sizes _on this branch_ change when choosing the _Solidity ABI_ instead of the ink! ABI.
 
-${abi_sol_diffs_markdown_table}
+${pr_abi_ink_v_sol_diffs_markdown_table}
 
 </details>
 
-<details><summary>Show contract sizes: ink! vs all ABI</summary>
+<details><summary>Show contract sizes: ink! vs all ABI mode</summary>
 
-The following table shows how the contract sizes _on this branch_ change when choosing the _all ABI_ instead of the ink! ABI.
+The following table shows how the contract sizes _on this branch_ change when choosing the _all ABI_ mode instead of the ink! ABI.
 
-${abi_all_diffs_markdown_table}
+${pr_abi_ink_v_all_diffs_markdown_table}
 
 </details>
 
